f73fd5445ceb8e2e594a2fa08045591920a34d7b
Sam Caldwell <github@samcaldwell.net>
2024-08-12 12:40:58 -0500

    add getExpectedDefaultRoute() and a test.  This is intended to help test other things.

--- getExpectedDefaultRoute.go
+++ getExpectedDefaultRoute.go
@@ -9,16 +9,16 @@ import (
 // getExpectedDefaultRoute - run "route -v4" and parses the output to obtain route info.
 func getExpectedDefaultRoute() (*RouteInfo, error) {
 	/*
-		         *  $ route -4
-				 *  Kernel IP routing table
-				 *	Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
-				 *	default         192.168.3.1     0.0.0.0         UG    600    0        0 wlp4s0
-				 *	link-local      0.0.0.0         255.255.0.0     U     1000   0        0 virbr0
-				 *	172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
-				 *	192.168.3.0     0.0.0.0         255.255.255.0   U     600    0        0 wlp4s0
-				 *	192.168.49.0    0.0.0.0         255.255.255.0   U     0      0        0 br-c7bddc1f0590
-				 *	192.168.122.0   0.0.0.0         255.255.255.0   U     0      0        0 virbr0
-	*/
+	 *  $ route -4
+	 *  Kernel IP routing table
+	 *	Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+	 *	default         192.168.3.1     0.0.0.0         UG    600    0        0 wlp4s0
+	 *	link-local      0.0.0.0         255.255.0.0     U     1000   0        0 virbr0
+	 *	172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
+	 *	192.168.3.0     0.0.0.0         255.255.255.0   U     600    0        0 wlp4s0
+	 *	192.168.49.0    0.0.0.0         255.255.255.0   U     0      0        0 br-c7bddc1f0590
+	 *	192.168.122.0   0.0.0.0         255.255.255.0   U     0      0        0 virbr0
+	 */
 	output, err := exec.Command("route", "-v4").Output()
 	if err != nil {
 		return nil, err
