d277a473c97d2516343892056e1346826a6b7bc1
Sam Caldwell <github@samcaldwell.net>
2024-08-12 16:29:16 -0500

    last tests for GetDefaultRouteInfo()

--- GetDefaultRouteIPv6_test.go
+++ GetDefaultRouteIPv6_test.go
@@ -1,1 +1,25 @@
 package network
+
+import (
+	"testing"
+)
+
+func TestGetDefaultRouteInfo(t *testing.T) {
+	t.Run("Test routes", func(t *testing.T) {
+		// Get the expected RouteInfo for comparison.
+		expected, err := getExpectedDefaultRoute()
+		if err != nil {
+			t.Error(err)
+		}
+		route, err := GetDefaultRouteInfo()
+		if err != nil {
+			t.Error(err)
+		}
+		if route != nil && !route.Equal(expected) {
+			t.Errorf("expected: %v, actual: %v", expected, route)
+		}
+		if route != nil && !route.Equal(expected) {
+			t.Errorf("expected: %v, actual: %v", expected, route)
+		}
+	})
+}
--- GetDefaultRouteInfo.go
+++ GetDefaultRouteInfo.go
@@ -1,16 +1,16 @@
 package network
 
 // GetDefaultRouteInfo - Get the default network address, gateway address, subnet mask and any errors.
-func GetDefaultRouteInfo() (v4, v6 *RouteInfo, err error) {
+func GetDefaultRouteInfo() (route *RouteInfo, err error) {
 	// First, check IPv4 routes
-	if v4, err = GetDefaultRouteIPv4(); err != nil {
-		return nil, nil, err
+	if route, err = GetDefaultRouteIPv4(); err != nil {
+		return nil, err
 	}
-
-	// If no IPv4 default route, check IPv6 routes
-	if v6, err = GetDefaultRouteIPv6(); err != nil {
-		return nil, nil, err
+	if route == nil {
+		//If no IPv4 default route, check IPv6 routes
+		if route, err = GetDefaultRouteIPv6(); err != nil {
+			return nil, err
+		}
 	}
-
-	return v4, v6, nil
+	return route, nil
 }
