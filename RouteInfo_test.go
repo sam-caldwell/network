3757d1feb64d209b73bc1cabae9a1d49703696be
Sam Caldwell <github@samcaldwell.net>
2024-08-12 11:16:37 -0500

    add test for RouteInfo.ToString()

--- RouteInfo_test.go
+++ RouteInfo_test.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -17,7 +18,22 @@ func TestRouteInfo(t *testing.T) {
 		}
 	})
 	t.Run("verify ToString() method", func(t *testing.T) {
-
+		interface0 := "interface0"
+		network, _ := StringToIPNet("0.0.0.0/0")
+		gateway := StringToIP("192.168.13.14")
+		subnetMask, _ := StringToIPMask("255.255.255.0")
+		route := RouteInfo{
+			Interface: interface0,
+			Network:   network,
+			Gateway:   gateway,
+			Netmask:   subnetMask,
+		}
+		expected := fmt.Sprintf("%s %s %s %s", interface0, network.String(), gateway.String(), subnetMask.String())
+		if actual := route.ToString(); actual != expected {
+			t.Fatalf("route mismatch\n"+
+				"  actual: %s\n"+
+				"expected: %s", actual, expected)
+		}
 	})
 
 }
