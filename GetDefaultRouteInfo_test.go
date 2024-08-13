57c0fad0c128849ebdb73346977b36f8417265d8
Sam Caldwell <github@samcaldwell.net>
2024-08-12 22:48:29 -0500

    Remove change from debugging GetDefaultRouteInfo() earlier

--- GetDefaultRouteInfo_test.go
+++ GetDefaultRouteInfo_test.go
@@ -9,19 +9,23 @@ import (
 func TestGetDefaultRouteInfo(t *testing.T) {
 	t.Run("Test routes", func(t *testing.T) {
 		// Get the expected RouteInfo for comparison.
-		expected, err := getExpectedDefaultRoute(false)
+		expectedV4, err := getExpectedDefaultRoute(false)
 		if err != nil {
 			t.Error(err)
 		}
-		route, err := GetDefaultRouteInfo()
+		expectedV6, err := getExpectedDefaultRoute(true)
 		if err != nil {
 			t.Error(err)
 		}
-		if route != nil && !route.Equal(expected) {
-			t.Errorf("expected: %v, actual: %v", expected, route)
+		route4, route6, err := GetDefaultRouteInfo()
+		if err != nil {
+			t.Error(err)
 		}
-		if route != nil && !route.Equal(expected) {
-			t.Errorf("expected: %v, actual: %v", expected, route)
+		if route4 != nil && !route4.Equal(expectedV4) {
+			t.Errorf("expectedV4: %v, actual: %v", expectedV4, route4)
+		}
+		if route6 != nil && !route6.Equal(expectedV6) {
+			t.Errorf("expected: %v, actual: %v", expectedV6, route6)
 		}
 	})
 }
