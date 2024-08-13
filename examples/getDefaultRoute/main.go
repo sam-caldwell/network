3a92ffa79e0a4e0c2172c4aabaabcc7106eab4b7
Sam Caldwell <github@samcaldwell.net>
2024-08-12 22:52:57 -0500

    test program added (getDefaultRoute)

--- examples/getDefaultRoute/main.go
+++ examples/getDefaultRoute/main.go
@@ -6,17 +6,18 @@ import (
 )
 
 func main() {
-	v4, v6, err := network.GetDefaultRouteInfo()
+	route4, route6, err := network.GetDefaultRouteInfo()
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	if v4 == nil {
-		log.Println("no ipv4 default route found")
+	if route4 == nil {
+		log.Println("no ipv4 default route4 found")
 	}
-	log.Printf("ipv4 route: %s", v4.ToString())
-	if v6 == nil {
-		log.Println("no ipv6 default route found")
+	log.Printf("            iface   network    gateway   mask")
+	log.Printf("ipv4 route: %s", route4.ToString())
+	if route6 == nil {
+		log.Println("no ipv6 default route6 found")
 	}
-	log.Printf("ipv6 route: %s", v6.ToString())
+	log.Printf("ipv6 route: %s", route6.ToString())
 }
