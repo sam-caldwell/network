ba0137314da87fbc88c1734b7de6c28d3dd6fe49
Sam Caldwell <github@samcaldwell.net>
2024-08-15 14:55:29 -0500

    break out Fqdn.FromString() to separate file

---
+++ Fqdn.FromString.go
@@ -0,0 +1,14 @@
+package network
+
+import "fmt"
+
+// FromString - Given a string pointer, convert it to an FQDN
+func (o *Fqdn) FromString(v *string) (err error) {
+
+	if IsValidAddress(*v) {
+		*o = Fqdn(*v)
+	} else {
+		err = fmt.Errorf("FQDN cannot be empty")
+	}
+	return err
+}
