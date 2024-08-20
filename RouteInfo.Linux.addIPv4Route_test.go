0ccfbfd61bd2e138d54f6454c6b43323ada08f27
Sam Caldwell <github@samcaldwell.net>
2024-08-20 14:43:20 -0500

    refactor containerized test for simplicity

--- RouteInfo.Linux.addIPv4Route_test.go
+++ RouteInfo.Linux.addIPv4Route_test.go
@@ -4,38 +4,19 @@ package network
 
 import (
 	"github.com/sam-caldwell/network/test"
-	"os/exec"
-	"strings"
 	"testing"
 )
 
 func TestAddIPv4Route(t *testing.T) {
-	t.Run("Build test container", func(t *testing.T) {
-		var output []byte
-		var err error
-		commandStr := []string{
-			"build", "--tag", "network-test:latest", ".",
-		}
-		output, err = exec.Command("docker", commandStr...).Output()
-		t.Logf("Container build output:\n===\n%s\n===\n", string(output))
-		if err != nil {
+	const testDockerImage = "network-test:latest"
+	t.Run("build container", func(t *testing.T) {
+		if err := test.BuildTestContainer(testDockerImage); err != nil {
 			t.Fatal(err)
 		}
 	})
-
-	t.Run("Run containerized test", func(t *testing.T) {
-		var output []byte
-		var err error
-		dir := test.GetCurrentWorkingDirectory()
-		commandStr := []string{
-			"run", "--cap-add=NET_ADMIN", "-v", dir + ":/opt", "network-test:latest",
-		}
-		output, err = exec.Command("docker", commandStr...).Output()
-		t.Logf("Container run output:\n===\n%s\n===\n", string(output))
-		if err != nil {
-			t.Fatalf("containerized test failed:\n"+
-				"error: %v\n"+
-				"command: %v", err, strings.Join(commandStr, " "))
+	t.Run("run test container", func(t *testing.T) {
+		if err := test.RunContainer(testDockerImage, "TestAddIpV4Route"); err != nil {
+			t.Fatal(err)
 		}
 	})
 }
