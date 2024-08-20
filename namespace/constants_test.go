dbc4a044e2990581212989e3a4bbec08359de053
Sam Caldwell <github@samcaldwell.net>
2024-08-20 18:11:59 -0500

    constants test for namespace

--- namespace/constants_test.go
+++ namespace/constants_test.go
@@ -5,13 +5,17 @@ import (
 )
 
 func TestConstants(t *testing.T) {
+
 	if bindMountPath != "/run/netns" {
 		t.Fatal("mismatch")
 	}
+
 	if closedHandle != -1 {
 		t.Fatal("mismatch")
 	}
+
 	if ErrNotImplemented != "not implemented" {
 		t.Fatal("mismatch")
 	}
+
 }
