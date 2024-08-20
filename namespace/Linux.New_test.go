5cc49ce55a7d2ffe294175f6c20fde3484d47727
Sam Caldwell <github@samcaldwell.net>
2024-08-20 18:12:51 -0500

    namespace/Linux.New() and test added

---
+++ namespace/Linux.New_test.go
@@ -0,0 +1,21 @@
+package namespace
+
+import (
+	"github.com/sam-caldwell/network/test"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	const testDockerImage = "network-test:latest"
+
+	t.Run("build container", func(t *testing.T) {
+		if err := test.BuildTestContainer(testDockerImage); err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("run test container", func(t *testing.T) {
+		if err := test.RunContainer(testDockerImage, "TestCreateNewNamespace"); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
