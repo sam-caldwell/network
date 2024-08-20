dfc92355ba78f09756943d124d40124d6a842371
Sam Caldwell <github@samcaldwell.net>
2024-08-20 14:32:06 -0500

    add test.RunContainer()

--- test/RunContainer.go
+++ test/RunContainer.go
@@ -6,10 +6,10 @@ import (
 )
 
 // RunContainer - Run a given test program inside a docker container
-func RunContainer(test string) error {
+func RunContainer(imageName, test string) error {
 	const command = "docker"
 	args := []string{
-		"run", "--cap-add=NET_ADMIN", "-v", GetCurrentWorkingDirectory() + ":/opt", "network-test:latest",
+		"run", "--cap-add=NET_ADMIN", "-v", GetCurrentWorkingDirectory() + ":/opt", imageName,
 	}
 	output, err := exec.Command(command, args...).Output()
 	if err != nil {
