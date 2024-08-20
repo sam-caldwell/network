0d30f3501d8a24ac9edba0160e9ec9363ba5341f
Sam Caldwell <github@samcaldwell.net>
2024-08-20 13:21:59 -0500

    test the constants

---
+++ namespace/constants_test.go
@@ -0,0 +1,17 @@
+package namespace
+
+import (
+	"testing"
+)
+
+func TestConstants(t *testing.T) {
+	if bindMountPath != "/run/netns" {
+		t.Fatal("mismatch")
+	}
+	if closedHandle != -1 {
+		t.Fatal("mismatch")
+	}
+	if ErrNotImplemented != "not implemented" {
+		t.Fatal("mismatch")
+	}
+}
