356febed10b34d9bdb3d6126b5d2c97cc7f1a61f
Sam Caldwell <github@samcaldwell.net>
2024-08-20 13:02:01 -0500

    use constant instead of -1 for closedHandle indicator

--- namespace/Handle.Close.go
+++ namespace/Handle.Close.go
@@ -4,13 +4,13 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// Close - close the NamespaceHandle and reset its file descriptor to -1.
+// Close - close the NamespaceHandle and reset its file descriptor to -1 (closedHandle).
 //
 // WARNING: DO NOT USE AFTER Close() is called.  Bad things will happen.
 func (ns *Handle) Close() error {
 	if err := unix.Close(int(*ns)); err != nil {
 		return err
 	}
-	*ns = -1
+	*ns = closedHandle
 	return nil
 }
