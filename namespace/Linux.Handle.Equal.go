5960c17f3ba65f483bc1e10a528f1824add45f1a
Sam Caldwell <github@samcaldwell.net>
2024-08-20 21:53:13 -0500

    add test for Linux.Handle.Equal

--- namespace/Linux.Handle.Equal.go
+++ namespace/Linux.Handle.Equal.go
@@ -10,15 +10,9 @@ import (
 //
 // This works by comparing the device and its file descriptor inode.
 func (ns *Handle) Equal(other Handle) bool {
-	if *ns == other {
-		return true
-	}
 	var a, b unix.Stat_t
-	if err := unix.Fstat(int(*ns), &a); err != nil {
-		return false
-	}
-	if err := unix.Fstat(int(other), &b); err != nil {
-		return false
-	}
-	return (a.Dev == b.Dev) && (a.Ino == b.Ino)
+	return (*ns == other) || (unix.Fstat(int(*ns), &a) == nil) &&
+		(unix.Fstat(int(other), &b) == nil) &&
+		(a.Dev == b.Dev) &&
+		(a.Ino == b.Ino)
 }
