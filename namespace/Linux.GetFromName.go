1bf8ce4780086168999ff3bccec3964075a90bcd
Sam Caldwell <github@samcaldwell.net>
2024-08-20 21:02:25 -0500

    add namespace/Linux.GetFromName() and test

--- namespace/Linux.GetFromName.go
+++ namespace/Linux.GetFromName.go
@@ -2,12 +2,11 @@
 
 package namespace
 
-import "path/filepath"
+import (
+	"path/filepath"
+)
 
 // GetFromName - return a handle to the named network namespace.
-//
-// Testing Note: `ip netns add` will create a namespace which we can fetch
-// for testing.
 func GetFromName(name string) (Handle, error) {
 	return GetFromPath(filepath.Join(bindMountPath, name))
 }
---
+++ namespace/Linux.GetFromName_test.go
@@ -0,0 +1,22 @@
+package namespace
+
+import (
+	"github.com/sam-caldwell/network/test"
+	"testing"
+)
+
+func TestGetFromName(t *testing.T) {
+	const testDockerImage = "network-test:latest"
+
+	t.Run("build container", func(t *testing.T) {
+		if err := test.BuildTestContainer(testDockerImage); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("run test container", func(t *testing.T) {
+		if err := test.RunContainer(testDockerImage, "TestCreateGetFromName"); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
