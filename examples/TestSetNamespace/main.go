9e8666bb5b886df52f935500db398afc8aafdc34
Sam Caldwell <github@samcaldwell.net>
2024-08-21 09:15:27 -0500

    add new NsTypes so setns makes sense

--- examples/TestSetNamespace/main.go
+++ examples/TestSetNamespace/main.go
@@ -1,37 +1,29 @@
-package namespace
+package main
 
 import (
 	"github.com/sam-caldwell/network/namespace"
 	"golang.org/x/sys/unix"
 	"log"
-	"os"
 )
 
 func main() {
 	const namespaceName = "testnamespace"
-	createTestNamespace := func() namespace.Handle {
-		f, err := os.CreateTemp("", namespaceName)
-		if err != nil {
-			log.Fatalf("Failed to create temp namespace file: %v", err)
-		}
-		defer func() { _ = f.Close() }()
-		// Create the namespace
-		fd := f.Fd()
-		return namespace.Handle(fd)
+
+	originalNamespace, err := namespace.Get()
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	// Test SetNamespace function
-	ns := createTestNamespace()
-
-	defer func() {
-		// Clean up temporary namespace file
-		if err := os.Remove(namespaceName); err != nil {
-			log.Fatalf("Failed to remove test namespace: %v", err)
-		}
-	}()
-
+	newNamespace, err := namespace.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if originalNamespace.Equal(newNamespace) {
+		log.Fatal("newNamespace create failed")
+	}
 	// Attempt to set the namespace
-	if err := namespace.SetNamespace(ns, unix.CLONE_NEWNET); err != nil {
+	if err := namespace.SetNamespace(originalNamespace, unix.CLONE_NEWNET); err != nil {
 		log.Fatalf("Failed to set namespace: %v", err)
 	}
+
+	log.Println("Successfully set the namespace.")
 }
--- namespace/Linux.SetNamespace.go
+++ namespace/Linux.SetNamespace.go
@@ -11,6 +11,8 @@ import (
 // We keep this wrapper around unix.Setns() because SetNamespace() is more readable
 // and less likely to be confused with a few other things we are building (e.g. name server).
 // ...and there's the golang love of changing things under us that make wrappers appealing.
-func SetNamespace(namespace Handle, namespaceType int) error {
-	return unix.Setns(int(namespace), namespaceType)
+//
+// See https://man7.org/linux/man-pages/man2/setns.2.html
+func SetNamespace(namespace Handle, namespaceType NsTypes) error {
+	return unix.Setns(int(namespace), int(namespaceType))
 }
