c043ffde9a16ef3e7d5bf38153d69f4bdf78174e
Sam Caldwell <github@samcaldwell.net>
2024-08-21 13:37:51 -0500

    Add BridgeVlanInfo struct

--- netlink/core/BridgeVlanInfo.go
+++ netlink/core/BridgeVlanInfo.go
@@ -2,7 +2,9 @@
 
 package core
 
-// BridgeVlanInfo
+// BridgeVlanInfo - represents a specific VLAN (Virtual Local Area Network) configuration associated with
+// a network bridge. This structure is typically used in low-level networking operations on Linux systems,
+// particularly when interacting with network interfaces and managing VLANs.
 //
 //	struct bridge_vlan_info {
 //	  __u16 flags;
