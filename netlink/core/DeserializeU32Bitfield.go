d237b35c0c27d2e7de2e049e363acd8d86dbc5bd
Sam Caldwell <github@samcaldwell.net>
2024-08-21 14:03:00 -0500

    add DeserializeU32Bitfield()

---
+++ netlink/core/DeserializeU32Bitfield.go
@@ -0,0 +1,21 @@
+package core
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// DeserializeUint32Bitfield - deserialize a Uint32Bitfield from a byte slice.
+func DeserializeUint32Bitfield(data []byte) (*Uint32Bitfield, error) {
+	if len(data) < SizeofUint32Bitfield {
+		return nil, errors.New("data slice too short")
+	}
+
+	// Create a new Uint32Bitfield and use binary package to handle endian conversion
+	bitfield := &Uint32Bitfield{
+		Value:    binary.LittleEndian.Uint32(data[0:4]),
+		Selector: binary.LittleEndian.Uint32(data[4:8]),
+	}
+
+	return bitfield, nil
+}
