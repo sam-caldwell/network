e8d6e5fdb9adff5e47b2a6708656ed10a5f35515
Sam Caldwell <github@samcaldwell.net>
2024-08-20 21:55:55 -0500

    fix namespace/GetFromPath()

--- namespace/Linux.GetFromPath.go
+++ namespace/Linux.GetFromPath.go
@@ -9,8 +9,5 @@ import (
 // GetFromPath - return a handle to the network namespace identified by the given path.
 func GetFromPath(path string) (Handle, error) {
 	fd, err := unix.Open(path, unix.O_RDONLY|unix.O_CLOEXEC, 0)
-	if err != nil {
-		return -1, err
-	}
-	return Handle(fd), nil
+	return Handle(fd), err
 }
