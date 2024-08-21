a34ed31a83eaf25a6db3c9d33c2ea5bf94567aa4
Sam Caldwell <github@samcaldwell.net>
2024-08-21 11:43:48 -0500

    add enableErrorMessageReporting and its associated setter functions

---
+++ netlink/core/enableErrorMessageReporting.go
@@ -0,0 +1,16 @@
+//go:build linux
+
+package core
+
+// enableErrorMessageReporting is the default error message reporting configuration for the new netlink sockets
+var enableErrorMessageReporting bool = false
+
+// EnableErrorMessageReporting - enable error message reporting for new netlink sockets
+func EnableErrorMessageReporting() {
+	enableErrorMessageReporting = true
+}
+
+// DisableErrorMessageReporting - disable error message reporting for new netlink sockets
+func DisableErrorMessageReporting() {
+	enableErrorMessageReporting = false
+}
