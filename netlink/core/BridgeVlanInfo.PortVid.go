d2b1097b8d81851d502487a551cc76375fdfcd90
Sam Caldwell <github@samcaldwell.net>
2024-08-21 13:53:21 -0500

    add comment to document BridgeVlanInfo.PortVID

--- netlink/core/BridgeVlanInfo.PortVid.go
+++ netlink/core/BridgeVlanInfo.PortVid.go
@@ -2,8 +2,10 @@
 
 package core
 
-func (b *BridgeVlanInfo) PortVID() bool {
+// PortVID - returns the VLAN ID assigned to a port in the bridge,
+// determining which VLAN traffic is tagged for that port.
+func (bridge *BridgeVlanInfo) PortVID() bool {
 
-	return b.Flags&BRIDGE_VLAN_INFO_PVID > 0
+	return bridge.Flags&BRIDGE_VLAN_INFO_PVID > 0
 
 }
