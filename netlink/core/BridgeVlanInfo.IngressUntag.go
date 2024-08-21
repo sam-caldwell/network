5627bac95c8eb0813cbc8a8338a9fef3e9c45210
Sam Caldwell <github@samcaldwell.net>
2024-08-21 13:46:39 -0500

    add comment for BridgeVlanInfo.IngressUntag()

--- netlink/core/BridgeVlanInfo.IngressUntag.go
+++ netlink/core/BridgeVlanInfo.IngressUntag.go
@@ -2,8 +2,11 @@
 
 package core
 
-func (b *BridgeVlanInfo) IngressUntag() bool {
+// IngressUntag - flag within the BridgeVlanInfo structure that indicates whether incoming packets
+// with a matching VLAN ID should have their VLAN tags removed (untagged) before being forwarded.
+// This is commonly used to manage how VLAN-tagged traffic is handled when it enters a network bridge.
+func (bridge *BridgeVlanInfo) IngressUntag() bool {
 
-	return b.Flags&BRIDGE_VLAN_INFO_UNTAGGED > 0
-	
+	return bridge.Flags&BRIDGE_VLAN_INFO_UNTAGGED > 0
+
 }
