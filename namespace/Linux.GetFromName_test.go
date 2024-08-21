0d51b6f2a4a3bd32b930831cd37801d59fc5ac7f
Sam Caldwell <github@samcaldwell.net>
2024-08-21 07:33:41 -0500

    back out change to GetFromName()

--- namespace/Linux.GetFromName_test.go
+++ namespace/Linux.GetFromName_test.go
@@ -1,44 +1,23 @@
-//go:build linux
-
 package namespace
 
 import (
-	"golang.org/x/sys/unix"
+	"github.com/sam-caldwell/network/test"
 	"testing"
 )
 
 func TestGetFromName(t *testing.T) {
-	t.Run("retrieve a handle for a named network namespace", func(t *testing.T) {
-		name := "my-namespace" // Adjust to a valid namespace name for testing
-		handle, err := GetFromName(name)
-		if err != nil {
-			t.Fatalf("expected no error. got: %v", err)
-		}
-		if handle < 0 {
-			t.Fatalf("expected handle to be a valid file descriptor, got: %d", handle)
-		}
+	t.Skip("Disabled for debugging")
+	const testDockerImage = "network-test:latest"
 
-		// Check if the handle is valid by attempting to get file status
-		var stat unix.Stat_t
-		err = unix.Fstat(int(handle), &stat)
-		if err != nil {
-			t.Fatalf("expected no error. got: %v", err)
-		}
-
-		// Clean up by closing the handle
-		if err := handle.Close(); err != nil {
-			t.Fatalf("expected no error when closing the file descriptor. got: %v", err)
+	t.Run("build container", func(t *testing.T) {
+		if err := test.BuildTestContainer(testDockerImage); err != nil {
+			t.Fatal(err)
 		}
 	})
 
-	t.Run("handle non-existent namespace", func(t *testing.T) {
-		name := "non-existent-namespace"
-		handle, err := GetFromName(name)
-		if err == nil {
-			t.Fatalf("expected an error for non-existent namespace")
-		}
-		if handle != closedHandle {
-			t.Fatalf("expected handle to be %v, got %v", closedHandle, handle)
+	t.Run("run test container", func(t *testing.T) {
+		if err := test.RunContainer(testDockerImage, "TestCreateGetFromName"); err != nil {
+			t.Fatal(err)
 		}
 	})
 }
