16c8f3d090396e8b17b1d382ee21c4f8ce6cbc0b
Sam Caldwell <github@samcaldwell.net>
2024-08-21 13:47:15 -0500

    add IfInfoMsg.Deserialize()

--- netlink/core/IfInfoMsg.Deserialize.go
+++ netlink/core/IfInfoMsg.Deserialize.go
@@ -1,5 +1,6 @@
 package core
 
+// Deserialize - Deserialize byte slice into IfInfoMsg struct
 func (msg *IfInfoMsg) Deserialize(b []byte) (err error) {
 	var result *IfInfoMsg
 	if result, err = DeserializeIfInfoMsg(b); err != nil {
