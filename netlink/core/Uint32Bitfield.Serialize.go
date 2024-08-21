4c51652f0042a47e5407e52b0af1f301edff0414
Sam Caldwell <github@samcaldwell.net>
2024-08-21 14:04:10 -0500

    add Uint32Bitfield.Serialize() method

---
+++ netlink/core/Uint32Bitfield.Serialize.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// Serialize - serialize the Uint32Bitfield struct into a byte slice
+func (a *Uint32Bitfield) Serialize() ([]byte, error) {
+	// Allocate a byte slice of the required size
+	data := make([]byte, SizeofUint32Bitfield)
+
+	// Check if SizeofUint32Bitfield matches the expected size
+	if len(data) < 8 {
+		return nil, errors.New("invalid size for Uint32Bitfield serialization")
+	}
+
+	// Use binary package to handle endian conversion
+	binary.LittleEndian.PutUint32(data[0:4], a.Value)
+	binary.LittleEndian.PutUint32(data[4:8], a.Selector)
+
+	return data, nil
+}
