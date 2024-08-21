e2c6df8be97e3afc0757367dfe0b95363d95a55f
Sam Caldwell <github@samcaldwell.net>
2024-08-21 07:20:31 -0500

    improved (faster) test for namespace Get()

--- namespace/Linux.Get_test.go
+++ namespace/Linux.Get_test.go
@@ -1,22 +1,30 @@
 package namespace
 
 import (
-	"github.com/sam-caldwell/network/test"
+	"golang.org/x/sys/unix"
 	"testing"
 )
 
 func TestGet(t *testing.T) {
-	const testDockerImage = "network-test:latest"
-
-	t.Run("build container", func(t *testing.T) {
-		if err := test.BuildTestContainer(testDockerImage); err != nil {
-			t.Fatal(err)
+	t.Run("retrieve the current thread's network namespace handle", func(t *testing.T) {
+		handle, err := Get()
+		if err != nil {
+			t.Fatalf("expected no error. got: %v", err)
 		}
-	})
+		if handle == closedHandle {
+			t.Fatalf("expected handle to be valid, got closedHandle")
+		}
 
-	t.Run("run test container", func(t *testing.T) {
-		if err := test.RunContainer(testDockerImage, "TestCreateGetSetNamespace"); err != nil {
-			t.Fatal(err)
+		// Check if the handle is valid by attempting to get file status
+		var stat unix.Stat_t
+		err = unix.Fstat(int(handle), &stat)
+		if err != nil {
+			t.Fatalf("expected no error. got: %v", err)
+		}
+
+		// Clean up by closing the handle
+		if err := handle.Close(); err != nil {
+			t.Fatalf("expected no error when closing the file descriptor. got: %v", err)
 		}
 	})
 }
