b0c8861ad3265108e521eb6eb8ba84798de81774
Sam Caldwell <github@samcaldwell.net>
2024-09-01 00:50:09 -0500

    add DeserializeIfaCacheInfo()

---
+++ netlink/core/DeserializeIfaCacheInfo.go
@@ -0,0 +1,29 @@
+package core
+
+import (
+	"errors"
+	"golang.org/x/sys/unix"
+)
+
+// DeserializeIfaCacheInfo converts a byte slice to an IfaCacheInfo struct
+//
+//	type IfaCacheinfo struct {
+//	   Prefered uint32
+//	   Valid    uint32
+//	   Cstamp   uint32
+//	   Tstamp   uint32
+//	}
+func DeserializeIfaCacheInfo(data []byte) (*IfaCacheInfo, error) {
+	if len(data) < int(SizeOfIfaCacheinfo) {
+		return nil, errors.New("data slice too short")
+	}
+	info := &IfaCacheInfo{
+		unix.IfaCacheinfo{
+			Prefered: nativeEndian.Uint32(data[0:4]),
+			Valid:    nativeEndian.Uint32(data[4:8]),
+			Cstamp:   nativeEndian.Uint32(data[8:12]),
+			Tstamp:   nativeEndian.Uint32(data[12:16]),
+		},
+	}
+	return info, nil
+}
