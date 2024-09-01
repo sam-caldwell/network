f9453a4cb96dbe5bc2a583eefe3330e2e41b2470
Sam Caldwell <github@samcaldwell.net>
2024-09-01 15:51:33 -0500

    refactor ParseRouteAttr() to get away from syscall

--- netlink/core/ParseRouteAttr.go
+++ netlink/core/ParseRouteAttr.go
@@ -2,19 +2,25 @@ package core
 
 import (
 	"golang.org/x/sys/unix"
-	"syscall"
 )
 
 func ParseRouteAttr(b []byte) ([]NetlinkRouteAttr, error) {
-	var attrs []syscall.NetlinkRouteAttr
+
+	var attrs []NetlinkRouteAttr
+
 	for len(b) >= unix.SizeofRtAttr {
 		a, vbuf, alen, err := netlinkRouteAttrAndValue(b)
 		if err != nil {
 			return nil, err
 		}
-		ra := NetlinkRouteAttr{Attr: RtAttr(*a), Value: vbuf[:int(a.Len)-unix.SizeofRtAttr]}
+		ra := NetlinkRouteAttr{
+			Attr:  *a,
+			Value: vbuf[:int(a.RtAttr.Len)-unix.SizeofRtAttr],
+		}
 		attrs = append(attrs, ra)
 		b = b[alen:]
 	}
+
 	return attrs, nil
+
 }
