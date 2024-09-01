f02190a1ff15eb2ef4479dea033e6f2fe2bf82a9
Sam Caldwell <github@samcaldwell.net>
2024-09-01 04:34:12 -0500

    add CtAttrCounters

--- netlink/core/CtAttrCounters.go
+++ netlink/core/CtAttrCounters.go
@@ -4,10 +4,21 @@ package core
 type CtAttrCounters int
 
 const (
-	CTA_COUNTERS_PACKETS   CtAttrCounters = 1 /* 64bit counters */
-	CTA_COUNTERS_BYTES     CtAttrCounters = 2 /* 64bit counters */
-	CTA_COUNTERS32_PACKETS CtAttrCounters = 3 /* old 32bit counters, unused */
-	CTA_COUNTERS32_BYTES   CtAttrCounters = 4 /* old 32bit counters, unused */
-	CTA_COUNTERS_PAD       CtAttrCounters = 5
-	CTA_COUNTERS_MAX                      = CTA_COUNTERS_PAD
+	// CtaCountersPackets - CTA_COUNTERS_PACKETS
+	CtaCountersPackets CtAttrCounters = 1 /* 64bit counters */
+
+	// CtaCountersBytes - CTA_COUNTERS_BYTES
+	CtaCountersBytes CtAttrCounters = 2 /* 64bit counters */
+
+	// CtaCounters32Packets - CTA_COUNTERS32_PACKETS
+	CtaCounters32Packets CtAttrCounters = 3 /* old 32bit counters, unused */
+
+	// CtaCounters32Bytes - CTA_COUNTERS32_BYTES
+	CtaCounters32Bytes CtAttrCounters = 4 /* old 32bit counters, unused */
+
+	// CtaCountersPad - CTA_COUNTERS_PAD
+	CtaCountersPad CtAttrCounters = 5
+
+	// CtaCountersMax - CTA_COUNTERS_MAX
+	CtaCountersMax = CtaCountersPad
 )
