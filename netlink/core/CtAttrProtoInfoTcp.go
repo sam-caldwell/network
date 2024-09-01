869e949b5a0d21a324fc5efb52cb85dccf27eb97
Sam Caldwell <github@samcaldwell.net>
2024-09-01 12:08:37 -0500

    code cleanup and comments

---
+++ netlink/core/CtAttrProtoInfoTcp.go
@@ -0,0 +1,33 @@
+package core
+
+// CtAttrProtoInfoTcp (ctattr_protoinfo_tcp)
+//
+// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/netfilter/nfnetlink_conntrack.h
+type CtAttrProtoInfoTcp uint8
+
+const (
+	// CtaProtoinfoTcpUnspec - CTA_PROTOINFO_TCP_UNSPEC - indicates that specific TCP protocol information is
+	// unspecified or not applicable in connection tracking.
+	CtaProtoinfoTcpUnspec = 0
+
+	// CtaProtoinfoTcpState - CTA_PROTOINFO_TCP_STATE - represent the state of a TCP connection within the
+	// connection tracking system.
+	CtaProtoinfoTcpState = 1
+
+	// CtaProtoinfoTcpWscaleOriginal - CTA_PROTOINFO_TCP_WSCALE_ORIGINAL - represent the original window scale factor
+	// of a TCP connection as it was negotiated during the connection establishment phase, within the connection tracking system.
+	CtaProtoinfoTcpWscaleOriginal = 2
+
+	// CtaProtoinfoTcpWscaleReply - CTA_PROTOINFO_TCP_WSCALE_REPLY - represent the window scale factor in the reply
+	// direction of a TCP connection, as it was negotiated during the connection establishment phase, within the
+	// connection tracking system.
+	CtaProtoinfoTcpWscaleReply = 3
+
+	// CtaProtoinfoTcpFlagsOriginal - CTA_PROTOINFO_TCP_FLAGS_ORIGINAL - represent the original TCP flags (such as
+	// SYN, ACK, etc.) that were set in the initial packet of a TCP connection, within the connection tracking system.
+	CtaProtoinfoTcpFlagsOriginal = 4
+
+	// CtaProtoinfoTcpFlagsReply - CTA_PROTOINFO_TCP_FLAGS_REPLY - represent the TCP flags in the reply direction of
+	// a connection, as observed in the connection tracking system.
+	CtaProtoinfoTcpFlagsReply = 5
+)
