e92b0a7336e410ad003b06d58b84acbba9802a6c
Sam Caldwell <github@samcaldwell.net>
2024-09-01 12:51:44 -0500

    update documentation for CnMsg (cn_msg)

--- netlink/core/CnMsg.go
+++ netlink/core/CnMsg.go
@@ -1,6 +1,12 @@
 package core
 
-// CnMsg represents a connector message.
+// CnMsg - cn_msg - represents a connector message.
+//
+// Structure in the Linux kernel's connector subsystem, used for communication between user space and kernel space.
+// It encapsulates a message with a header (nlmsghdr) and data (cn_msg), allowing for various types of notifications
+// or commands to be sent between the kernel and user space processes.
+//
+// See https://github.com/torvalds/linux/blob/master/drivers/connector/cn_proc.c
 type CnMsg struct {
 	ID     CbID   // Contains Idx and Val fields to identify the message or its source/destination.
 	Seq    uint32 // Sequence number to track the order of messages.
