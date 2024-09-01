97949fcf7debeea1056e5d9fce996bf7388b497b
Sam Caldwell <github@samcaldwell.net>
2024-09-01 00:30:20 -0500

    Add CbID and test

---
+++ netlink/core/CbID.go
@@ -0,0 +1,7 @@
+package core
+
+// CbID - represents an identifier used in callback or tracking mechanisms.
+type CbID struct {
+	Idx uint32
+	Val uint32
+}
---
+++ netlink/core/CbID_test.go
@@ -0,0 +1,12 @@
+package core
+
+import (
+	"testing"
+)
+
+func TestCbID(t *testing.T) {
+	_ = CbID{
+		Idx: uint32(0),
+		Val: uint32(0),
+	}
+}
