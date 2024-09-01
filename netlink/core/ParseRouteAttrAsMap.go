a6c5acd066edc1fdbfb77ea51f423f50f24c426d
Sam Caldwell <github@samcaldwell.net>
2024-09-01 15:53:06 -0500

    ParseRouteAttrAsMap()

--- netlink/core/ParseRouteAttrAsMap.go
+++ netlink/core/ParseRouteAttrAsMap.go
@@ -1,6 +1,6 @@
 package core
 
-// ParseRouteAttrAsMap parses provided buffer that contains raw RtAttrs and returns a map of parsed
+// ParseRouteAttrAsMap - parses provided buffer that contains raw RtAttrs and returns a map of parsed
 // attributes indexed by attribute type or error if occurred.
 func ParseRouteAttrAsMap(b []byte) (map[uint16]NetlinkRouteAttr, error) {
 	attrMap := make(map[uint16]NetlinkRouteAttr)
