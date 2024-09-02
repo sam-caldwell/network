0d9b5078e42c70b1254fc656ed64749bcff34da7
Sam Caldwell <github@samcaldwell.net>
2024-09-01 21:17:50 -0500

    Add NetlinkRequest.Execute()

---
+++ netlink/core/NetlinkRequest.Execute.go
@@ -0,0 +1,15 @@
+package core
+
+// Execute - Execute the request against the given sockType. Returns a list of netlink messages in serialized format,
+// optionally filtered by resType.
+func (req *NetlinkRequest) Execute(sockType int, resType uint16) ([][]byte, error) {
+	var res [][]byte
+	err := req.ExecuteIter(sockType, resType, func(msg []byte) bool {
+		res = append(res, msg)
+		return true
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
