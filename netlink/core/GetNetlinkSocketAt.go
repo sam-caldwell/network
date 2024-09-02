79978751ff78ab9e129ecd51e9dab235830008bd
Sam Caldwell <github@samcaldwell.net>
2024-09-01 22:24:04 -0500

    add GetNetlinkSocketAt()

---
+++ netlink/core/GetNetlinkSocketAt.go
@@ -0,0 +1,26 @@
+//go:build linux
+
+package core
+
+import (
+	"github.com/sam-caldwell/network/namespace"
+)
+
+// GetNetlinkSocketAt - open netlink socket in the newNamespace, and positions the thread back into the network
+// namespace specified by currentNamespace, when done.
+//
+// If currentNamespace is closed, the function derives the current namespace and moves back into it when done.
+// If newNamespace is close, the socket will be opened in the current network namespace.
+func GetNetlinkSocketAt(newNamespace, currentNamespace namespace.Handle, protocol int) (*NetlinkSocket, error) {
+
+	c, err := executeInNetns(newNamespace, currentNamespace)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer c()
+
+	return getNetlinkSocket(protocol)
+
+}
