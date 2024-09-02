7e5c26776a7a988a353d8025c2d3041d567485e6
Sam Caldwell <github@samcaldwell.net>
2024-09-01 20:50:12 -0500

    add NetlinkRequest struct

---
+++ netlink/core/NetlinkRequest.go
@@ -0,0 +1,21 @@
+package core
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// NetlinkRequest - represents a request message that can be sent via netlink.
+type NetlinkRequest struct {
+
+	// unix.NlMsghdr - represents a request message that can be sent via netlink.
+	unix.NlMsghdr
+
+	// Data - The payload data to include in the message.
+	Data []NetlinkRequestData
+
+	// RawData - The sequence number for the request.
+	RawData []byte
+
+	// Sockets - Additional flags for the request.
+	Sockets map[int]*SocketHandle
+}
