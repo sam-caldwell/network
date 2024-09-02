12db36ecf42f52af84b20154f1a0905ef14efce4
Sam Caldwell <github@samcaldwell.net>
2024-09-01 23:56:00 -0500

    add NetlinkSocket.SetExtAck()

---
+++ netlink/core/NetlinkSocket.SetExtAck.go
@@ -0,0 +1,20 @@
+//go:build linux
+
+package core
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// SetExtAck - request error messages to be reported on the socket
+func (s *NetlinkSocket) SetExtAck(enable bool) error {
+
+	var n int
+
+	if enable {
+		n = 1
+	}
+
+	return unix.SetsockoptInt(int(s.fd), unix.SOL_NETLINK, unix.NETLINK_EXT_ACK, n)
+
+}
