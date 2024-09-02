77a5a2df85c02edc4515b73c78d4c559acff4f4d
Sam Caldwell <github@samcaldwell.net>
2024-09-01 20:39:58 -0500

    add NetlinkRequestData interface

---
+++ netlink/core/NetlinkRequestData.go
@@ -0,0 +1,8 @@
+package core
+
+// NetlinkRequestData - interface that abstracts the data included in a netlink request, allowing different types of
+// data to be encapsulated and sent as part of a netlink message.
+type NetlinkRequestData interface {
+	Len() int
+	Serialize() []byte
+}
