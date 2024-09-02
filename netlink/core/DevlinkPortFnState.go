869ef299dced75ac447497a878d82d1d2ae3f163
Sam Caldwell <github@samcaldwell.net>
2024-09-02 13:49:41 -0500

    add DevlinkPortFnState enum

---
+++ netlink/core/DevlinkPortFnState.go
@@ -0,0 +1,25 @@
+package core
+
+// DevlinkPortFnState - devlink_port_fn_state
+//
+// The devlink_port_fn_state enum is used to indicate the operational state of a function associated with a devlink
+// port. This could refer to a network function or virtual function on a physical port of a network device.
+//
+// It is useful in the management and configuration of network device ports, particularly in environments where
+// features like SR-IOV are used.
+//
+// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
+type DevlinkPortFnState uint8
+
+const (
+
+	// DevlinkPortFnStateInactive - DEVLINK_PORT_FN_STATE_INACTIVE - This state indicates that the function is
+	// currently active. An active function is operational and can participate in network activities such as sending
+	// or receiving packets.
+	DevlinkPortFnStateInactive DevlinkPortFnState = 0
+
+	// DevlinkPortFnStateActive - DEVLINK_PORT_FN_STATE_ACTIVE -
+	// This state indicates that the function is currently inactive. In this state, the function is not operational
+	// and does not participate in any network activity.
+	DevlinkPortFnStateActive DevlinkPortFnState = 1
+)
