477d9c5a93374cb9acf824268e292348a46a7b29
Sam Caldwell <github@samcaldwell.net>
2024-09-01 21:16:45 -0500

    fix typo

---
+++ netlink/core/NetlinkRequest.AddRawData.go
@@ -0,0 +1,6 @@
+package core
+
+// AddRawData - add raw bytes to the end of the NetlinkRequest object during serialization
+func (req *NetlinkRequest) AddRawData(data []byte) {
+	req.RawData = append(req.RawData, data...)
+}
