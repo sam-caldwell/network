78795b2c57776a48934609ce3a467ccac198fc55
Sam Caldwell <github@samcaldwell.net>
2024-09-01 23:45:39 -0500

    add NetlinkSocket.Send()

---
+++ netlink/core/NetlinkSocket.Send.go
@@ -0,0 +1,37 @@
+//go:build linux
+
+package core
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"sync/atomic"
+)
+
+// Send - send a given NetlinkRequest via the current socket file descriptor to netlink
+func (s *NetlinkSocket) Send(request *NetlinkRequest) (err error) {
+
+	if request == nil {
+		return fmt.Errorf("nil request")
+	}
+
+	socketFd := int(atomic.LoadInt32(&s.fd))
+
+	if socketFd < 0 {
+
+		return fmt.Errorf("send called on a closed socket")
+
+	}
+
+	var requestBytes []byte
+	if requestBytes, err = request.Serialize(); err != nil {
+		return err
+	}
+
+	if err = unix.Sendto(socketFd, requestBytes, 0, &s.lsa); err != nil {
+		return err
+	}
+
+	return err
+
+}
