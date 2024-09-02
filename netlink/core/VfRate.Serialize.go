14f19207c8a10872258a162ef67b5295a1da58f5
Sam Caldwell <github@samcaldwell.net>
2024-09-02 11:54:19 -0500

    add VfRate and its methods along with DeserializeVfRate()

--- netlink/core/VfRate.Serialize.go
+++ netlink/core/VfRate.Serialize.go
@@ -7,9 +7,13 @@ import (
 
 // Serialize - convert the VfRate struct into a byte slice, which can be used in netlink messages.
 func (v *VfRate) Serialize() ([]byte, error) {
+
 	buf := new(bytes.Buffer)
+
 	if err := binary.Write(buf, binary.LittleEndian, v); err != nil {
 		return nil, err
 	}
+
 	return buf.Bytes(), nil
+
 }
