639999df769b3624ffdb9c9376f0629ac1a24fe0
Sam Caldwell <github@samcaldwell.net>
2024-09-01 23:02:11 -0500

    add NetlinkSocket.SetSendTimeout()

---
+++ netlink/core/NetlinkSocket.SetSendTimeout.go
@@ -0,0 +1,16 @@
+//go:build linux
+
+package core
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// SetSendTimeout - set send timeout on the socket
+//
+// Set send timeout of SOCKET_SEND_TIMEOUT, this will allow the Send to periodically unblock and avoid some routine
+// from hanging on send on a closed fd.
+func (s *NetlinkSocket) SetSendTimeout(timeout *unix.Timeval) error {
+
+	return unix.SetsockoptTimeval(int(s.fd), unix.SOL_SOCKET, unix.SO_SNDTIMEO, timeout)
+}
