675e7069edfe02d6cfe26037a2d6c7600dfdc0a6
Sam Caldwell <github@samcaldwell.net>
2024-09-02 13:40:09 -0500

    add DevlinkResourceUnit type and enum values

---
+++ netlink/core/DevlinkResourceUnit.go
@@ -0,0 +1,17 @@
+package core
+
+// DevlinkResourceUnit - devlink_resource_unit - represents the devlink resource unit type.
+//
+// The devlink_resource_unit is typically used to define units of resources for a particular network device.
+//
+// In the context of a switch, suppose you want to manage the number of routing table entries. The
+// devlink_resource_unit might define the unit of these entries, so the user can query and configure
+// the device to allocate or deallocate entries in a meaningful way.
+//
+// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
+type DevlinkResourceUnit uint8
+
+const (
+	// DevlinkResourceUnitEntry represents the DEVLINK_RESOURCE_UNIT_ENTRY.
+	DevlinkResourceUnitEntry DevlinkResourceUnit = 0
+)
