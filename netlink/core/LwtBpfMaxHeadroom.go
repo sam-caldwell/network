aa6e9545d41e2dafe05f7c3c43c4beda501af456
Sam Caldwell <github@samcaldwell.net>
2024-09-01 20:37:11 -0500

    add LwtBpfMaxHeadroom

---
+++ netlink/core/LwtBpfMaxHeadroom.go
@@ -0,0 +1,9 @@
+package core
+
+const (
+	// LwtBpfMaxHeadroom - LWT_BPF_MAX_HEADROOM - constant in the Linux kernel's networking subsystem, specifically
+	// in the context of Lightweight Tunnels (LWT) and BPF (Berkeley Packet Filter) programs. This constant defines
+	// the maximum amount of headroom, in bytes, that can be reserved by a BPF program when it is attached to a
+	// lightweight tunnel for packet processing.
+	LwtBpfMaxHeadroom = 256
+)
