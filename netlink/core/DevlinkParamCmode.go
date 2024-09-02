d0297caf2e771e88f8f47ce1643d9609eb03dc1a
Sam Caldwell <github@samcaldwell.net>
2024-09-02 13:23:49 -0500

    make devlink parameters strongly typed (WIP)

--- netlink/core/DevlinkParamCmode.go
+++ netlink/core/DevlinkParamCmode.go
@@ -7,21 +7,21 @@ type DevlinkParamCmode uint8
 const (
 	// DevlinkParamCmodeRuntime - DEVLINK_PARAM_CMODE_RUNTIME
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkParamCmodeRuntime = 0
+	DevlinkParamCmodeRuntime DevlinkParamCmode = 0
 
 	// DevlinkParamCmodeDriverinit - DEVLINK_PARAM_CMODE_DRIVERINIT
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkParamCmodeDriverinit = 1
+	DevlinkParamCmodeDriverinit DevlinkParamCmode = 1
 
 	// DevlinkParamCmodePermanent - DEVLINK_PARAM_CMODE_PERMANENT
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkParamCmodePermanent = 2
+	DevlinkParamCmodePermanent DevlinkParamCmode = 2
 
 	/* Add new configuration modes above */
 
 	// __devlinkParamCmodeMax - __DEVLINK_PARAM_CMODE_MAX
-	__devlinkParamCmodeMax = 3
+	__devlinkParamCmodeMax DevlinkParamCmode = 3
 
 	// DevlinkParamCmodeMax - DEVLINK_PARAM_CMODE_MAX
-	DevlinkParamCmodeMax = __devlinkParamCmodeMax - 1
+	DevlinkParamCmodeMax DevlinkParamCmode = __devlinkParamCmodeMax - 1
 )
--- netlink/core/DevlinkParamType.go
+++ netlink/core/DevlinkParamType.go
@@ -5,17 +5,17 @@ type DevlinkParamType uint8
 
 const (
 	// DevlinkParamTypeU8 - DEVLINK_PARAM_TYPE_U8 - uint8
-	DevlinkParamTypeU8 = 1
+	DevlinkParamTypeU8 DevlinkParamType = 1
 
 	// DevlinkParamTypeU16 - DEVLINK_PARAM_TYPE_U16 - uint16
-	DevlinkParamTypeU16 = 2
+	DevlinkParamTypeU16 DevlinkParamType = 2
 
 	// DevlinkParamTypeU32 - DEVLINK_PARAM_TYPE_U32 - uint32
-	DevlinkParamTypeU32 = 3
+	DevlinkParamTypeU32 DevlinkParamType = 3
 
 	// DevlinkParamTypeString - DEVLINK_PARAM_TYPE_STRING - string
-	DevlinkParamTypeString = 5
+	DevlinkParamTypeString DevlinkParamType = 5
 
 	// DevlinkParamTypeBool - DEVLINK_PARAM_TYPE_BOOL - bool
-	DevlinkParamTypeBool = 6
+	DevlinkParamTypeBool DevlinkParamType = 6
 )
