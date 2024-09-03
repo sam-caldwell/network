e17bbfa1cf1f976a8349f1aa1c2a243cc76f6ad7
Sam Caldwell <github@samcaldwell.net>
2024-09-03 12:44:59 -0500

    add error handling to IfaCacheInfo.Deserialize()

--- netlink/core/IfaCacheInfo.Deserialize.go
+++ netlink/core/IfaCacheInfo.Deserialize.go
@@ -1,6 +1,10 @@
 package core
 
 // DeserializeIfaCacheInfo - deserialize the IfaCacheInfo from a byte slice
-func (msg *IfaCacheInfo) DeserializeIfaCacheInfo(b []byte) {
-	*msg = *DeserializeIfaCacheInfo(b)
+func (msg *IfaCacheInfo) DeserializeIfaCacheInfo(b []byte) error {
+	info, err := DeserializeIfaCacheInfo(b)
+	if err == nil {
+		*msg = *info
+	}
+	return err
 }
