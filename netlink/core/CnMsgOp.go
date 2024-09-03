b722027edd96839c7cbb522a271e8e38ffd28812
Sam Caldwell <github@samcaldwell.net>
2024-09-02 23:49:57 -0500

    update documentation

--- netlink/core/CnMsgOp.go
+++ netlink/core/CnMsgOp.go
@@ -2,6 +2,10 @@ package core
 
 // CnMsgOp - represents connector message operation, combining message identification, sequence, acknowledgment,
 // and operational flags with an additional operation-specific field.
+//
+// Note that this EXTENDS cn_msg found in the Linux source tree and adds an Op field.
+//
+// https://github.com/torvalds/linux/blob/master/include/linux/connector.h
 type CnMsgOp struct {
 	CnMsg
 	// here we differ from the C header
