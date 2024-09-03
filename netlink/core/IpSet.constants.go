88b79005ad6a1845406d528f46b003a461340426
Sam Caldwell <github@samcaldwell.net>
2024-09-03 13:55:11 -0500

    add IpSet constants

---
+++ netlink/core/IpSet.constants.go
@@ -0,0 +1,15 @@
+package core
+
+const (
+	// IpsetProtocol - IPSET_PROTOCOL - The protocol version
+	IpsetProtocol = 7
+
+	// IpsetProtocolMin - IPSET_PROTOCOL_MIN -  The protocol version minimum
+	IpsetProtocolMin = 6
+
+	// IpsetMaxnamelen - IPSET_MAXNAMELEN - Max length of strings including NUL: set and type identifiers
+	IpsetMaxnamelen = 32
+
+	// IpsetMaxCommentSize - IPSET_MAX_COMMENT_SIZE - Maximum permissible comment length we will accept over netlink
+	IpsetMaxCommentSize = 255
+)
