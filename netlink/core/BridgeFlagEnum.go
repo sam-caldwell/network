8fceccab65903a835b2ee81924fadfc33b3ad991
Sam Caldwell <github@samcaldwell.net>
2024-09-02 23:34:44 -0500

    fix formatting and rename BridgeFlagEnum

---
+++ netlink/core/BridgeFlagEnum.go
@@ -0,0 +1,18 @@
+package core
+
+// BridgeFlagEnum - Bridge Flags
+//
+// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
+type BridgeFlagEnum int
+
+const (
+	//BridgeFlagsMaster -BRIDGE_FLAGS_MASTER - Bridge command to/from master
+	//
+	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
+	BridgeFlagsMaster BridgeFlagEnum = 1
+
+	// BridgeFlagsSelf - BRIDGE_FLAGS_SELF - Bridge command to/from lowerdev
+	//
+	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
+	BridgeFlagsSelf BridgeFlagEnum = 2
+)
