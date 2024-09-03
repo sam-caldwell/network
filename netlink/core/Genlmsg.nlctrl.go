4244d44d4fb37529632269d174835df1e41bd815
Sam Caldwell <github@samcaldwell.net>
2024-09-03 10:51:11 -0500

    add Genlmsg.nlctrl

---
+++ netlink/core/Genlmsg.nlctrl.go
@@ -0,0 +1,12 @@
+package core
+
+// Family nlctrl netlink specification
+// See https://docs.kernel.org/networking/netlink_spec/nlctrl.html
+const (
+	// GenlCtrlName - https://docs.kernel.org/networking/netlink_spec/nlctrl.html
+	GenlCtrlName = "nlctrl"
+	// GenlCtrlVersion - https://docs.kernel.org/networking/netlink_spec/nlctrl.html
+	GenlCtrlVersion = 2
+	// GenlCtrlCmdGetFamily - https://docs.kernel.org/networking/netlink_spec/nlctrl.html
+	GenlCtrlCmdGetFamily = 3
+)
