d4d3bf0bc8d3ebcb7f060677289b26b1de04aeaa
Sam Caldwell <github@samcaldwell.net>
2024-09-03 12:39:48 -0500

    add NlaFlags

--- netlink/core/NlaFlags.go
+++ netlink/core/NlaFlags.go
@@ -1,18 +1,20 @@
-//go:build linux
-
 package core
+
+import "unsafe"
+
+type NlaFlags uint16
 
 const (
 
 	// NlaFNested - NLA_F_NESTED (1 << 15)
-	NlaFNested uint16 = 32768
+	NlaFNested NlaFlags = 32768
 
 	// NlaFNetByteorder - NLA_F_NET_BYTEORDER (1 << 14)
-	NlaFNetByteorder uint16 = 16384
+	NlaFNetByteorder NlaFlags = 16384
 
 	// NlaTypeMask - NLA_TYPE_MASK - Mask bits for the NLA type
 	NlaTypeMask = ^(NlaFNested | NlaFNetByteorder)
 
 	// NlaAlignto - NLA_ALIGNTO 4
-	NlaAlignto uint16 = 4
+	NlaAlignto NlaFlags = NlaFlags(unsafe.Sizeof(NlaTypeMask) * 2)
 )
