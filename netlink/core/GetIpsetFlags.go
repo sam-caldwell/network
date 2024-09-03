0d424d3ffb56ea250375f454f97085dcca13473f
Sam Caldwell <github@samcaldwell.net>
2024-09-03 13:57:16 -0500

    add GetIpsetFlags()

---
+++ netlink/core/GetIpsetFlags.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// GetIpsetFlags - return the int version of the IpSet Flags
+func GetIpsetFlags(cmd IpSetCmdEnum) int {
+	switch cmd {
+	case IpsetCmdCreate:
+		return unix.NLM_F_REQUEST | unix.NLM_F_ACK | unix.NLM_F_CREATE
+	case IpsetCmdDestroy,
+		IpsetCmdFlush,
+		IpsetCmdRename,
+		IpsetCmdSwap,
+		IpsetCmdTest:
+		return unix.NLM_F_REQUEST | unix.NLM_F_ACK
+	case IpsetCmdList,
+		IpsetCmdSave:
+		return unix.NLM_F_REQUEST | unix.NLM_F_ACK | unix.NLM_F_ROOT | unix.NLM_F_MATCH | unix.NLM_F_DUMP
+	case IpsetCmdAdd,
+		IpsetCmdDel:
+		return unix.NLM_F_REQUEST | unix.NLM_F_ACK
+	case IpsetCmdHeader,
+		IpsetCmdType,
+		IpsetCmdProtocol:
+		return unix.NLM_F_REQUEST
+	default:
+		return 0
+	}
+}
