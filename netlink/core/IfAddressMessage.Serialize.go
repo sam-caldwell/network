fb25e4f02024879885853a8ad429c6f528cd9a79
Sam Caldwell <github@samcaldwell.net>
2024-09-03 12:53:23 -0500

    IfAddressMessage.Serialize() now has error handling and memory safety

--- netlink/core/IfAddressMessage.Serialize.go
+++ netlink/core/IfAddressMessage.Serialize.go
@@ -1,11 +1,31 @@
 package core
 
 import (
-	"golang.org/x/sys/unix"
-	"unsafe"
+	"bytes"
+	"encoding/binary"
 )
 
 // Serialize - serialize an interface address message as a byte slice.
-func (msg *IfAddressMessage) Serialize() []byte {
-	return (*(*[unix.SizeofIfAddrmsg]byte)(unsafe.Pointer(msg)))[:]
+func (msg *IfAddressMessage) Serialize() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	// Write each field to the buffer in the correct order and format
+	if err := binary.Write(buf, binary.LittleEndian, msg.Family); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Prefixlen); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Flags); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Scope); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Index); err != nil {
+		return nil, err
+	}
+
+	// Return the byte slice
+	return buf.Bytes(), nil
 }
