e3269a684c078321c54697204a2a152c1b81b6ec
Sam Caldwell <github@samcaldwell.net>
2024-09-03 12:47:00 -0500

    fix docs for IfaCachInfo

--- netlink/core/IfaCacheInfo.go
+++ netlink/core/IfaCacheInfo.go
@@ -4,15 +4,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// IfaCacheInfo -
-// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_addr.h
+// IfaCacheInfo - Wrapper around unix.IfaCacheinfo with additional functionality.
 //
-//	struct ifa_cacheinfo {
-//		__u32	ifa_prefered;
-//		__u32	ifa_valid;
-//		__u32	cstamp; /* created timestamp, hundredths of seconds */
-//		__u32	tstamp; /* updated timestamp, hundredths of seconds */
-//	};
+// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_addr.h
 type IfaCacheInfo struct {
 	unix.IfaCacheinfo
 }
