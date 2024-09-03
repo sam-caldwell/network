d1b7b9bcd88be85d6ebea9d82cbd37d920244021
Sam Caldwell <github@samcaldwell.net>
2024-09-03 09:59:30 -0500

    rename enum

--- netlink/core/DevlinkEswitchEncapModeEnum.go
+++ netlink/core/DevlinkEswitchEncapModeEnum.go
@@ -1,19 +1,19 @@
 package core
 
-// DevlinkEswitchEncapMode - devlink_eswitch_encap_mode
+// DevlinkEswitchEncapModeEnum - devlink_eswitch_encap_mode
 //
 // See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-type DevlinkEswitchEncapMode uint8
+type DevlinkEswitchEncapModeEnum uint8
 
 const (
 
 	// DevlinkEswitchEncapModeNone - DEVLINK_ESWITCH_ENCAP_MODE_NONE
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkEswitchEncapModeNone DevlinkEswitchEncapMode = 0
+	DevlinkEswitchEncapModeNone DevlinkEswitchEncapModeEnum = 0
 
 	// DevlinkEswitchEncapModeBasic - DEVLINK_ESWITCH_ENCAP_MODE_BASIC
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkEswitchEncapModeBasic DevlinkEswitchEncapMode = 1
+	DevlinkEswitchEncapModeBasic DevlinkEswitchEncapModeEnum = 1
 )
