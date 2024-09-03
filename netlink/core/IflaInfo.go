61e586f7a8caa210ae28b7e9c6514ef0b03c9d28
Sam Caldwell <github@samcaldwell.net>
2024-09-02 20:19:16 -0500

    Add IflaInfo enum and values

---
+++ netlink/core/IflaInfo.go
@@ -0,0 +1,51 @@
+package core
+
+// IflaInfo - Enumeration for interface info attributes in Linux kernel
+//
+// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+type IflaInfo uint8
+
+const (
+
+	// IflaInfoUnspec - IFLA_INFO_UNSPEC - This is an unspecified placeholder value used in the enumeration.
+	// It represents an undefined or unused attribute within the context of interface information.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoUnspec IflaInfo = 0
+
+	// IflaInfoKind - IFLA_INFO_KIND - This attribute represents the kind of the network device.
+	// For example, it can indicate if the device is a VLAN, bridge, or another type of network interface.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoKind IflaInfo = 1
+
+	// IflaInfoData - IFLA_INFO_DATA - This attribute contains the data specific to the type of network interface
+	// specified by IFLA_INFO_KIND. It could include various parameters that are relevant to the network device.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoData IflaInfo = 2
+
+	// IflaInfoXstats - IFLA_INFO_XSTATS - This attribute is used to represent extended statistics for the network
+	// interface. It provides additional statistical data beyond the basic interface statistics.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoXstats IflaInfo = 3
+
+	// IflaInfoSlaveKind - IFLA_INFO_SLAVE_KIND - This attribute represents the kind of a slave interface.
+	// Slave interfaces are typically part of a bonding or bridging setup.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoSlaveKind IflaInfo = 4
+
+	// IflaInfoSlaveData - IFLA_INFO_SLAVE_DATA - This attribute contains the data specific to the slave interface,
+	// similar to how IFLA_INFO_DATA contains data for the primary interface.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoSlaveData IflaInfo = 5
+
+	// IflaInfoMax - IFLA_INFO_MAX - This constant represents the maximum valid value for the IflaInfo enumeration.
+	// It is often used as a boundary marker for validation purposes.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaInfoMax = iota - 1
+)
