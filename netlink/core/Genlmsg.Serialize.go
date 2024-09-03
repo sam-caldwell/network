e5dc461099f31466aefd59d7ba041e1edaca2274
Sam Caldwell <github@samcaldwell.net>
2024-09-03 12:42:43 -0500

    format fixes

--- netlink/core/Genlmsg.Serialize.go
+++ netlink/core/Genlmsg.Serialize.go
@@ -7,12 +7,16 @@ import (
 
 // Serialize converts the Genlmsg structure into a byte slice.
 func (msg *Genlmsg) Serialize() ([]byte, error) {
+
 	buf := new(bytes.Buffer)
 	if err := binary.Write(buf, binary.LittleEndian, msg.Command); err != nil {
 		return nil, err
 	}
+
 	if err := binary.Write(buf, binary.LittleEndian, msg.Version); err != nil {
 		return nil, err
 	}
+
 	return buf.Bytes(), nil
+
 }
--- netlink/core/Genlmsg.nlctrl.go
+++ netlink/core/Genlmsg.nlctrl.go
@@ -3,10 +3,13 @@ package core
 // Family nlctrl netlink specification
 // See https://docs.kernel.org/networking/netlink_spec/nlctrl.html
 const (
+
 	// GenlCtrlName - https://docs.kernel.org/networking/netlink_spec/nlctrl.html
 	GenlCtrlName = "nlctrl"
+
 	// GenlCtrlVersion - https://docs.kernel.org/networking/netlink_spec/nlctrl.html
 	GenlCtrlVersion = 2
+
 	// GenlCtrlCmdGetFamily - https://docs.kernel.org/networking/netlink_spec/nlctrl.html
 	GenlCtrlCmdGetFamily = 3
 )
