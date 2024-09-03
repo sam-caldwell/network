35a7177e7bb208d91acf368d4237a7e27c09384e
Sam Caldwell <github@samcaldwell.net>
2024-09-03 13:02:47 -0500

    rename IfLaExtMaskEnum

--- netlink/core/IfLaExtMaskEnum.go
+++ netlink/core/IfLaExtMaskEnum.go
@@ -1,32 +1,32 @@
 package core
 
-// IfLaExtMask - New extended info filters for IFLA_EXT_MASK
+// IfLaExtMaskEnum - New extended info filters for IFLA_EXT_MASK
 // https://github.com/torvalds/linux/blob/master/include/uapi/linux/rtnetlink.h
-type IfLaExtMask uint8
+type IfLaExtMaskEnum uint8
 
 const (
 
 	// RtextFilterVf - RTEXT_FILTER_VF
-	RtextFilterVf IfLaExtMask = 1
+	RtextFilterVf IfLaExtMaskEnum = 1
 
 	// RtextFilterBrVlan - RTEXT_FILTER_BRVLAN
-	RtextFilterBrVlan IfLaExtMask = 2
+	RtextFilterBrVlan IfLaExtMaskEnum = 2
 
 	// RtextFilterBrVlanCompressed - RTEXT_FILTER_BRVLAN_COMPRESSED
-	RtextFilterBrVlanCompressed IfLaExtMask = 4
+	RtextFilterBrVlanCompressed IfLaExtMaskEnum = 4
 
 	// RtextFilterSkipStats - RTEXT_FILTER_SKIP_STATS
-	RtextFilterSkipStats IfLaExtMask = 8
+	RtextFilterSkipStats IfLaExtMaskEnum = 8
 
 	// RtextFilterMrp - RTEXT_FILTER_MRP
-	RtextFilterMrp IfLaExtMask = 16
+	RtextFilterMrp IfLaExtMaskEnum = 16
 
 	// RtextFilterCfmConfig - RTEXT_FILTER_CFM_CONFIG
-	RtextFilterCfmConfig IfLaExtMask = 32
+	RtextFilterCfmConfig IfLaExtMaskEnum = 32
 
 	// RtextFilterCfmStatus - RTEXT_FILTER_CFM_STATUS
-	RtextFilterCfmStatus IfLaExtMask = 64
+	RtextFilterCfmStatus IfLaExtMaskEnum = 64
 
 	// RtextFilterMst - RTEXT_FILTER_MST
-	RtextFilterMst IfLaExtMask = 128
+	RtextFilterMst IfLaExtMaskEnum = 128
 )
