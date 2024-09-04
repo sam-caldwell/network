3fa70487dc6aaa0110ace4bc622b930ed18205f2
Sam Caldwell <github@samcaldwell.net>
2024-09-03 23:50:56 -0500

    add SEG6EncapModeString()

--- netlink/core/SEG6EncapModeString.go
+++ netlink/core/SEG6EncapModeString.go
@@ -9,11 +9,17 @@ package core
 // Returns:
 //   - string: A string describing the mode ("inline", "encap", or "unknown").
 func SEG6EncapModeString(mode Seg6IptunMode) string {
+
 	switch mode {
+
 	case Seg6IptunModeInline:
 		return "inline"
+
 	case Seg6IptunModeEncap:
 		return "encap"
+
 	}
+
 	return "unknown"
+
 }
