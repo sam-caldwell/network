4d473525d0e5bf4b051d81097d9d981c26d234c7
Sam Caldwell <github@samcaldwell.net>
2024-09-03 23:28:40 -0500

    Add RtGenMsg.Serialize()

---
+++ netlink/core/RtGenMsg.Serialize.go
@@ -0,0 +1,12 @@
+package core
+
+// Serialize - return a byte slice containing the serialized contents of the RtGenMsg struct.
+func (msg *RtGenMsg) Serialize() []byte {
+
+	out := make([]byte, msg.Len())
+
+	out[0] = msg.Family
+
+	return out
+
+}
