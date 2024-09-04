6faef302e3bc2f47a4bdf3e775f1e364d30b00f4
Sam Caldwell <github@samcaldwell.net>
2024-09-04 15:15:49 -0500

    TcHtbCopt

--- netlink/core/DeserializeTcHtbCopt.go
+++ netlink/core/DeserializeTcHtbCopt.go
@@ -1,7 +1,42 @@
 package core
 
-import "unsafe"
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+)
 
-func DeserializeTcHtbCopt(b []byte) *TcHtbCopt {
-	return (*TcHtbCopt)(unsafe.Pointer(&b[0:SizeofTcHtbCopt][0]))
+// DeserializeTcHtbCopt safely deserializes a byte slice into a TcHtbCopt structure.
+func DeserializeTcHtbCopt(b []byte) (*TcHtbCopt, error) {
+	if len(b) < SizeofTcHtbCopt {
+		return nil, errors.New("byte slice too short to deserialize TcHtbCopt")
+	}
+
+	msg := &TcHtbCopt{}
+	reader := bytes.NewReader(b)
+
+	// Deserialize fields using binary.Read
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Rate); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Ceil); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Buffer); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Cbuffer); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Quantum); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Level); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &msg.Prio); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
 }
