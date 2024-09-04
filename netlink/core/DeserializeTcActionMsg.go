98ce18bef52eae74dfb8de9ef1fc4633d552da2d
Sam Caldwell <github@samcaldwell.net>
2024-09-04 12:56:30 -0500

    add DeserializeTcActionMsg()

---
+++ netlink/core/DeserializeTcActionMsg.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
+
+// DeserializeTcActionMsg safely deserializes a byte slice into a TcActionMsg object.
+func DeserializeTcActionMsg(b []byte) *TcActionMsg {
+	if len(b) < SizeofTcActionMsg {
+		log.Fatalf("Byte slice too short to deserialize TcActionMsg")
+	}
+
+	msg := &TcActionMsg{}
+	buf := bytes.NewReader(b[:SizeofTcActionMsg])
+	err := binary.Read(buf, binary.LittleEndian, msg)
+	if err != nil {
+		log.Fatalf("Failed to deserialize TcActionMsg: %v", err)
+	}
+
+	return msg
+}
