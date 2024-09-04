d20b63dac7bbdb6048e0c26a3556fbe8d5ed3693
Sam Caldwell <github@samcaldwell.net>
2024-09-03 21:30:34 -0500

    add PrintAttributes()

--- netlink/core/PrintAttributes.go
+++ netlink/core/PrintAttributes.go
@@ -2,19 +2,30 @@ package core
 
 import "fmt"
 
-func PrintAttributes(data []byte) {
-	printAttributes(data, 0)
+// PrintAttributes - Print the netlink attributes contained in the input byte slice
+func PrintAttributes(rawAttributes []byte) {
+
+	printAttributes(rawAttributes, 0)
+
 }
 
-func printAttributes(data []byte, level int) {
-	for attr := range ParseAttributes(data) {
-		for i := 0; i < level; i++ {
+// printAttributes - Given a byte slice and starting levels to print, write the attributes to stdout
+func printAttributes(rawAttributes []byte, startingLevel int) {
+
+	for attr := range ParseAttributes(rawAttributes) {
+
+		for i := 0; i < startingLevel; i++ {
 			print("> ")
 		}
+
 		nested := attr.Type&NlaFNested != 0
+
 		fmt.Printf("type=%d nested=%v len=%v %v\n", attr.Type&NlaTypeMask, nested, len(attr.Value), attr.Value)
+
 		if nested {
-			printAttributes(attr.Value, level+1)
+			printAttributes(attr.Value, startingLevel+1)
 		}
+
 	}
+
 }
