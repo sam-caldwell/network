5f81e9fa84f1b6c436f9c2ce03c7d03fd1342cf9
Sam Caldwell <github@samcaldwell.net>
2024-09-03 22:23:48 -0500

    add IfaCacheInfo.Serialize()

--- netlink/core/IfaCacheInfo.Serialize.go
+++ netlink/core/IfaCacheInfo.Serialize.go
@@ -4,20 +4,21 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// Serialize - serialize IfaCacheInfo to byte slice
-//
-//	struct ifa_cacheinfo {
-//		__u32	ifa_prefered;
-//		__u32	ifa_valid;
-//		__u32	cstamp; /* created timestamp, hundredths of seconds */
-//		__u32	tstamp; /* updated timestamp, hundredths of seconds */
-//	};
+// Serialize - ifa_cacheinfo - serialize IfaCacheInfo to byte slice
 func (msg *IfaCacheInfo) Serialize() []byte {
+
 	length := unix.SizeofIfaCacheinfo
+
 	b := make([]byte, length)
-	nativeEndian.PutUint32(b[0:4], msg.Prefered)
-	nativeEndian.PutUint32(b[4:8], msg.Valid)
-	nativeEndian.PutUint32(b[8:12], msg.Cstamp)
-	nativeEndian.PutUint32(b[12:16], msg.Tstamp)
+
+	NativeEndian.PutUint32(b[0:4], msg.Prefered)
+
+	NativeEndian.PutUint32(b[4:8], msg.Valid)
+
+	NativeEndian.PutUint32(b[8:12], msg.Cstamp)
+
+	NativeEndian.PutUint32(b[12:16], msg.Tstamp)
+
 	return b
+
 }
