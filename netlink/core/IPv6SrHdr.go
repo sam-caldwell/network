04066527b606de819c5e2f2df3b9e497877671d7
Sam Caldwell <github@samcaldwell.net>
2024-09-03 23:35:36 -0500

    add IPv6SrHdr struct

--- netlink/core/IPv6SrHdr.go
+++ netlink/core/IPv6SrHdr.go
@@ -4,14 +4,23 @@ import (
 	"net"
 )
 
+// IPv6SrHdr - Represents an IPv6 Segment Routing Header (SRH)
+//
+// The SRH is an IPv6 extension header used for source routing. It allows the sender to specify a list of intermediate
+// nodes (segments) that the packet must traverse before reaching its final destination.
+//
+// This structure includes fields such as the next header, routing type, flags, and an array of segments representing
+// the intermediate nodes.
+//
+// See: https://github.com/torvalds/linux/blob/master/include/uapi/linux/seg6.h
 type IPv6SrHdr struct {
-	nextHdr      uint8
-	hdrLen       uint8
-	routingType  uint8
-	segmentsLeft uint8
-	firstSegment uint8
-	flags        uint8
-	reserved     uint16
+	nextHdr      uint8  // Next header type (e.g., TCP, UDP, etc.)
+	hdrLen       uint8  // Length of the SRH
+	routingType  uint8  // Routing type for Segment Routing
+	segmentsLeft uint8  // Number of segments left to visit
+	firstSegment uint8  // Index of the first segment
+	flags        uint8  // Flags (e.g., O-bit)
+	reserved     uint16 // Reserved field for future use
 
-	Segments []net.IP
+	Segments []net.IP // List of segments (intermediate node IP addresses)
 }
