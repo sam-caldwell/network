277a750d43956bcca05deee1e7732d0a17270b92
Sam Caldwell <github@samcaldwell.net>
2024-09-03 22:44:09 -0500

    Add DeserializeRtMsg()

---
+++ netlink/core/DeserializeRtMsg.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// DeserializeRtMsg - Converts a byte slice into an RtMsg struct.
+//
+// This function manually parses the byte slice to safely construct
+// the RtMsg struct, avoiding unsafe pointer usage.
+//
+// See: https://github.com/torvalds/linux/blob/master/include/uapi/linux/rtnetlink.h
+func DeserializeRtMsg(b []byte) *RtMsg {
+	if len(b) < unix.SizeofRtMsg {
+		return nil // Error handling for insufficient byte length
+	}
+
+	return &RtMsg{
+		RtMsg: unix.RtMsg{
+			Family:   b[0],
+			Dst_len:  b[1],
+			Src_len:  b[2],
+			Tos:      b[3],
+			Table:    b[4],
+			Protocol: b[5],
+			Scope:    b[6],
+			Type:     b[7],
+			Flags:    uint32(b[8]) | uint32(b[9])<<8 | uint32(b[10])<<16 | uint32(b[11])<<24,
+		},
+	}
+}
