f6d90736a4c60298d6bd1491053d0f59eeee504c
Sam Caldwell <github@samcaldwell.net>
2024-09-03 22:21:05 -0500

    update VfStats.Serialize()

--- netlink/core/VfStats.Serialize.go
+++ netlink/core/VfStats.Serialize.go
@@ -15,16 +15,16 @@ func (stats *VfStats) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 
 	// Helper function to create and write an RtAttr to the buffer
-	writeStat := func(attrType uint16, value uint64) error {
+	writeStat := func(attrType IflaVfStatsEnum, value uint64) error {
 		attr := RtAttr{
 			RtAttr: unix.RtAttr{
-				Type: attrType,
+				Type: uint16(attrType),
 				Len:  uint16(binary.Size(value) + unix.SizeofRtAttr),
 			},
 			Data: make([]byte, 8),
 		}
 		binary.LittleEndian.PutUint64(attr.Data, value)
-		if err := binary.Write(buf, nativeEndian, &attr.RtAttr); err != nil {
+		if err := binary.Write(buf, NativeEndian, &attr.RtAttr); err != nil {
 			return err
 		}
 		if _, err := buf.Write(attr.Data); err != nil {
@@ -34,28 +34,28 @@ func (stats *VfStats) Serialize() ([]byte, error) {
 	}
 
 	// Serialize each VfStats field into an RtAttr and write to the buffer
-	if err := writeStat(IFLA_VF_STATS_RX_PACKETS, stats.RxPackets); err != nil {
+	if err := writeStat(IflaVfStatsRxPackets, stats.RxPackets); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_TX_PACKETS, stats.TxPackets); err != nil {
+	if err := writeStat(IflaVfStatsTxPackets, stats.TxPackets); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_RX_BYTES, stats.RxBytes); err != nil {
+	if err := writeStat(IflaVfStatsRxBytes, stats.RxBytes); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_TX_BYTES, stats.TxBytes); err != nil {
+	if err := writeStat(IflaVfStatsTxBytes, stats.TxBytes); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_MULTICAST, stats.Multicast); err != nil {
+	if err := writeStat(IflaVfStatsMulticast, stats.Multicast); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_BROADCAST, stats.Broadcast); err != nil {
+	if err := writeStat(IflaVfStatsBroadcast, stats.Broadcast); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_RX_DROPPED, stats.RxDropped); err != nil {
+	if err := writeStat(IflaVfStatsRxDropped, stats.RxDropped); err != nil {
 		return nil, err
 	}
-	if err := writeStat(IFLA_VF_STATS_TX_DROPPED, stats.TxDropped); err != nil {
+	if err := writeStat(IflaVfStatsTxDropped, stats.TxDropped); err != nil {
 		return nil, err
 	}
 
