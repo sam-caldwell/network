f846b17f9d503d7fe0f3467a49201dfb5bccf38b
Sam Caldwell <github@samcaldwell.net>
2024-09-04 21:30:59 -0500

    add TcConnmark

--- netlink/core/TcConnmark.Serialize.go
+++ netlink/core/TcConnmark.Serialize.go
@@ -10,14 +10,26 @@ import (
 // It avoids using unsafe.Pointer by manually converting each field to bytes.
 //
 // Returns a serialized byte slice.
-func (msg *TcConnmark) Serialize() []byte {
+func (msg *TcConnmark) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.LittleEndian, msg.Index)   // TcGen: Index
-	binary.Write(buf, binary.LittleEndian, msg.Capab)   // TcGen: Capab
-	binary.Write(buf, binary.LittleEndian, msg.Action)  // TcGen: Action
-	binary.Write(buf, binary.LittleEndian, msg.Refcnt)  // TcGen: Refcnt
-	binary.Write(buf, binary.LittleEndian, msg.Bindcnt) // TcGen: Bindcnt
-	binary.Write(buf, binary.LittleEndian, msg.Zone)    // TcConnmark: Zone
+	if err := binary.Write(buf, binary.LittleEndian, msg.Index); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Capab); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Action); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Refcnt); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Bindcnt); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.LittleEndian, msg.Zone); err != nil {
+		return nil, err
+	}
 
-	return buf.Bytes()
+	return buf.Bytes(), nil
 }
