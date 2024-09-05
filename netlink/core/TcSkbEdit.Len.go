a9f678100e76fd39bcca155b2a545d6dc3e20f21
Sam Caldwell <github@samcaldwell.net>
2024-09-04 23:16:47 -0500

    add TcSkbEdit.Len()

--- netlink/core/TcSkbEdit.Len.go
+++ netlink/core/TcSkbEdit.Len.go
@@ -7,9 +7,6 @@ package core
 //
 // The size of TcSkbEdit is determined based on the size of its fields,
 // ensuring it aligns correctly with the underlying system architecture.
-//
-// See the related Linux kernel code:
-// - https://github.com/torvalds/linux/blob/master/include/uapi/linux/pkt_cls.h
 func (msg *TcSkbEdit) Len() int {
 	return SizeofTcSkbEdit
 }
