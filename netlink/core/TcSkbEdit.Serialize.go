6114528f5f5d2bc3e5d6f717d33b2d47ee1cfc0f
Sam Caldwell <github@samcaldwell.net>
2024-09-04 22:52:46 -0500

    add TcaSkbEdit*

--- netlink/core/TcSkbEdit.Serialize.go
+++ netlink/core/TcSkbEdit.Serialize.go
@@ -1,7 +1,15 @@
 package core
 
-import "unsafe"
-
+// Serialize converts the TcSkbEdit structure into a byte slice in a safe way.
 func (msg *TcSkbEdit) Serialize() []byte {
-	return (*(*[SizeofTcSkbEdit]byte)(unsafe.Pointer(x)))[:]
+	buf := make([]byte, SizeofTcSkbEdit)
+
+	// Write each field to the buffer
+	NativeEndian.PutUint32(buf[0:4], msg.Index)
+	NativeEndian.PutUint32(buf[4:8], msg.Capab)
+	NativeEndian.PutUint32(buf[8:12], uint32(msg.Action))
+	NativeEndian.PutUint32(buf[12:16], uint32(msg.Refcnt))
+	NativeEndian.PutUint32(buf[16:20], uint32(msg.Bindcnt))
+
+	return buf
 }
