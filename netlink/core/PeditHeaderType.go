00c11aabcdd21f6c51d86c6825f2ffcf07267abe
Sam Caldwell <github@samcaldwell.net>
2024-09-04 23:57:13 -0500

    add PeditHeaderType

---
+++ netlink/core/PeditHeaderType.go
@@ -0,0 +1,6 @@
+package core
+
+// PeditHeaderType - TCA_PEDIT_KEY_EX_HDR_TYPE_NETWORK is a special case for legacy users. It
+//
+//	means no specific header type - offset is relative to the network layer
+type PeditHeaderType uint16
