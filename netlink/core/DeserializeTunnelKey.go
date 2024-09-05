96b4399e3a8993fc8e7a4b7effe60960a7026692
Sam Caldwell <github@samcaldwell.net>
2024-09-04 22:47:47 -0500

    add DeserializeTunnelKey()

--- netlink/core/DeserializeTunnelKey.go
+++ netlink/core/DeserializeTunnelKey.go
@@ -1,7 +1,25 @@
 package core
 
-import "unsafe"
+import (
+	"errors"
+)
 
-func DeserializeTunnelKey(b []byte) *TcTunnelKey {
-	return (*TcTunnelKey)(unsafe.Pointer(&b[0:SizeofTcTunnelKey][0]))
+// DeserializeTunnelKey safely deserializes a byte slice into a TcTunnelKey struct.
+// The function ensures that the byte slice is the correct length and uses
+// binary encoding to parse the data correctly.
+func DeserializeTunnelKey(b []byte) (*TcTunnelKey, error) {
+	if len(b) < SizeofTcTunnelKey {
+		return nil, errors.New("DeserializeTunnelKey: byte slice too short")
+	}
+
+	msg := &TcTunnelKey{}
+
+	// Deserialize each field in the struct.
+	msg.Index = NativeEndian.Uint32(b[0:4])
+	msg.Capab = NativeEndian.Uint32(b[4:8])
+	msg.Action = int32(NativeEndian.Uint32(b[8:12]))
+	msg.Refcnt = int32(NativeEndian.Uint32(b[12:16]))
+	msg.Bindcnt = int32(NativeEndian.Uint32(b[16:20]))
+
+	return msg, nil
 }
