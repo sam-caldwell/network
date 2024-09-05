e6e8f1a6ec960843d04e27518b92d3dcef60ac13
Sam Caldwell <github@samcaldwell.net>
2024-09-04 22:14:37 -0500

    add TcCsum

---
+++ netlink/core/TcCsum.Len.go
@@ -0,0 +1,13 @@
+package core
+
+// Len - Returns the size of the TcCsum structure in bytes.
+//
+// The TcCsum structure size is determined by the SizeofTcCsum constant, which reflects the size of this struct
+// in the Linux kernel. This function is essential when serializing or deserializing the structure in
+// networking contexts, such as when interacting with netlink messages or other low-level kernel operations.
+//
+// For more details, refer to:
+// - Linux Kernel Source: https://github.com/torvalds/linux/blob/master/include/uapi/linux/tc_act/tc_csum.h
+func (msg *TcCsum) Len() int {
+	return SizeofTcCsum
+}
