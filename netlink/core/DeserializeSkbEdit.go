b90653236cbd36af3aa29823984941fc751ff992
Sam Caldwell <github@samcaldwell.net>
2024-09-04 22:49:11 -0500

    add DeserializeSkbEdit()

--- netlink/core/DeserializeSkbEdit.go
+++ netlink/core/DeserializeSkbEdit.go
@@ -1,7 +1,24 @@
 package core
 
-import "unsafe"
+import (
+	"errors"
+)
 
-func DeserializeSkbEdit(b []byte) *TcSkbEdit {
-	return (*TcSkbEdit)(unsafe.Pointer(&b[0:SizeofTcSkbEdit][0]))
+// DeserializeSkbEdit safely deserializes a byte slice into a TcSkbEdit struct.
+// The function ensures the byte slice is of the correct size and uses binary deserialization.
+func DeserializeSkbEdit(b []byte) (*TcSkbEdit, error) {
+	if len(b) < SizeofTcSkbEdit {
+		return nil, errors.New("DeserializeSkbEdit: byte slice too short")
+	}
+
+	msg := &TcSkbEdit{}
+
+	// Deserialize each field in the TcSkbEdit struct.
+	msg.Index = NativeEndian.Uint32(b[0:4])
+	msg.Capab = NativeEndian.Uint32(b[4:8])
+	msg.Action = int32(NativeEndian.Uint32(b[8:12]))
+	msg.Refcnt = int32(NativeEndian.Uint32(b[12:16]))
+	msg.Bindcnt = int32(NativeEndian.Uint32(b[16:20]))
+
+	return msg, nil
 }
