868367b2861b712f85e78163218a98832102f891
Sam Caldwell <github@samcaldwell.net>
2024-09-04 23:16:00 -0500

    add DeserializeTcPolice

---
+++ netlink/core/DeserializeTcPolice.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"errors"
+)
+
+// DeserializeTcPolice - Safely converts a byte slice into a TcPolice struct.
+// This method ensures that the length of the input byte slice matches the expected size of TcPolice.
+//
+// If the length of the input byte slice is less than the size of TcPolice, it returns an error.
+func DeserializeTcPolice(b []byte) (*TcPolice, error) {
+	if len(b) < SizeofTcPolice {
+		return nil, errors.New("DeserializeTcPolice: byte slice too short")
+	}
+
+	msg := &TcPolice{}
+
+	// Copy the content of the byte slice into the TcPolice struct.
+	// Manually copy each field to avoid the risks of using unsafe.Pointer.
+	msg.Index = NativeEndian.Uint32(b[0:4])
+	msg.Action = int32(NativeEndian.Uint32(b[4:8]))
+	msg.Limit = NativeEndian.Uint32(b[8:12])
+	msg.Burst = NativeEndian.Uint32(b[12:16])
+	msg.Mtu = NativeEndian.Uint32(b[16:20])
+	msg.Rate = *DeserializeTcRateSpec(b[20 : 20+SizeofTcRateSpec])
+	msg.PeakRate = *DeserializeTcRateSpec(b[20+SizeofTcRateSpec : 20+2*SizeofTcRateSpec])
+	msg.Refcnt = int32(NativeEndian.Uint32(b[36:40]))
+	msg.Bindcnt = int32(NativeEndian.Uint32(b[40:44]))
+	msg.Capab = NativeEndian.Uint32(b[44:48])
+
+	return msg, nil
+}
