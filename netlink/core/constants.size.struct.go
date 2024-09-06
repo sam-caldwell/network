74be0c796afae88fddd82c6627ae54fc296b8e06
Sam Caldwell <github@samcaldwell.net>
2024-09-06 16:21:34 -0500

    update and reorder the size constants

--- netlink/core/constants.size.struct.go
+++ netlink/core/constants.size.struct.go
@@ -12,20 +12,122 @@ const (
 	// SizeOfIfaCacheinfo - 0x16 // bytes as derived from unix.SizeofIfaCacheinfo
 	SizeOfIfaCacheinfo = int(unsafe.Sizeof(IfaCacheInfo{}))
 
-	//SizeofIfInfoMsg -  0x16 // bytes as derived from unix.SizeofIfInfomsg
-	SizeofIfInfoMsg = int(unsafe.Sizeof(IfInfoMsg{}))
+	// SizeofBridgeVlanInfo - size of BridgeVlanInfo
+	SizeofBridgeVlanInfo = int(unsafe.Sizeof(BridgeVlanInfo{}))
 
 	// SizeofCnMsgOp - size of CnMsgOp struct
 	SizeofCnMsgOp = int(unsafe.Sizeof(CnMsgOp{}))
 
-	// SizeofBridgeVlanInfo - size of BridgeVlanInfo
-	SizeofBridgeVlanInfo = int(unsafe.Sizeof(BridgeVlanInfo{}))
+	// SizeofGenlmsg - size of Genlmsg struct
+	SizeofGenlmsg = int(unsafe.Sizeof(Genlmsg{}))
 
 	// SizeofNfgenmsg - Track the message sizes for the correct serialization/deserialization
 	SizeofNfgenmsg = int(unsafe.Sizeof(Nfgenmsg{}))
 
+	// SizeofNlMsghdr - Size of unix.NlMsghdr struct
+	SizeofNlMsghdr = int(unsafe.Sizeof(unix.NlMsghdr{}))
+
+	// SizeofRtNexthop - Size of the RtNexthop struct
+	SizeofRtNexthop = int(unsafe.Sizeof(RtNexthop{}))
+
+	// SizeOfTcNetemRate is the size of the TcNetemRate structure, defining rate options in netem.
+	SizeOfTcNetemRate = int(unsafe.Sizeof(TcNetemRate{}))
+
+	// SizeofTcActionMsg is the size of the TcActionMsg structure, which represents actions taken on packets in TC.
+	SizeofTcActionMsg = int(unsafe.Sizeof(TcActionMsg{}))
+
+	// SizeofTcConnmark is the size of the TcConnmark structure, used for connection marking in TC.
+	SizeofTcConnmark = int(unsafe.Sizeof(TcConnmark{}))
+
+	// SizeofTcCsum is the size of the TcCsum structure, representing checksum actions in traffic control.
+	SizeofTcCsum = int(unsafe.Sizeof(TcCsum{}))
+
+	// SizeofTcGen is the size of the TcGen structure, representing general traffic control options.
+	SizeofTcGen = int(unsafe.Sizeof(TcGen{}))
+
+	// SizeofTcHtbCopt is the size of the TcHtbCopt structure, representing options for hierarchical token bucket (HTB) classes in TC.
+	SizeofTcHtbCopt = int(unsafe.Sizeof(TcHtbCopt{}))
+
+	// SizeofTcHtbGlob is the size of the TcHtbGlob structure, representing global parameters for HTB.
+	SizeofTcHtbGlob = int(unsafe.Sizeof(TcHtbGlob{}))
+
+	// SizeofTcInfoMsg -  0x16 // bytes as derived from unix.SizeofIfInfomsg
+	SizeofIfInfoMsg = int(unsafe.Sizeof(IfInfoMsg{}))
+
+	// SizeofTcMsg is the size of the TcMsg structure, which is used to configure traffic control (TC) in Linux.
+	SizeofTcMsg = int(unsafe.Sizeof(TcMsg{}))
+
+	// SizeofTcMirred is the size of the TcMirred structure, representing "mirred" (mirror/redirect) actions in TC.
+	SizeofTcMirred = int(unsafe.Sizeof(TcMirred{}))
+
+	// SizeofTcNetemCorr is the size of the TcNetemCorr structure, representing correlation options in netem.
+	SizeofTcNetemCorr = int(unsafe.Sizeof(TcNetemCorr{}))
+
+	// SizeofTcNetemCorrupt is the size of the TcNetemCorrupt structure, representing options for packet corruption in netem.
+	SizeofTcNetemCorrupt = int(unsafe.Sizeof(TcNetemCorrupt{}))
+
+	// SizeofTcNetemQopt is the size of the TcNetemQopt structure, used for network emulation (netem) options in traffic control.
+	SizeofTcNetemQopt = int(unsafe.Sizeof(TcNetemQopt{}))
+
+	// SizeofTcNetemReorder is the size of the TcNetemReorder structure, used to configure packet reordering in netem.
+	SizeofTcNetemReorder = int(unsafe.Sizeof(TcNetemReorder{}))
+
+	// SizeofTcPolice is the size of the TcPolice structure, representing police actions in TC.
+	SizeofTcPolice = int(unsafe.Sizeof(TcPolice{}))
+
+	// SizeofTcPrioMap is the size of the TcPrioMap structure, used to map priorities for traffic control.
+	SizeofTcPrioMap = int(unsafe.Sizeof(TcPrioMap{}))
+
+	// SizeofTcRateSpec is the size of the TcRateSpec structure, which defines the rate specification for traffic control.
+	SizeofTcRateSpec = int(unsafe.Sizeof(TcRateSpec{}))
+
+	// SizeofTcSfqQopt is the size of the TcSfqQopt structure, representing options for SFQ.
+	SizeofTcSfqQopt = int(unsafe.Sizeof(TcSfqQopt{}))
+
+	// SizeofTcSfqQoptV1 is the size of the TcSfqQoptV1 structure, representing options for stochastic fair queuing (SFQ) version 1.
+	SizeofTcSfqQoptV1 = int(unsafe.Sizeof(TcSfqQoptV1{}))
+
+	// SizeofTcSfqRedStats is the size of the TcSfqRedStats structure, representing RED (random early detection) statistics in SFQ.
+	SizeofTcSfqRedStats = int(unsafe.Sizeof(TcSfqRedStats{}))
+
+	// SizeofTcSkbEdit is the size of the TcSkbEdit structure, representing modifications to skb (socket buffer) in TC.
+	SizeofTcSkbEdit = int(unsafe.Sizeof(TcSkbEdit{}))
+
+	// SizeofTcTbfQopt is the size of the TcTbfQopt structure, which represents options for token bucket filter (TBF) in traffic control.
+	SizeofTcTbfQopt = int(unsafe.Sizeof(TcTbfQopt{}))
+
+	// SizeofTcTunnelKey is the size of the TcTunnelKey structure, representing tunnel key actions in TC.
+	SizeofTcTunnelKey = int(unsafe.Sizeof(TcTunnelKey{}))
+
+	// SizeofTcf is the size of the Tcf structure, representing a filter in traffic control.
+	SizeofTcf = int(unsafe.Sizeof(Tcf{}))
+
+	// SizeofUint32Bitfield is the size of a 32-bit bitfield used in various traffic control structures.
+	SizeofUint32Bitfield = int(unsafe.Sizeof(Uint32Bitfield{}))
+
+	// SizeofVfGUID - Size of VfGUID struct
+	SizeofVfGUID = int(unsafe.Sizeof(VfGUID{}))
+
+	// SizeofVfLinkState - Size of VfLinkState struct
+	SizeofVfLinkState = int(unsafe.Sizeof(VfLinkState{}))
+
 	// SizeofVfMac - Size of VfMac struct
 	SizeofVfMac = int(unsafe.Sizeof(VfMac{}))
+
+	// SizeofVfRate - Size of VfRate struct
+	SizeofVfRate = int(unsafe.Sizeof(VfRate{}))
+
+	// SizeofVfRssQueryEn - Size of VfRssQueryEn struct
+	SizeofVfRssQueryEn = int(unsafe.Sizeof(VfRssQueryEn{}))
+
+	// SizeofVfSpoofchk - Size of VfSpoofchk
+	SizeofVfSpoofchk = int(unsafe.Sizeof(VfSpoofchk{}))
+
+	// SizeofVfTrust - Size of VfTrust struct
+	SizeofVfTrust = int(unsafe.Sizeof(VfTrust{}))
+
+	// SizeofVfTxRate - Size of VfTxRate struct
+	SizeofVfTxRate = int(unsafe.Sizeof(VfTxRate{}))
 
 	// SizeofVfVlan - Size of VfVlan struct
 	SizeofVfVlan = int(unsafe.Sizeof(VfVlan{}))
@@ -33,82 +135,54 @@ const (
 	// SizeofVfVlanInfo - Size of IfLaVfVlanInfoStruct struct
 	SizeofVfVlanInfo = int(unsafe.Sizeof(IfLaVfVlanInfoStruct{}))
 
-	// SizeofNlMsghdr - Size of unix.NlMsghdr struct
-	SizeofNlMsghdr = int(unsafe.Sizeof(unix.NlMsghdr{}))
+	// SizeofXfrmAddress is the size of the XfrmAddress structure, used to represent addresses in XFRM (IPsec framework).
+	SizeofXfrmAddress = int(unsafe.Sizeof(XfrmAddress{}))
 
-	// SizeofUnixRtAttr - Size of unix.RtAttr struct (note this is smaller than our core.RtAttr which wraps and extends
-	// the unix.RtAttr.
-	SizeofUnixRtAttr = int(unsafe.Sizeof(unix.RtAttr{}))
+	// SizeofXfrmAlgo is the size of the XfrmAlgo structure, representing cryptographic algorithms for IPsec SAs.
+	SizeofXfrmAlgo = int(unsafe.Sizeof(XfrmAlgo{}))
 
-	//SizeofGenlmsg - size of Genlmsg struct
-	SizeofGenlmsg = int(unsafe.Sizeof(Genlmsg{}))
+	// SizeofXfrmAlgoAEAD is the size of the XfrmAlgoAEAD structure, representing AEAD (authenticated encryption) algorithms for IPsec.
+	SizeofXfrmAlgoAEAD = int(unsafe.Sizeof(XfrmAlgoAEAD{}))
 
-	// SizeofVfRate - Size of VfRate struct
-	SizeofVfRate = int(unsafe.Sizeof(VfRate{}))
+	// SizeofXfrmAlgoAuth is the size of the XfrmAlgoAuth structure, representing authentication algorithms for IPsec SAs.
+	SizeofXfrmAlgoAuth = int(unsafe.Sizeof(XfrmAlgoAuth{}))
 
-	// SizeofVfSpoofchk - Size of VfSpoofchk
-	SizeofVfSpoofchk = int(unsafe.Sizeof(VfSpoofchk{}))
+	// SizeofXfrmEncapTmpl is the size of the XfrmEncapTmpl structure, representing encapsulation templates for IPsec.
+	SizeofXfrmEncapTmpl = int(unsafe.Sizeof(XfrmEncapTmpl{}))
 
-	// SizeofVfTxRate - Size of VfTxRate struct
-	SizeofVfTxRate = int(unsafe.Sizeof(VfTxRate{}))
+	// SizeofXfrmId is the size of the XfrmId structure, representing the identifier for IPsec Security Associations.
+	SizeofXfrmId = int(unsafe.Sizeof(XfrmId{}))
 
-	// SizeofVfLinkState - Size of VfLinkState struct
-	SizeofVfLinkState = int(unsafe.Sizeof(VfLinkState{}))
-
-	// SizeofVfRssQueryEn - Size of VfRssQueryEn struct
-	SizeofVfRssQueryEn = int(unsafe.Sizeof(VfRssQueryEn{}))
-
-	// SizeofVfTrust - Size of VfTrust struct
-	SizeofVfTrust = int(unsafe.Sizeof(VfTrust{}))
-
-	// SizeofVfGUID - Size of VfGUID struct
-	SizeofVfGUID = int(unsafe.Sizeof(VfGUID{}))
-
-	// SizeofNfattr -
-	SizeofNfattr = 4
-
-	// SizeofNfConntrack -
-	SizeofNfConntrack = 376
-
-	// SizeofNfctTupleHead -
-	SizeofNfctTupleHead = 52
-
-	// SizeofRtNexthop - Size of the RtNexthop struct
-	SizeofRtNexthop = int(unsafe.Sizeof(RtNexthop{}))
-)
-
-const (
-	SizeofTcMsg           = int(unsafe.Sizeof(TcMsg{}))
-	SizeofTcActionMsg     = int(unsafe.Sizeof(TcActionMsg{}))
-	SizeofTcf             = int(unsafe.Sizeof(Tcf{}))
-	SizeofTcPrioMap       = int(unsafe.Sizeof(TcPrioMap{}))
-	SizeofTcRateSpec      = int(unsafe.Sizeof(TcRateSpec{}))
-	SizeofTcNetemQopt     = int(unsafe.Sizeof(TcNetemQopt{}))
-	SizeofTcNetemCorr     = int(unsafe.Sizeof(TcNetemCorr{}))
-	SizeofTcNetemReorder  = int(unsafe.Sizeof(TcNetemReorder{}))
-	SizeofTcNetemCorrupt  = int(unsafe.Sizeof(TcNetemCorrupt{}))
-	SizeOfTcNetemRate     = int(unsafe.Sizeof(TcNetemRate{}))
-	SizeofTcTbfQopt       = int(unsafe.Sizeof(TcTbfQopt{}))
-	SizeofTcHtbCopt       = int(unsafe.Sizeof(TcHtbCopt{}))
-	SizeofTcHtbGlob       = int(unsafe.Sizeof(TcHtbGlob{}))
-	SizeofTcPolice        = int(unsafe.Sizeof(TcPolice{}))
-	SizeofTcU32Key        = int(unsafe.Sizeof(TcU32Key{}))
-	SizeofTcU32Sel        = int(unsafe.Sizeof(TcU32Sel{})) // without keys
-	SizeofTcGen           = int(unsafe.Sizeof(TcGen{}))
-	SizeofUint32Bitfield  = int(unsafe.Sizeof(Uint32Bitfield{}))
-	SizeofTcConnmark      = int(unsafe.Sizeof(TcConnmark{}))
-	SizeofTcCsum          = int(unsafe.Sizeof(TcCsum{}))
-	SizeofTcMirred        = int(unsafe.Sizeof(TcMirred{}))
-	SizeofTcTunnelKey     = int(unsafe.Sizeof(TcTunnelKey{}))
-	SizeofTcSkbEdit       = int(unsafe.Sizeof(TcSkbEdit{}))
-	SizeofTcSfqQoptV1     = int(unsafe.Sizeof(TcSfqQoptV1{}))
-	SizeofTcSfqQopt       = int(unsafe.Sizeof(TcSfqQopt{}))
-	SizeofTcSfqRedStats   = int(unsafe.Sizeof(TcSfqRedStats{}))
-	SizeofXfrmUserExpire  = int(unsafe.Sizeof(XfrmUserExpire{}))
-	SizeofXfrmAddress     = int(unsafe.Sizeof(XfrmAddress{}))
-	SizeofXfrmSelector    = int(unsafe.Sizeof(XfrmSelector{}))
+	// SizeofXfrmLifetimeCfg is the size of the XfrmLifetimeCfg structure, representing lifetime configuration for IPsec SAs.
 	SizeofXfrmLifetimeCfg = int(unsafe.Sizeof(XfrmLifetimeCfg{}))
+
+	// SizeofXfrmLifetimeCur is the size of the XfrmLifetimeCur structure, representing current lifetime usage of IPsec SAs.
 	SizeofXfrmLifetimeCur = int(unsafe.Sizeof(XfrmLifetimeCur{}))
-	SizeofXfrmId          = int(unsafe.Sizeof(XfrmId{}))
-	SizeofXfrmMark        = int(unsafe.Sizeof(XfrmMark{}))
+
+	// SizeofXfrmMark is the size of the XfrmMark structure, used for packet classification in IPsec SAs.
+	SizeofXfrmMark = int(unsafe.Sizeof(XfrmMark{}))
+
+	// SizeofXfrmReplayState is the size of the XfrmReplayState structure, representing replay state for IPsec SAs.
+	SizeofXfrmReplayState = int(unsafe.Sizeof(XfrmReplayState{}))
+
+	// SizeofXfrmReplayStateEsn is the size of the XfrmReplayStateEsn structure, representing replay state with extended sequence numbers.
+	SizeofXfrmReplayStateEsn = int(unsafe.Sizeof(XfrmReplayStateEsn{}))
+
+	// SizeofXfrmStats is the size of the XfrmStats structure, representing IPsec framework statistics.
+	SizeofXfrmStats = int(unsafe.Sizeof(XfrmStats{}))
+
+	// SizeofXfrmUserExpire is the size of the XfrmUserExpire structure, used for XFRM (IPsec framework) expiration events.
+	SizeofXfrmUserExpire = int(unsafe.Sizeof(XfrmUserExpire{}))
+
+	// SizeofXfrmUsersaFlush is the size of the XfrmUsersaFlush structure, representing requests to flush Security Associations in XFRM.
+	SizeofXfrmUsersaFlush = int(unsafe.Sizeof(XfrmUsersaFlush{}))
+
+	// SizeofXfrmUsersaId is the size of the XfrmUsersaId structure, representing user-level Security Association identifiers.
+	SizeofXfrmUsersaId = int(unsafe.Sizeof(XfrmUsersaId{}))
+
+	// SizeofXfrmUsersaInfo is the size of the XfrmUsersaInfo structure, representing user-level Security Association information.
+	SizeofXfrmUsersaInfo = int(unsafe.Sizeof(XfrmUsersaInfo{}))
+
+	// SizeofXfrmUserSpiInfo is the size of the XfrmUserSpiInfo structure, used for Security Parameter Index (SPI) information.
+	SizeofXfrmUserSpiInfo = int(unsafe.Sizeof(XfrmUserSpiInfo{}))
 )
