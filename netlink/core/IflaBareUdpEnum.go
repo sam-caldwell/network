6c29565e33369cde441bd5e39c3d556807ee4499
Sam Caldwell <github@samcaldwell.net>
2024-09-06 17:48:41 -0500

    fix typo

--- netlink/core/IflaBareUdpEnum.go
+++ netlink/core/IflaBareUdpEnum.go
@@ -1,9 +1,9 @@
 package core
 
-// IflaBareUdpEnum - Enumeration for Bare UDP interface attributes.
+// IflaBareUdpEnum - Enumeration for Bare IpProtoUDP interface attributes.
 //
-// These attributes are used to configure and manage Bare UDP interfaces in the Linux kernel.
-// Bare UDP is a lightweight tunneling protocol that encapsulates Layer 3 protocols in UDP.
+// These attributes are used to configure and manage Bare IpProtoUDP interfaces in the Linux kernel.
+// Bare IpProtoUDP is a lightweight tunneling protocol that encapsulates Layer 3 protocols in IpProtoUDP.
 //
 // See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
 type IflaBareUdpEnum uint8
@@ -16,31 +16,31 @@ const (
 	IflaBareUdpUnspec IflaBareUdpEnum = 0
 
 	// IflaBareUdpPort - IFLA_BAREUDP_PORT -
-	// Specifies the UDP destination port for the Bare UDP interface.
+	// Specifies the IpProtoUDP destination port for the Bare IpProtoUDP interface.
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
 	IflaBareUdpPort IflaBareUdpEnum = 1
 
 	// IflaBareUdpEthertype - IFLA_BAREUDP_ETHERTYPE -
-	// Specifies the EtherType for encapsulated packets on the Bare UDP interface.
+	// Specifies the EtherType for encapsulated packets on the Bare IpProtoUDP interface.
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
 	IflaBareUdpEthertype IflaBareUdpEnum = 2
 
 	// IflaBareUdpSrcportMin - IFLA_BAREUDP_SRCPORT_MIN -
-	// Specifies the minimum UDP source port for the Bare UDP interface.
+	// Specifies the minimum IpProtoUDP source port for the Bare IpProtoUDP interface.
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
 	IflaBareUdpSrcportMin IflaBareUdpEnum = 3
 
 	// IflaBareUdpMultiprotoMode - IFLA_BAREUDP_MULTIPROTO_MODE -
-	// Indicates whether multiple protocols are supported on the Bare UDP interface.
+	// Indicates whether multiple protocols are supported on the Bare IpProtoUDP interface.
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
 	IflaBareUdpMultiprotoMode IflaBareUdpEnum = 4
 
 	// IflaBareUdpMax - IFLA_BAREUDP_MAX -
-	// The maximum value for Bare UDP attributes, used for validation purposes.
+	// The maximum value for Bare IpProtoUDP attributes, used for validation purposes.
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
 	IflaBareUdpMax = iota - 1
