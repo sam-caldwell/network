6cf27a00ae4c92afe289189de5a0192f71bf7674
Sam Caldwell <github@samcaldwell.net>
2024-09-06 18:41:11 -0500

    resovle conflict

--- netlink/core/PeditHeaderType.go
+++ netlink/core/PeditHeaderType.go
@@ -1,6 +1,7 @@
 package core
 
-// PeditHeaderType - TCA_PEDIT_KEY_EX_HDR_TYPE_NETWORK is a special case for legacy users. It
+// PeditHeaderType - TCA_PEDIT_KEY_EX_HDR_TYPE_NETWORK is a special case for legacy users. It means no specific
+// header type - offset is relative to the network layer
 //
-//	means no specific header type - offset is relative to the network layer
+// See TcaPeditKeyExHdrType
 type PeditHeaderType uint16
--- netlink/core/TcaPeditKeyExHdrType.go
+++ netlink/core/TcaPeditKeyExHdrType.go
@@ -1,30 +1,33 @@
 package core
 
-// TcaPeditKeyExHdrType represents the header types used in the pedit action within the Linux kernel's traffic control (TC).
-// These values define the header types that can be manipulated by the extended pedit key feature.
+// TcaPeditKeyExHdrType represents the header types used in the pedit action within the Linux kernel's
+// traffic control (TC). These values define the header types that can be manipulated by the extended pedit key
+// feature.
+//
+// # See PeditHeaderType
 //
 // For more details, refer to the Linux Kernel Source:
 // https://github.com/torvalds/linux/blob/master/include/uapi/linux/tc_act/tc_pedit.h
-type TcaPeditKeyExHdrType uint8
+type TcaPeditKeyExHdrType uint16
 
 const (
 	// TcaPeditKeyExHdrTypeNetwork - TCA_PEDIT_KEY_EX_HDR_TYPE_NETWORK - represents a network layer header.
-	TcaPeditKeyExHdrTypeNetwork TcaPeditKeyExHdrType = iota
+	TcaPeditKeyExHdrTypeNetwork PeditHeaderType = iota
 
-	// TcaPeditKeyExHdrTypeEth - TCA_PEDIT_KEY_EX_HDR_TYPE_ETH - represents an Ethernet header.
-	TcaPeditKeyExHdrTypeEth TcaPeditKeyExHdrType = iota
+	// TcaPeditKeyExHdrTypeEth - TcaPeditKeyExHdrTypeEth - represents an Ethernet header.
+	TcaPeditKeyExHdrTypeEth PeditHeaderType = iota
 
 	// TcaPeditKeyExHdrTypeIP4 - TCA_PEDIT_KEY_EX_HDR_TYPE_IP4 - represents an IPv4 header.
-	TcaPeditKeyExHdrTypeIP4 TcaPeditKeyExHdrType = iota
+	TcaPeditKeyExHdrTypeIP4 PeditHeaderType = iota
 
 	// TcaPeditKeyExHdrTypeIP6 - TCA_PEDIT_KEY_EX_HDR_TYPE_IP6 - represents an IPv6 header.
-	TcaPeditKeyExHdrTypeIP6 TcaPeditKeyExHdrType = iota
+	TcaPeditKeyExHdrTypeIP6 PeditHeaderType = iota
 
 	// TcaPeditKeyExHdrTypeTCP - TCA_PEDIT_KEY_EX_HDR_TYPE_TCP - represents a TCP header.
-	TcaPeditKeyExHdrTypeTCP TcaPeditKeyExHdrType = iota
+	TcaPeditKeyExHdrTypeTCP PeditHeaderType = iota
 
 	// TcaPeditKeyExHdrTypeUDP - TCA_PEDIT_KEY_EX_HDR_TYPE_UDP - represents a UDP header.
-	TcaPeditKeyExHdrTypeUDP TcaPeditKeyExHdrType = iota
+	TcaPeditKeyExHdrTypeUDP PeditHeaderType = iota
 
 	// PeditHdrTypeMax - __PEDIT_HDR_TYPE_MAX - represents the maximum number of header types.
 	PeditHdrTypeMax = iota - 1
