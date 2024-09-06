9626c31da8a5e79609002d8c1c696b2572cec8d0
Sam Caldwell <github@samcaldwell.net>
2024-09-06 14:59:25 -0500

    add XfrmaOutputMark

---
+++ netlink/core/XfrmaOutputMark.go
@@ -0,0 +1,5 @@
+package core
+
+// XfrmaOutputMark - XFRMA_OUTPUT_MARK -
+// This is a shorthand reference for the XFRMA_SET_MARK attribute, reused for convenience.
+const XfrmaOutputMark = XfrmaSetMark
