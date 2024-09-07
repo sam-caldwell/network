6e81870b05525d88b5afe40316840697486ba684
Sam Caldwell <github@samcaldwell.net>
2024-09-06 20:33:16 -0500

    dcument XfrmMsg

--- netlink/core/XfrmMsg.go
+++ netlink/core/XfrmMsg.go
@@ -1,5 +1,10 @@
 package core
 
+// XfrmMsg -  XFRM messages used in IPsec configuration and management.
+// These message types are part of the Linux kernel's XFRM subsystem, which is responsible for managing
+// Security Associations (SA), Security Policies (SP), and related components in IPsec.
+//
+// See: https://github.com/torvalds/linux/blob/master/include/uapi/linux/xfrm.h
 type XfrmMsg interface {
 	Type() XfrmMsgType
 }
