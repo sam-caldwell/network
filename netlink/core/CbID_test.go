c447a847d220522c880111d34f37ca4053f07687
Sam Caldwell <github@samcaldwell.net>
2024-09-07 16:59:52 -0500

    add tests for CbId

--- netlink/core/CbID_test.go
+++ netlink/core/CbID_test.go
@@ -2,11 +2,35 @@ package core
 
 import (
 	"testing"
+	"unsafe"
 )
 
 func TestCbID(t *testing.T) {
-	_ = CbID{
-		Idx: uint32(0),
-		Val: uint32(0),
-	}
+	t.Run("size field verification", func(t *testing.T) {
+		_ = CbID{
+			Idx: uint32(0),
+			Val: uint32(0),
+		}
+	})
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 8
+		var o CbID
+		if unsafe.Sizeof(o) != expectedSizeInBytes {
+			t.Fatalf("size check (struct) failed")
+		}
+	})
+	t.Run("size check (Idx)", func(t *testing.T) {
+		const expectedSizeInBytes = 4
+		var o CbID
+		if unsafe.Sizeof(o.Idx) != expectedSizeInBytes {
+			t.Fatalf("size check (Idx) failed")
+		}
+	})
+	t.Run("size check (Val)", func(t *testing.T) {
+		const expectedSizeInBytes = 4
+		var o CbID
+		if unsafe.Sizeof(o.Val) != expectedSizeInBytes {
+			t.Fatalf("size check (Val) failed")
+		}
+	})
 }
