071de024fb4be3241ab600afeeb1b25cc9b532a7
Sam Caldwell <github@samcaldwell.net>
2024-09-07 14:48:25 -0500

    code cleanup

--- netlink/core/DeserializeRtNexthop.go
+++ netlink/core/DeserializeRtNexthop.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"encoding/binary"
 	"golang.org/x/sys/unix"
 )
 
--- netlink/core/VfTrust.go
+++ netlink/core/VfTrust.go
@@ -3,11 +3,6 @@
 package core
 
 // VfTrust - ifla_vf_trust
-//
-//	struct ifla_vf_trust {
-//	  __u32 vf;
-//	  __u32 setting;
-//	};
 type VfTrust struct {
 	Vf      uint32
 	Setting uint32
