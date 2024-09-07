1e4bb1d8b45f6f682ee281b2ad2bf7024b28b125
Sam Caldwell <github@samcaldwell.net>
2024-09-07 14:41:08 -0500

    add NewRtAttrChild()

--- netlink/core/NewRtAttrChild.go
+++ netlink/core/NewRtAttrChild.go
@@ -3,6 +3,6 @@ package core
 // NewRtAttrChild - add RtAttr as child to parent and return this new attribute
 //
 // ***Deprecated***  Use AddRtAttr() on the parent object
-func NewRtAttrChild(parent *RtAttr, attrType int, data []byte) *RtAttr {
+func NewRtAttrChild(parent *RtAttr, attrType uint16, data []byte) *RtAttr {
 	return parent.AddRtAttr(attrType, data)
 }
