093b9dba877cb3baf69a80b2fffe34b1ad23ac13
Sam Caldwell <github@samcaldwell.net>
2024-09-07 14:42:34 -0500

    fix typo

--- netlink/core/TcU32Sel.Len.go
+++ netlink/core/TcU32Sel.Len.go
@@ -5,5 +5,5 @@ package core
 // The size is calculated as the base size of TcU32Sel plus the size of all keys in the Keys array.
 // Each key is represented by a TcU32Key, and the total number of keys is stored in the Nkeys field.
 func (msg *TcU32Sel) Len() int {
-	return SizeofTcU32Sel + int(msg.Nkeys)*SizeofTcU32Key
+	return SizeOfTcU32Sel + int(msg.Nkeys)*SizeOfTcU32Key
 }
