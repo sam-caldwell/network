d6de2d2e87c154d1138f0c0711ddbe8bfc08d6cb
Sam Caldwell <github@samcaldwell.net>
2024-09-07 00:51:19 -0500

    add InterfaceFamily.Equal()

--- netlink/core/InterfaceFamily.Equal.go
+++ netlink/core/InterfaceFamily.Equal.go
@@ -2,5 +2,7 @@ package core
 
 // Equal - Compare two InterfaceFamily objects and return boolean indicating equivalence.
 func (lhs *InterfaceFamily) Equal(rhs InterfaceFamily) bool {
+
 	return *lhs == rhs
+
 }
