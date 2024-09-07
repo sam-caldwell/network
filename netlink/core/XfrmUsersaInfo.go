d86fe1345bf1a311de48df18874bbd1dd6af904d
Sam Caldwell <github@samcaldwell.net>
2024-09-06 22:02:31 -0500

    add XfrmUsersaInfo

--- netlink/core/XfrmUsersaInfo.go
+++ netlink/core/XfrmUsersaInfo.go
@@ -10,35 +10,35 @@ type XfrmUsersaInfo struct {
 	// It specifies the source and destination addresses, ports, protocol, and other traffic-related fields.
 	Sel XfrmSelector
 
-	// Id represents the XfrmId, which contains the Security Parameter Index (SPI), destination address, and protocol.
+	// Id - represents the XfrmId, which contains the Security Parameter Index (SPI), destination address, and protocol.
 	// This uniquely identifies the IPsec SA.
 	Id XfrmId
 
-	// Saddr represents the source address for the Security Association (SA).
+	// Saddr - represents the source address for the Security Association (SA).
 	// It can be an IPv4 or IPv6 address, depending on the address family.
 	Saddr XfrmAddress
 
-	// Lft represents the lifetime configuration of the SA, defining when the SA should expire based on byte count or time.
+	// Lft - represents the lifetime configuration of the SA, defining when the SA should expire based on byte count or time.
 	// This field corresponds to the xfrm_lifetime_cfg structure in the Linux kernel.
 	Lft XfrmLifetimeCfg
 
-	// Curlft represents the current lifetime usage of the SA, tracking how many bytes or packets have been sent and how long
+	// Curlft - represents the current lifetime usage of the SA, tracking how many bytes or packets have been sent and how long
 	// the SA has been in use.
 	Curlft XfrmLifetimeCur
 
-	// Stats represents various statistics related to the Security Association (SA), such as replay attacks, integrity check failures,
+	// Stats - represents various statistics related to the Security Association (SA), such as replay attacks, integrity check failures,
 	// and the size of the replay window.
 	Stats XfrmStats
 
-	// Seq represents a sequence number used to identify the SA in the context of netlink messages.
+	// Seq - represents a sequence number used to identify the SA in the context of netlink messages.
 	// It helps track the state and synchronization of the SA.
 	Seq uint32
 
-	// Reqid is a unique identifier used to match policies to Security Associations (SAs).
+	// Reqid - is a unique identifier used to match policies to Security Associations (SAs).
 	// It is particularly useful in scenarios where multiple policies share a single SA.
 	Reqid uint32
 
-	// Family specifies the address family (AF_INET for IPv4, AF_INET6 for IPv6) of the SA.
+	// Family - specifies the address family (AF_INET for IPv4, AF_INET6 for IPv6) of the SA.
 	// It indicates whether the SA operates on IPv4 or IPv6 traffic.
 	Family uint16
 
