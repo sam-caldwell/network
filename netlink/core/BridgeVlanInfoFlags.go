c7d32facc4d5bb225b9c7eef18a704fb6929c3cf
Sam Caldwell <github@samcaldwell.net>
2024-09-07 16:45:02 -0500

    tests added and passing for BridgeVlanInfoFlags

--- netlink/core/BridgeVlanInfoFlags.go
+++ netlink/core/BridgeVlanInfoFlags.go
@@ -1,43 +1,48 @@
 package core
 
-// BridgeVlanInfoEnum -
+// BridgeVlanInfoFlags -
 //
 // https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-type BridgeVlanInfoEnum uint16
+type BridgeVlanInfoFlags uint16
 
 const (
+	// BridgeVlanInfoFlagsUnset - flags are unset
+	//
+	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
+	BridgeVlanInfoFlagsUnset = 0
+
 	// BridgeVlanInfoMaster - BRIDGE_VLAN_INFO_MASTER - Operate on Bridge device as well
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoMaster BridgeVlanInfoEnum = 1
+	BridgeVlanInfoMaster BridgeVlanInfoFlags = 1
 
 	// BridgeVlanInfoPvid - BRIDGE_VLAN_INFO_PVID -  VLAN is PVID, ingress untagged
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoPvid BridgeVlanInfoEnum = 2
+	BridgeVlanInfoPvid BridgeVlanInfoFlags = 2
 
 	// BridgeVlanInfoUntagged - BRIDGE_VLAN_INFO_UNTAGGED - VLAN egresses untagged
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoUntagged BridgeVlanInfoEnum = 4
+	BridgeVlanInfoUntagged BridgeVlanInfoFlags = 4
 
 	// BridgeVlanInfoRangeBegin - BRIDGE_VLAN_INFO_RANGE_BEGIN - VLAN is start of vlan range
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoRangeBegin BridgeVlanInfoEnum = 8
+	BridgeVlanInfoRangeBegin BridgeVlanInfoFlags = 8
 
 	// BridgeVlanInfoRangeEnd - BRIDGE_VLAN_INFO_RANGE_END - VLAN is end of vlan range
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoRangeEnd BridgeVlanInfoEnum = 16
+	BridgeVlanInfoRangeEnd BridgeVlanInfoFlags = 16
 
 	// BridgeVlanInfoBrentry - BRIDGE_VLAN_INFO_BRENTRY - Global bridge VLAN entry
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoBrentry BridgeVlanInfoEnum = 32
+	BridgeVlanInfoBrentry BridgeVlanInfoFlags = 32
 
 	// BridgeVlanInfoOnlyOpts - BRIDGE_VLAN_INFO_ONLY_OPTS - Skip create/delete/flags
 	//
 	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_bridge.h
-	BridgeVlanInfoOnlyOpts BridgeVlanInfoEnum = 64
+	BridgeVlanInfoOnlyOpts BridgeVlanInfoFlags = 64
 )
