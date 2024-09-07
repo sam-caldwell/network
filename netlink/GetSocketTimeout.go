2401fe52301a32933e1986988a03161b3f58671b
Sam Caldwell <github@samcaldwell.net>
2024-09-06 23:42:19 -0500

    add GetSocketTimeout()

---
+++ netlink/GetSocketTimeout.go
@@ -0,0 +1,13 @@
+package netlink
+
+import (
+	"github.com/sam-caldwell/network/netlink/core"
+	"golang.org/x/sys/unix"
+	"time"
+)
+
+// GetSocketTimeout returns the timeout value used by default netlink sockets
+func GetSocketTimeout() time.Duration {
+	nanoseconds := unix.TimevalToNsec(core.SocketTimeoutTv)
+	return time.Duration(nanoseconds) * time.Nanosecond
+}
