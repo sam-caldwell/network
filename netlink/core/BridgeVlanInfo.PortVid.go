261474d0ae16ef08137f2e49dfebba1116656516
Sam Caldwell <github@samcaldwell.net>
2024-09-07 16:32:20 -0500

    fixes and tests for BridgeVlanInfo.PortVid()

--- netlink/core/BridgeVlanInfo.PortVid.go
+++ netlink/core/BridgeVlanInfo.PortVid.go
@@ -1,11 +1,13 @@
-//go:build linux
-
 package core
 
-// PortVID - returns the VLAN ID assigned to a port in the bridge,
-// determining which VLAN traffic is tagged for that port.
+// PortVID checks if the Port VLAN ID (PVID) flag is set within the BridgeVlanInfo structure.
+// The PVID indicates whether a port should associate untagged incoming packets with the VLAN ID
+// defined in the VID field of the BridgeVlanInfo struct. This is typically used in network bridges
+// that support VLANs to handle how untagged traffic is processed.
+//
+// Return:
+//   - bool: true if the PVID flag is set (meaning the port is configured with a default VLAN ID for untagged packets),
+//     false otherwise.
 func (bridge *BridgeVlanInfo) PortVID() bool {
-
 	return bridge.Flags&BridgeVlanInfoPvid > 0
-
 }
