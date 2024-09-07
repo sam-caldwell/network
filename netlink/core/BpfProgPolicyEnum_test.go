41f92c7e358f7a4805994884bcab6e56e29e7f88
Sam Caldwell <github@samcaldwell.net>
2024-09-07 15:24:10 -0500

    add test for BpfProgPolicyEnum

---
+++ netlink/core/BpfProgPolicyEnum_test.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestBpfProgPolicyEnum(t *testing.T) {
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 1
+		o := BpfProgPolicyEnum(0)
+		if unsafe.Sizeof(o) != expectedSizeInBytes {
+			t.Fatalf("size check failed")
+		}
+	})
+	t.Run("value check", func(t *testing.T) {
+		if value := BpfProgPolicyEnum(0); value != LwtBpfProgUnspec {
+			t.Fatalf("value check failed 0")
+		}
+		if value := BpfProgPolicyEnum(1); value != LwtBpfProgFd {
+			t.Fatalf("value check failed 0")
+		}
+		if value := BpfProgPolicyEnum(2); value != LwtBpfProgName {
+			t.Fatalf("value check failed 0")
+		}
+		if value := BpfProgPolicyEnum(2); value != LwtBpfProgMax {
+			t.Fatalf("value check failed 0")
+		}
+	})
+}
