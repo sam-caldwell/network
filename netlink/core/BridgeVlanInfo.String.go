3c54768aa0c1efda68319684868ea1bc2bc02c88
Sam Caldwell <github@samcaldwell.net>
2024-09-07 16:37:30 -0500

    Make BridgeVlanInfo.String() prettier and add test

--- netlink/core/BridgeVlanInfo.String.go
+++ netlink/core/BridgeVlanInfo.String.go
@@ -6,8 +6,14 @@ import (
 	"fmt"
 )
 
-// String - Return string value of BridgeVlanInfo
-// ToDo: Should we format this a bit better?
+// String - Returns a string representation of the BridgeVlanInfo struct.
+// It outputs the field names and their values in a key-value format, making it easier to debug and inspect
+// the content of the BridgeVlanInfo struct.
+//
+// Example output:
+// BridgeVlanInfo{Flags: 0x0100, Vid: 100}
+//
+// This method is useful when logging or printing the contents of a BridgeVlanInfo object.
 func (bridge *BridgeVlanInfo) String() string {
-	return fmt.Sprintf("%+v", *bridge)
+	return fmt.Sprintf("BridgeVlanInfo{Flags: 0x%04x, Vid: %d}", bridge.Flags, bridge.Vid)
 }
