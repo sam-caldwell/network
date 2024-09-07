3258d720ac48cbc7c11f81a07762274054016124
Sam Caldwell <github@samcaldwell.net>
2024-09-06 19:12:37 -0500

    continuing to work through TcPedit*

--- netlink/core/TcPedit.SetIPv6Src.go
+++ netlink/core/TcPedit.SetIPv6Src.go
@@ -2,21 +2,42 @@ package core
 
 import "net"
 
+// SetIPv6Src sets the IPv6 source address for the pedit action.
+// This function modifies the IPv6 source address by splitting the 16-byte address into 4 parts,
+// each represented by 4 bytes (uint32). It then creates pedit keys and extended pedit keys for
+// each part and sets the appropriate offset and command.
+//
+// Parameters:
+//
+//	ip6 - The IPv6 source address to set (net.IP).
 func (p *TcPedit) SetIPv6Src(ip6 net.IP) {
+	// Extract the first 4 bytes (0-3) of the IPv6 address as uint32.
 	u32 := NativeEndian.Uint32(ip6[:4])
 
+	// Create a new TcPeditKey and TcPeditKeyEx for the first 4 bytes.
 	tKey := TcPeditKey{}
 	tKeyEx := TcPeditKeyEx{}
 
+	// Set the value for the first 4 bytes.
 	tKey.Val = u32
+
+	// Set the offset to 8, indicating the position in the IPv6 header.
 	tKey.Off = 8
+
+	// Set the extended key type to IPv6 and command to "Set".
 	tKeyEx.HeaderType = TcaPeditKeyExHdrTypeIP6
 	tKeyEx.Cmd = PeditCmdSet
 
+	// Append the new key and extended key to the TcPedit struct.
 	p.Keys = append(p.Keys, tKey)
 	p.KeysEx = append(p.KeysEx, tKeyEx)
+
+	// Increment the number of keys in the TcPedit selection.
 	p.Sel.NKeys++
 
+	// Repeat the same steps for the remaining 12 bytes (divided into 4-byte segments).
+
+	// Extract the second 4 bytes (4-7).
 	u32 = NativeEndian.Uint32(ip6[4:8])
 	tKey = TcPeditKey{}
 	tKeyEx = TcPeditKeyEx{}
@@ -28,9 +49,9 @@ func (p *TcPedit) SetIPv6Src(ip6 net.IP) {
 
 	p.Keys = append(p.Keys, tKey)
 	p.KeysEx = append(p.KeysEx, tKeyEx)
-
 	p.Sel.NKeys++
 
+	// Extract the third 4 bytes (8-11).
 	u32 = NativeEndian.Uint32(ip6[8:12])
 	tKey = TcPeditKey{}
 	tKeyEx = TcPeditKeyEx{}
@@ -42,9 +63,9 @@ func (p *TcPedit) SetIPv6Src(ip6 net.IP) {
 
 	p.Keys = append(p.Keys, tKey)
 	p.KeysEx = append(p.KeysEx, tKeyEx)
-
 	p.Sel.NKeys++
 
+	// Extract the fourth 4 bytes (12-15).
 	u32 = NativeEndian.Uint32(ip6[12:16])
 	tKey = TcPeditKey{}
 	tKeyEx = TcPeditKeyEx{}
@@ -56,6 +77,5 @@ func (p *TcPedit) SetIPv6Src(ip6 net.IP) {
 
 	p.Keys = append(p.Keys, tKey)
 	p.KeysEx = append(p.KeysEx, tKeyEx)
-
 	p.Sel.NKeys++
 }
