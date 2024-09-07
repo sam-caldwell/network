78a4ac98ceae0fc694fdffee98e0d9043ec02044
Sam Caldwell <github@samcaldwell.net>
2024-09-06 23:27:59 -0500

    add ParseAddr()

---
+++ netlink/ParseAddr.go
@@ -0,0 +1,18 @@
+package netlink
+
+import "strings"
+
+// ParseAddr parses the string representation of a CIDR address and label.  Note: label portion is optional
+func ParseAddr(s string) (*Addr, error) {
+	label := ""
+	parts := strings.Split(s, " ")
+	if len(parts) > 1 {
+		s = parts[0]
+		label = parts[1]
+	}
+	m, err := ParseIPNet(s)
+	if err != nil {
+		return nil, err
+	}
+	return &Addr{IPNet: m, Label: label}, nil
+}
