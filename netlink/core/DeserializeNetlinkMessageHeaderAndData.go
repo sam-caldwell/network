4272d83d2949d91dac41aa4d918a038a58a00904
Sam Caldwell <github@samcaldwell.net>
2024-09-07 23:26:24 -0500

    add test for DeserializeNetlinkMessageHeaderAndData() with some fixes

---
+++ netlink/core/DeserializeNetlinkMessageHeaderAndData.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	"errors"
+	"golang.org/x/sys/unix"
+)
+
+// DeserializeNetlinkMessageHeaderAndData - deserialize a message header and data from a byte slice.
+func DeserializeNetlinkMessageHeaderAndData(b []byte) (header *unix.NlMsghdr, remainingData []byte, messageLength int, err error) {
+	if len(b) < unix.NLMSG_HDRLEN {
+		return nil, nil, 0, errors.New("input too short to contain NlMsghdr")
+	}
+
+	header, err = DeserializeNlMsgHdr(b)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+
+	messageLength = nlmAlignOf(int(header.Len))
+
+	if int(header.Len) < unix.NLMSG_HDRLEN || messageLength > len(b) {
+		return nil, nil, 0, unix.EINVAL
+	}
+
+	return header, b[unix.NLMSG_HDRLEN:], messageLength, nil
+}
--- netlink/core/DeserializeNetlinkMessageHeaderAndData_test.go
+++ netlink/core/DeserializeNetlinkMessageHeaderAndData_test.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"golang.org/x/sys/unix"
 	"testing"
@@ -8,63 +10,83 @@ import (
 
 func TestDeserializeNetlinkMessageHeaderAndData(t *testing.T) {
 	t.Run("input too short", func(t *testing.T) {
-		// Prepare a byte slice with insufficient length
-		buf := make([]byte, unix.NLMSG_HDRLEN-1)
+		buf := make([]byte, unix.NLMSG_HDRLEN-1) // Insufficient length
 		_, _, _, err := DeserializeNetlinkMessageHeaderAndData(buf)
-		// Expect an error due to short byte slice
+
 		if err == nil {
 			t.Fatalf("Expected error due to short byte slice, but got none")
 		}
-		// Ensure the error message is correct
+
 		expectedError := "input too short to contain NlMsghdr"
 		if err.Error() != expectedError {
 			t.Errorf("Expected error message '%s', but got '%s'", expectedError, err.Error())
 		}
 	})
-	t.Run("invalid Netlink Message Header", func(t *testing.T) {
-		var (
-			header        *unix.NlMsghdr
-			remainingData []byte
-			msgLength     int
-			err           error
-		)
 
-		t.Run("setup", func(t *testing.T) {
-			/*
-				 type NlMsghdr struct {
-					Len   uint32  // 0x00 0x01 0x02 0x03
-					Type  uint16  // 0x10 0x11
-					Flags uint16  // 0x20 0x21
-					Seq   uint32  // 0x30 0x31 0x32 0x33
-					Pid   uint32  // 0x40 0x41 0x42 0x43
-				}
-			*/
-			buf := []byte{
-				0x00, 0x01, 0x02, 0x03,
-				0x10, 0x11,
-				0x20, 0x21,
-				0x30, 0x031, 0x032, 0x33,
-				0x40, 0x41, 0x42, 0x43,
-				0x50,
-			}
-			header, remainingData, msgLength, err = DeserializeNetlinkMessageHeaderAndData(buf)
-		})
-		t.Run("verify no error state", func(t *testing.T) {
-			if header != nil {
-				t.Errorf("header should be nil, but got %v", header)
-			}
-			if remainingData != nil {
-				t.Errorf("remainingData should be nil, but got %v", remainingData)
-			}
-			if msgLength != 0 {
-				t.Errorf("msgLength should be 0, but got %d", msgLength)
-			}
-			if err == nil {
-				t.Fatalf("expected error, but got none")
-			}
-			if !errors.Is(unix.EINVAL, err) {
-				t.Errorf("Expected error '%v', but got '%v'", unix.EINVAL, err)
-			}
-		})
+	t.Run("invalid Netlink Message Header", func(t *testing.T) {
+		buf := []byte{
+			0x00, 0x01, 0x02, 0x03, // Len
+			0x10, 0x11, // Type
+			0x20, 0x21, // Flags
+			0x30, 0x31, 0x32, 0x33, // Seq
+			0x40, 0x41, 0x42, 0x43, // Pid
+			0x50, // Extra byte (invalid)
+		}
+
+		header, remainingData, msgLength, err := DeserializeNetlinkMessageHeaderAndData(buf)
+
+		if header != nil {
+			t.Errorf("header should be nil, but got %v", header)
+		}
+		if remainingData != nil {
+			t.Errorf("remainingData should be nil, but got %v", remainingData)
+		}
+		if msgLength != 0 {
+			t.Errorf("msgLength should be 0, but got %d", msgLength)
+		}
+		if err == nil {
+			t.Fatalf("expected error, but got none")
+		}
+		if !errors.Is(unix.EINVAL, err) {
+			t.Errorf("Expected error '%v', but got '%v'", unix.EINVAL, err)
+		}
+	})
+
+	t.Run("test with a valid netlink header", func(t *testing.T) {
+		netlinkMsg := []byte{
+			// Netlink Header (NlMsghdr)
+			0x1c, 0x00, 0x00, 0x00, // Len: 28 (16-byte header + 12-byte payload)
+			0x01, 0x00, // Type: 1 (NLMSG_NOOP, for example)
+			0x01, 0x00, // Flags: 1 (NLM_F_REQUEST)
+			0x78, 0x56, 0x34, 0x12, // Seq: 0x12345678
+			0xef, 0xcd, 0xab, 0x89, // Pid: 0x89abcdef
+
+			// Payload (data)
+			0xaa, 0xbb, 0xcc, 0xdd, // Some example payload data
+			0x11, 0x22, 0x33, 0x44,
+			0x55, 0x66, 0x77, 0x88,
+		}
+
+		header, remainingData, msgLength, err := DeserializeNetlinkMessageHeaderAndData(netlinkMsg)
+		if err != nil {
+			t.Errorf("error should be nil, but got %v", err)
+		}
+
+		expectedLen := binary.LittleEndian.Uint32([]byte{0x1c, 0x00, 0x00, 0x00})
+		if header.Len != expectedLen {
+			t.Errorf("Len should be %d, but got %d", expectedLen, header.Len)
+		}
+
+		expectedRemainingData := []byte{
+			0xaa, 0xbb, 0xcc, 0xdd, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,
+		}
+		if !bytes.Equal(expectedRemainingData, remainingData) {
+			t.Errorf("remainingData should be %v, but got %v", expectedRemainingData, remainingData)
+		}
+
+		expectedMsgLength := nlmAlignOf(int(header.Len))
+		if msgLength != expectedMsgLength {
+			t.Fatalf("msgLength should be %d, but got %d", expectedMsgLength, msgLength)
+		}
 	})
 }
