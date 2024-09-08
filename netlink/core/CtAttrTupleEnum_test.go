91c8707f733232aca4c7dc5c2843ad9a12eba174
Sam Caldwell <github@samcaldwell.net>
2024-09-07 20:54:10 -0500

    add CtAttrTupleEnum test

---
+++ netlink/core/CtAttrTupleEnum_test.go
@@ -0,0 +1,33 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestCtAttrTupleEnum(t *testing.T) {
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 1
+		if sz := unsafe.Sizeof(CtAttrTupleEnum(0)); sz != expectedSizeInBytes {
+			t.Fatal("size check failed")
+		}
+	})
+	t.Run("value check", func(t *testing.T) {
+		type TestData struct {
+			actual   CtAttrTupleEnum
+			expected CtAttrTupleEnum
+		}
+		testData := []TestData{
+			{actual: 0, expected: CtaTupleUnspec},
+			{actual: 1, expected: CtaTupleIp},
+			{actual: 2, expected: CtaTupleProto},
+			{actual: 3, expected: CtaTupleZone},
+			{actual: 3, expected: CtaTupleMax},
+		}
+		for _, v := range testData {
+			if v.actual != v.expected {
+				t.Fatalf("value check failed.  actual: %d, expected: %d", v.actual, v.expected)
+			}
+		}
+	})
+}
