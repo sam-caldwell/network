37dd79dbe8da15c5882833094663e3b78441eaa4
Sam Caldwell <github@samcaldwell.net>
2024-09-08 18:32:58 -0500

    add test for DeserializeTcNetemReorder

--- netlink/core/DeserializeTcNetemReorder.go
+++ netlink/core/DeserializeTcNetemReorder.go
@@ -1,13 +1,26 @@
 package core
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"errors"
+)
 
 // DeserializeTcNetemReorder - Safely deserializes a byte slice into TcNetemReorder.
-func DeserializeTcNetemReorder(b []byte) *TcNetemReorder {
+//
+//	type TcNetemReorder struct {
+//	   Probability uint32
+//	   Correlation uint32
+//	}
+func DeserializeTcNetemReorder(b []byte) (*TcNetemReorder, error) {
+	if b == nil || len(b) < 8 {
+		return nil, errors.New("input too small")
+	}
 
 	return &TcNetemReorder{
+
 		Probability: binary.LittleEndian.Uint32(b[0:4]),
+
 		Correlation: binary.LittleEndian.Uint32(b[4:8]),
-	}
+	}, nil
 
 }
