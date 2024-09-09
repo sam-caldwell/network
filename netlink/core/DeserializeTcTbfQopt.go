90c9e68f33c25f5fb93782985d85c5725507ef67
Sam Caldwell <github@samcaldwell.net>
2024-09-08 23:13:56 -0500

    fixes for DeserializeTcTbfQopt()

--- netlink/core/DeserializeTcTbfQopt.go
+++ netlink/core/DeserializeTcTbfQopt.go
@@ -7,25 +7,35 @@ import (
 )
 
 // DeserializeTcTbfQopt safely deserializes a byte slice into a TcTbfQopt object.
+//
+//	type TcTbfQopt struct {
+//	   Rate     TcRateSpec
+//	   Peakrate TcRateSpec
+//	   Limit    uint32
+//	   Buffer   uint32
+//	   Mtu      uint32
+//	}
 func DeserializeTcTbfQopt(b []byte) (*TcTbfQopt, error) {
 	if len(b) < SizeOfTcTbfQopt {
 		return nil, errors.New("DeserializeTcTbfQopt: insufficient byte slice size")
 	}
 
-	buf := bytes.NewReader(b)
 	tbf := &TcTbfQopt{}
 
-	// Use LittleEndian or BigEndian depending on your architecture
-	err := binary.Read(buf, binary.LittleEndian, &tbf.Rate)
+	temp, err := DeserializeTcRateSpec(b[0:SizeOfTcRateSpec])
 	if err != nil {
 		return nil, err
+	} else {
+		tbf.Rate = *temp
 	}
-
-	err = binary.Read(buf, binary.LittleEndian, &tbf.Peakrate)
+	temp, err = DeserializeTcRateSpec(b[SizeOfTcRateSpec : 2*SizeOfTcRateSpec])
 	if err != nil {
 		return nil, err
+	} else {
+		tbf.Peakrate = *temp
 	}
 
+	buf := bytes.NewReader(b[2*SizeOfTcRateSpec:])
 	err = binary.Read(buf, binary.LittleEndian, &tbf.Limit)
 	if err != nil {
 		return nil, err
