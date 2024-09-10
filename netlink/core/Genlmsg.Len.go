30801ad95c1b64caa6f56da4ffdee214dd3f5429
Sam Caldwell <github@samcaldwell.net>
2024-09-10 17:25:55 -0500

    cleanup Genlmsg

--- netlink/core/Genlmsg.Len.go
+++
@@ -1,8 +0,0 @@
-package core
-
-import "unsafe"
-
-// Len returns the length of the Genlmsg structure in bytes.
-func (msg *Genlmsg) Len() int {
-	return int(unsafe.Sizeof(Genlmsg{}))
-}
--- netlink/core/Genlmsg.go
+++ netlink/core/Genlmsg.go
@@ -1,9 +1,77 @@
 package core
 
-// Genlmsg represents a generic netlink message with a command and version.
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+)
+
+// Genlmsg represents the Generic Netlink message header (genlmsghdr) as defined in the Linux kernel.
+// This header is part of the Generic Netlink protocol, which is an extension of the Netlink socket interface
+// and is used to simplify communication between kernel modules and user-space applications.
+//
+// The structure corresponds to the `genlmsghdr` struct in the Linux kernel:
+// https://github.com/torvalds/linux/blob/master/include/uapi/linux/genetlink.h
+//
+// In C, the genlmsghdr structure is defined as follows:
+//
+//	struct genlmsghdr {
+//	   __u8    cmd;    /* Command identifier */
+//	   __u8    version; /* Interface version */
+//	};
+//
+// It is used in conjunction with a `nlmsghdr` to specify commands and messages for different subsystems
+// (families) in Generic Netlink communication.
+//
+// See Linux source: include/uapi/linux/genetlink.h
+//
+// Translation to Go:
+// - Command: corresponds to the `cmd` field in C, used to specify the command identifier.
+// - Version: corresponds to the `version` field in C, specifying the interface version.
 type Genlmsg struct {
-	// Command identifier
+	// Command - Command identifier for the Generic Netlink message.
+	// This corresponds to the `cmd` field in the C structure.
 	Command uint8
-	// Version of the interface
+
+	// Version - Interface version for the Generic Netlink message.
+	// This corresponds to the `version` field in the C structure.
 	Version uint8
+}
+
+// Len returns the length of the Genlmsg structure in bytes.
+// It calculates the length using the binary.Size function, which ensures safe and portable size calculation.
+func (msg *Genlmsg) Len() (int, error) {
+	size := binary.Size(*msg)
+	if size < 0 {
+		return 0, errors.New("unable to calculate the size of Genlmsg")
+	}
+	return size, nil
+}
+
+// Serialize converts the Genlmsg structure into a byte slice.
+func (msg *Genlmsg) Serialize() ([]byte, error) {
+
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, NativeEndian, msg.Command); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(buf, NativeEndian, msg.Version); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+
+}
+
+// DeserializeGenlMsg - creates a Genlmsg structure from a byte slice.  It returns an error if the byte slice is
+// of incorrect length.
+func DeserializeGenlMsg(b []byte) (*Genlmsg, error) {
+	if len(b) < 2 {
+		return nil, errors.New("byte slice size is too small")
+	}
+	return &Genlmsg{
+		Command: b[0],
+		Version: b[1],
+	}, nil
 }
