d0c662c88ee0428860dacc5c336ef06a0381392d
Sam Caldwell <github@samcaldwell.net>
2024-09-10 18:46:57 -0500

    add test for DevlinkEswitchInlineModeEnum

---
+++ netlink/core/DevlinkEswitchInlineModeEnum_test.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestDevlinkEswitchInlineModeEnum(t *testing.T) {
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 1
+		if sz := int(unsafe.Sizeof(DevlinkEswitchInlineModeEnum(0))); sz != expectedSizeInBytes {
+			t.Fatal("size check failed")
+		}
+	})
+	t.Run("value check", func(t *testing.T) {
+		type TestData struct {
+			actual DevlinkEswitchInlineModeEnum
+			expect DevlinkEswitchInlineModeEnum
+		}
+		testData := []TestData{
+			{actual: DevlinkEswitchInlineModeNone, expect: 0},
+			{actual: DevlinkEswitchInlineModeLink, expect: 1},
+			{actual: DevlinkEswitchInlineModeNetwork, expect: 2},
+			{actual: DevlinkEswitchInlineModeTransport, expect: 3},
+		}
+		for _, v := range testData {
+			if v.actual != v.expect {
+				t.Fatalf("value check failed. actual: %v, expect: %v", v.actual, v.expect)
+			}
+		}
+	})
+}
