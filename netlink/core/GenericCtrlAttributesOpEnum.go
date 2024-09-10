7c76a7e63adb054b676c7cea47c010b5105a0202
Sam Caldwell <github@samcaldwell.net>
2024-09-10 18:01:25 -0500

    rename GenericCtrlAttributesOpEnum

--- netlink/core/GenericCtrlAttributesOpEnum.go
+++ netlink/core/GenericCtrlAttributesOpEnum.go
@@ -1,25 +1,30 @@
 package core
 
-// GenCtrlAttributesOpEnum - Enumeration for Generic Netlink Control Operation Attributes
+// GenericCtrlAttributesOpEnum - Enumeration for Generic Netlink Control Operation Attributes
 //
 // This enumeration defines various attributes used in the Generic Netlink (Genl) control operations.
-// These attributes are utilized by the Generic Netlink controller to manage and query the operations of different Netlink families.
+// These attributes are utilized by the Generic Netlink controller to manage and query the operations of different
+// Netlink families.
 // See https://docs.kernel.org/networking/gtp.html
-type GenCtrlAttributesOpEnum uint8
+type GenericCtrlAttributesOpEnum uint8
 
 const (
 	// GenlCtrlAttrOpUnspec - Unspecified attribute.
 	// This is a placeholder for an unspecified or unknown attribute in the Generic Netlink control operation.
+	//
 	// See https://docs.kernel.org/networking/gtp.html
-	GenlCtrlAttrOpUnspec GenCtrlAttributesOpEnum = 0
+	GenlCtrlAttrOpUnspec GenericCtrlAttributesOpEnum = 0
 
 	// GenlCtrlAttrOpId - Operation identifier attribute.
 	// This attribute specifies the identifier for a particular operation in the Generic Netlink control.
+	//
 	// See https://docs.kernel.org/networking/gtp.html
-	GenlCtrlAttrOpId GenCtrlAttributesOpEnum = 1
+	GenlCtrlAttrOpId GenericCtrlAttributesOpEnum = 1
 
 	// GenlCtrlAttrOpFlags - Operation flags attribute.
-	// This attribute specifies the flags associated with a particular operation, defining its behavior or capabilities.
+	// This attribute specifies the flags associated with a particular operation, defining its behavior or
+	// capabilities.
+	//
 	// See https://docs.kernel.org/networking/gtp.html
-	GenlCtrlAttrOpFlags GenCtrlAttributesOpEnum = 2
+	GenlCtrlAttrOpFlags GenericCtrlAttributesOpEnum = 2
 )
