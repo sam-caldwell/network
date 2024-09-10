221667793134ddba4bbf5d1da6b5c39fd86cbf01
Sam Caldwell <github@samcaldwell.net>
2024-09-10 18:59:56 -0500

    add test for DevlinkParamTypeEnum

--- netlink/core/DevlinkParamTypeEnum.go
+++ netlink/core/DevlinkParamTypeEnum.go
@@ -1,21 +1,36 @@
 package core
 
-// DevlinkParamTypeEnum - datatype used in a given devlink parameter
+// DevlinkParamTypeEnum - Enum representing the data types used for devlink parameters.
+// These types correspond to the parameter types defined in the Linux kernel for devlink
+// (See: https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h).
+//
+// The devlink parameters provide a mechanism to manage and configure device settings
+// dynamically. Each parameter has an associated type that defines the kind of value it holds.
 type DevlinkParamTypeEnum uint8
 
 const (
-	// DevlinkParamTypeU8 - DEVLINK_PARAM_TYPE_U8 - uint8
+	// DevlinkParamTypeU8 - DEVLINK_PARAM_TYPE_U8
+	// Represents an unsigned 8-bit integer parameter (uint8).
+	// Defined in the Linux kernel: https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
 	DevlinkParamTypeU8 DevlinkParamTypeEnum = 1
 
-	// DevlinkParamTypeU16 - DEVLINK_PARAM_TYPE_U16 - uint16
+	// DevlinkParamTypeU16 - DEVLINK_PARAM_TYPE_U16
+	// Represents an unsigned 16-bit integer parameter (uint16).
+	// Defined in the Linux kernel: https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
 	DevlinkParamTypeU16 DevlinkParamTypeEnum = 2
 
-	// DevlinkParamTypeU32 - DEVLINK_PARAM_TYPE_U32 - uint32
+	// DevlinkParamTypeU32 - DEVLINK_PARAM_TYPE_U32
+	// Represents an unsigned 32-bit integer parameter (uint32).
+	// Defined in the Linux kernel: https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
 	DevlinkParamTypeU32 DevlinkParamTypeEnum = 3
 
-	// DevlinkParamTypeString - DEVLINK_PARAM_TYPE_STRING - string
+	// DevlinkParamTypeString - DEVLINK_PARAM_TYPE_STRING
+	// Represents a string parameter.
+	// Defined in the Linux kernel: https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
 	DevlinkParamTypeString DevlinkParamTypeEnum = 5
 
-	// DevlinkParamTypeBool - DEVLINK_PARAM_TYPE_BOOL - bool
+	// DevlinkParamTypeBool - DEVLINK_PARAM_TYPE_BOOL
+	// Represents a boolean parameter.
+	// Defined in the Linux kernel: https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
 	DevlinkParamTypeBool DevlinkParamTypeEnum = 6
 )
---
+++ netlink/core/DevlinkParamTypeEnum_test.go
@@ -0,0 +1,33 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestDevlinkParamTypeEnum(t *testing.T) {
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 1
+		if sz := int(unsafe.Sizeof(DevlinkParamTypeEnum(0))); sz != expectedSizeInBytes {
+			t.Fatal("size check failed")
+		}
+	})
+	t.Run("value check", func(t *testing.T) {
+		type TestData struct {
+			actual DevlinkParamTypeEnum
+			expect DevlinkParamTypeEnum
+		}
+		testData := []TestData{
+			{actual: DevlinkParamTypeU8, expect: 1},
+			{actual: DevlinkParamTypeU16, expect: 2},
+			{actual: DevlinkParamTypeU32, expect: 3},
+			{actual: DevlinkParamTypeString, expect: 5},
+			{actual: DevlinkParamTypeBool, expect: 6},
+		}
+		for _, v := range testData {
+			if v.actual != v.expect {
+				t.Fatalf("value check failed. actual: %v, expect: %v", v.actual, v.expect)
+			}
+		}
+	})
+}
