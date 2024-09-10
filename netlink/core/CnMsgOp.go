5e4a3a85995257e21d44937a509705052223c8a6
Sam Caldwell <github@samcaldwell.net>
2024-09-10 16:47:50 -0500

    code cleanup for CnMsgOp

--- netlink/core/CnMsgOp.go
+++ netlink/core/CnMsgOp.go
@@ -123,3 +123,12 @@ func (msg *CnMsgOp) Serialize() ([]byte, error) {
 
 	return buf, nil
 }
+
+// DeserializeCnMsgOp - Deserialize []byte into CnMsgOp
+func DeserializeCnMsgOp(b []byte) (*CnMsgOp, error) {
+	var o CnMsgOp
+	if err := o.Deserialize(b); err != nil {
+		return nil, err
+	}
+	return &o, nil
+}
