2c33c510c2b56a9b39027e4c3a6298dfefb8daf0
Sam Caldwell <github@samcaldwell.net>
2024-09-11 11:43:00 -0500

    add GetFromThread() and test

---
+++ namespace/GetFromThread.go
@@ -0,0 +1,12 @@
+package namespace
+
+import (
+	"fmt"
+)
+
+// GetFromThread - return the network namespace handle for the given process id (pid) and thread id (tid)
+func GetFromThread(pid, tid int) (Handle, error) {
+
+	return GetFromPath(fmt.Sprintf("/proc/%d/task/%d/ns/net", pid, tid))
+
+}
