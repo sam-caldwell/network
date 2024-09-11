d45eeef56f24002bf5747746f7cc2b5639092dc2
Sam Caldwell <github@samcaldwell.net>
2024-09-10 20:54:15 -0500

    add improved documentation to namespace package Linux.New()

--- namespace/Linux.New.go
+++ namespace/Linux.New.go
@@ -4,7 +4,6 @@ package namespace
 
 import (
 	"golang.org/x/sys/unix"
-	"runtime"
 )
 
 // New creates a new network namespace using the `unshare` system call.
@@ -24,9 +23,6 @@ import (
 //   - Handle: A handle to the new network namespace.
 //   - error: An error if the system call fails (e.g., insufficient privileges or unsupported kernel).
 func New() (Handle, error) {
-
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
 
 	// Call unshare to create a new network namespace (CLONE_NEWNET isolates the network namespace).
 	if err := unix.Unshare(unix.CLONE_NEWNET); err != nil {
