5784f5eebb252ba8f8e29a66bc0a2c7e0fd84a50
Sam Caldwell <github@samcaldwell.net>
2024-09-11 12:37:33 -0500

    add Set() and SetNamespace()

---
+++ namespace/SetNamespace.go
@@ -0,0 +1,16 @@
+package namespace
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// SetNamespace - set the namespace (using golang.org/x/sys/unix.Setns)
+//
+// We keep this wrapper around unix.Setns() because SetNamespace() is more readable
+// and less likely to be confused with a few other things we are building (e.g. name server).
+// ...and there's the golang love of changing things under us that make wrappers appealing.
+//
+// See https://man7.org/linux/man-pages/man2/setns.2.html
+func SetNamespace(namespace Handle, namespaceType Types) error {
+	return unix.Setns(int(namespace), int(namespaceType))
+}
