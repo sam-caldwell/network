9ff1598927ce5042d364c657a611f897c8db639f
Sam Caldwell <github@samcaldwell.net>
2024-09-10 19:13:40 -0500

    add test for DevlinkResourceUnitEnum

---
+++ netlink/core/DevlinkResourceUnitEnum_test.go
@@ -0,0 +1,29 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestDevlinkResourceUnitEnum(t *testing.T) {
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 1
+		if sz := int(unsafe.Sizeof(DevlinkResourceUnitEnum(0))); sz != expectedSizeInBytes {
+			t.Fatal("size check failed")
+		}
+	})
+	t.Run("value check", func(t *testing.T) {
+		type TestData struct {
+			actual DevlinkResourceUnitEnum
+			expect DevlinkResourceUnitEnum
+		}
+		testData := []TestData{
+			{actual: DevlinkResourceUnitEntry, expect: 0},
+		}
+		for _, v := range testData {
+			if v.actual != v.expect {
+				t.Fatalf("value check failed. actual: %v, expect: %v", v.actual, v.expect)
+			}
+		}
+	})
+}
