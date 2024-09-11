f635a710be9c58c1dc565f7a96e03bfbc90cac93
Sam Caldwell <github@samcaldwell.net>
2024-09-10 19:12:18 -0500

    add test for DevlinkPortTypeEnum

---
+++ netlink/core/DevlinkPortTypeEnum_test.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestDevlinkPortTypeEnum(t *testing.T) {
+	t.Run("size check", func(t *testing.T) {
+		const expectedSizeInBytes = 1
+		if sz := int(unsafe.Sizeof(DevlinkPortTypeEnum(0))); sz != expectedSizeInBytes {
+			t.Fatal("size check failed")
+		}
+	})
+	t.Run("value check", func(t *testing.T) {
+		type TestData struct {
+			actual DevlinkPortTypeEnum
+			expect DevlinkPortTypeEnum
+		}
+		testData := []TestData{
+			{actual: DevlinkPortTypeNotset, expect: 0},
+			{actual: DevlinkPortTypeAuto, expect: 1},
+			{actual: DevlinkPortTypeEth, expect: 2},
+			{actual: DevlinkPortTypeIb, expect: 3},
+		}
+		for _, v := range testData {
+			if v.actual != v.expect {
+				t.Fatalf("value check failed. actual: %v, expect: %v", v.actual, v.expect)
+			}
+		}
+	})
+}
