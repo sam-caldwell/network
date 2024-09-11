3eb2e9c9dadb6ee6b7c948c4f655ed919438d931
Sam Caldwell <github@samcaldwell.net>
2024-09-10 22:06:38 -0500

    code cleanup

--- namespace/Handle_test.go
+++ namespace/Handle_test.go
@@ -13,7 +13,7 @@ import (
 func TestHandle_Type(t *testing.T) {
 
 	t.Run("test Handle type size", func(t *testing.T) {
-		const expectedSizeInBytes = 8
+		const expectedSizeInBytes = int(unsafe.Sizeof(int(0)))
 		if sz := int(unsafe.Sizeof(Handle(0))); sz != expectedSizeInBytes {
 			t.Errorf("Expected size of %d, got %d", expectedSizeInBytes, sz)
 		}
