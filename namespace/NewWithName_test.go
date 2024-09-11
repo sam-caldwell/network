422dd25d05e7ad1168a593652cc75870b1dc455c
Sam Caldwell <github@samcaldwell.net>
2024-09-11 12:21:28 -0500

    add test for NewWithName()

---
+++ namespace/NewWithName_test.go
@@ -0,0 +1,24 @@
+//go:build linux
+
+package namespace
+
+import (
+	"github.com/sam-caldwell/network/test"
+	"testing"
+)
+
+func TestNewWithName(t *testing.T) {
+	const testDockerImage = "network-test:latest"
+
+	t.Run("build container", func(t *testing.T) {
+		if err := test.BuildTestContainer(testDockerImage); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("run test container", func(t *testing.T) {
+		if err := test.RunContainer(testDockerImage, "TestCreateNewNamespaceWithName"); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
