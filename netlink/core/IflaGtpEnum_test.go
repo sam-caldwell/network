b77d4efdaca49fc013c0d05bb2f63d0859543710
Sam Caldwell <github@samcaldwell.net>
2024-09-12 00:03:54 -0500

    add test for IflaGtpEnum

---
+++ netlink/core/IflaGtpEnum_test.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+// TestIflaGtpEnum tests the size of the IflaGtpEnum type and the values of its enumerated constants.
+func TestIflaGtpEnum(t *testing.T) {
+	// Verify the size of the IflaGtpEnum type is 1 byte (uint8)
+	if size := unsafe.Sizeof(IflaGtpUnspec); size != 1 {
+		t.Errorf("Expected size of IflaGtpEnum to be 1 byte, but got %d", size)
+	}
+
+	// Test the values of the constants
+	tests := []struct {
+		name  string
+		value IflaGtpEnum
+		want  IflaGtpEnum
+	}{
+		{"IflaGtpUnspec", IflaGtpUnspec, 0},
+		{"IflaGtpFd0", IflaGtpFd0, 1},
+		{"IflaGtpFd1", IflaGtpFd1, 2},
+		{"IflaGtpPdpHashsize", IflaGtpPdpHashsize, 3},
+		{"IflaGtpRole", IflaGtpRole, 4},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.value != tt.want {
+				t.Errorf("Expected %s to have value %d, but got %d", tt.name, tt.want, tt.value)
+			}
+		})
+	}
+}
