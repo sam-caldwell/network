0a64c3b231554ee4bbf098c322f2b22d4717f7ce
Sam Caldwell <github@samcaldwell.net>
2024-09-12 13:35:48 -0500

    fixes and tests (passing) for MplsStackDecode()

--- netlink/core/MplsStackDecode.go
+++ netlink/core/MplsStackDecode.go
@@ -1,30 +1,50 @@
 package core
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"log"
+)
 
 // MplsStackDecode - given a byte slice (inputBuffer) return a list of MplsLabels
-func MplsStackDecode(inputBuffer []byte) []MplsLabels {
+//
+// Reference: RFC 5462, RFC 3032
+//
+//	  0                   1                   2                   3
+//	  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//	 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//	 |                Label                  | TC  |S|       TTL     |
+//	 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//		Label:  Label Value, 20 bits
+//		TC:     Traffic Class field, 3 bits
+//		S:      Bottom of Stack, 1 bit
+//		TTL:    Time to Live, 8 bits
+func MplsStackDecode(inputBuffer []byte) []uint32 {
 
 	if len(inputBuffer)%4 != 0 {
+		log.Printf("return nil on %v", inputBuffer)
 		return nil
 	}
 
-	stack := make([]MplsLabels, 0, len(inputBuffer)/4)
+	sz := len(inputBuffer) / 4
+	stack := make([]uint32, 0, sz)
 
 	for len(inputBuffer) > 0 {
-
 		l := binary.BigEndian.Uint32(inputBuffer[:4])
-
 		inputBuffer = inputBuffer[4:]
 
-		stack = append(stack, MplsLabels(l)>>MplsLsLabelShift)
+		// Correct extraction of the MPLS label (upper 20 bits)
+		label := l >> MplsLsLabelShift
+		log.Printf("Label: %04x", label)
 
-		if (l>>MplsLsSShift)&1 > 0 {
+		// Append the label to the stack
+		stack = append(stack, label)
+
+		// If the bottom-of-stack bit is set, break the loop
+		if (l>>8)&1 > 0 {
 			break
 		}
-
 	}
 
 	return stack
-
 }
--- netlink/core/MplsStackDecode_test.go
+++ netlink/core/MplsStackDecode_test.go
@@ -8,36 +8,37 @@ func TestMplsStackDecode(t *testing.T) {
 	tests := []struct {
 		name      string
 		input     []byte
-		expected  []MplsLabels
+		expected  []uint32
 		expectNil bool
 	}{
 		{
+			//row:0
 			name: "Valid MPLS Stack",
+			// Each label is encoded into 4 bytes as:
+			// {0x001230FF, 0x002340FF, 0x003450FF, 0x004560FF}
+			// 20-bit labels: 0x123, 0x234, 0x345, 0x456 (last label has the Bottom of Stack bit set)
 			input: []byte{
-				0x00, 0x01, 0x23, 0x00, 0x00, 0x02, 0x34, 0x00,
-				0x00, 0x03, 0x45, 0x00, 0x00, 0x04, 0x56, 0x00,
+				//|<-----label (20 bit)----->|TCS|<---TTL--->|
+				//|             20           |3+1|           |
+				0b00000000, 0b00000000, 0b00000000, 0b11111111, // Label: 0x000, TC: 000, S: 0, TTL: 0xFF
+				0b00000000, 0b00010010, 0b00110000, 0b11111111, // Label: 0x123, TC: 000, S: 0, TTL: 0xFF
+				0b00000000, 0b00100011, 0b01000000, 0b11111111, // Label: 0x234, TC: 000, S: 0, TTL: 0xFF
+				0b00000000, 0b00110100, 0b01010000, 0b11111111, // Label: 0x345, TC: 000, S: 0, TTL: 0xFF
+				0b00000000, 0b00110101, 0b01011111, 0b11111111, // Label: 0x355, TC: FFF, S: 1, TTL: 0xFF
 			},
-			expected:  []MplsLabels{0x123, 0x234, 0x345, 0x456},
+			expected:  []uint32{0x000, 0x123, 0x234, 0x345, 0x355},
 			expectNil: false,
 		},
 		{
+			//row:1
 			name:      "Non-multiple of 4 bytes",
-			input:     []byte{0x00, 0x01, 0x23, 0x00, 0x00},
+			input:     []byte{0x00, 0x01, 0x23, 0xFF, 0x00},
 			expected:  nil,
 			expectNil: true,
 		},
-		{
-			name: "MPLS stack with bottom-of-stack (S bit set)",
-			input: []byte{
-				0x00, 0x01, 0x23, 0x00, 0x00, 0x02, 0x34, 0x00,
-				0x00, 0x03, 0x45, 0x00, 0x00, 0x04, 0x56, 0x80,
-			},
-			expected:  []MplsLabels{0x123, 0x234, 0x345, 0x456},
-			expectNil: false,
-		},
 	}
 
-	equalMplsLabelStacks := func(a, b []MplsLabels) bool {
+	equalMplsLabelStacks := func(a, b []uint32) bool {
 		if len(a) != len(b) {
 			return false
 		}
@@ -49,13 +50,13 @@ func TestMplsStackDecode(t *testing.T) {
 		return true
 	}
 
-	for _, tt := range tests {
+	for row, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			result := MplsStackDecode(tt.input)
 			if tt.expectNil && result != nil {
-				t.Errorf("Expected nil, got %v", result)
+				t.Errorf("(row:%d) Expected nil, got %v", row, result)
 			} else if !tt.expectNil && !equalMplsLabelStacks(result, tt.expected) {
-				t.Errorf("Expected %v, got %v", tt.expected, result)
+				t.Errorf("(row:%d) Expected %v, got %v", row, tt.expected, result)
 			}
 		})
 	}
