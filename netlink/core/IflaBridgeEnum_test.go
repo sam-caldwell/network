9e4176c9fbac1ebeb1b11e8176c15491fb9f8bd1
Sam Caldwell <github@samcaldwell.net>
2024-09-11 23:57:20 -0500

    add IflaBridgeEnum test

---
+++ netlink/core/IflaBridgeEnum_test.go
@@ -0,0 +1,38 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+// TestIflaBridgeEnum tests the size of the IflaBridgeEnum type and the values of its enumerated constants.
+func TestIflaBridgeEnum(t *testing.T) {
+	// Verify the size of the IflaBridgeEnum type is 4 bytes (int)
+	if size := unsafe.Sizeof(IflaBridgeFlags); size != 4 {
+		t.Errorf("Expected size of IflaBridgeEnum to be 4 bytes, but got %d", size)
+	}
+
+	// Test the values of the constants
+	tests := []struct {
+		name  string
+		value IflaBridgeEnum
+		want  IflaBridgeEnum
+	}{
+		{"IflaBridgeFlags", IflaBridgeFlags, 0},
+		{"IflaBridgeMode", IflaBridgeMode, 1},
+		{"IflaBridgeVlanInfo", IflaBridgeVlanInfo, 2},
+		{"IflaBridgeVlanTunnelInfo", IflaBridgeVlanTunnelInfo, 3},
+		{"IflaBridgeMrp", IflaBridgeMrp, 4},
+		{"IflaBridgeCfm", IflaBridgeCfm, 5},
+		{"IflaBridgeMst", IflaBridgeMst, 6},
+		{"IflaBridgeMax", IflaBridgeMax, 6}, // Maximum value equals IflaBridgeMst
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.value != tt.want {
+				t.Errorf("Expected %s to have value %d, but got %d", tt.name, tt.want, tt.value)
+			}
+		})
+	}
+}
