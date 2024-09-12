5ed110c8868e7c37aff74d3ba392141f768c05a7
Sam Caldwell <github@samcaldwell.net>
2024-09-12 12:00:45 -0500

    add test for MplsLabels

---
+++ netlink/core/MplsLabels_test.go
@@ -0,0 +1,40 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestMplsLabels(t *testing.T) {
+	// Subtest for checking the size of the MplsLabels type
+	t.Run("TestSize", func(t *testing.T) {
+		const expectedSizeInBytes = 4
+		var label MplsLabels
+
+		if size := unsafe.Sizeof(label); size != uintptr(expectedSizeInBytes) {
+			t.Errorf("Expected size %d but got %d", expectedSizeInBytes, size)
+		}
+	})
+
+	// Subtest for checking the values of the constants
+	t.Run("TestValues", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			value    MplsLabels
+			expected MplsLabels
+		}{
+			{"MplsLsLabelShift", MplsLsLabelShift, 12},
+			{"MplsLsTcShift", MplsLsTcShift, 9},
+			{"MplsLsSShift", MplsLsSShift, 8},
+			{"MplsLsTtlShift", MplsLsTtlShift, 0},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				if test.value != test.expected {
+					t.Errorf("Expected %d but got %d for %s", test.expected, test.value, test.name)
+				}
+			})
+		}
+	})
+}
