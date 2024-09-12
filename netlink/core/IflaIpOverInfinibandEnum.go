f70bdac0b135cb0b9ec05d83d64c72acf72b1c6d
Sam Caldwell <github@samcaldwell.net>
2024-09-12 00:13:44 -0500

    add tests and fixes for IflaIpOverInfinibandEnum

---
+++ netlink/core/IflaIpOverInfinibandEnum.go
@@ -0,0 +1,41 @@
+package core
+
+// IflaIpOverInfinibandEnum - Enumeration for IP-over-InfiniBand (IpOIb) interface attributes.
+//
+// These attributes are used to configure and manage IP-over-InfiniBand (IpOIb) interfaces in the Linux kernel.
+// IpOverInfiniband is a network protocol that allows IP traffic to be sent over InfiniBand networks.
+//
+// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+type IflaIpOverInfinibandEnum uint8
+
+const (
+	// IflaIpOverInfinibandUnspec - IFLA_IpOverInfiniband_UNSPEC - Unspecified attribute, used as a placeholder
+	// for unknown or default values.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaIpOverInfinibandUnspec IflaIpOverInfinibandEnum = iota
+
+	// IflaIpOverInfinibandPkey - IFLA_IpOverInfiniband_PKEY - Specifies the Partition Key (PKEY) for the
+	// IpOverInfiniband interface.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaIpOverInfinibandPkey
+
+	// IflaIpOverInfinibandMode - IFLA_IpOverInfiniband_MODE - Specifies the mode of the IpOverInfiniband
+	// interface (e.g., connected or datagram mode).
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaIpOverInfinibandMode
+
+	// IflaIpOverInfinibandUmcast - IFLA_IpOverInfiniband_UMCAST - Indicates whether multicast support is enabled
+	// for the IpOverInfiniband interface.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaIpOverInfinibandUmcast
+
+	// IflaIpOverInfinibandMax - IFLA_IpOverInfiniband_MAX - The maximum value for IpOverInfiniband attributes,
+	// used for validation purposes.
+	//
+	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
+	IflaIpOverInfinibandMax = iota - 1
+)
