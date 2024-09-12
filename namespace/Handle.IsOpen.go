cf79a3a87e456f17c5a49d35521497160ace67a7
Sam Caldwell <github@samcaldwell.net>
2024-09-11 21:04:43 -0500

    Rename Handle.IsOpen() receiver

--- namespace/Handle.IsOpen.go
+++ namespace/Handle.IsOpen.go
@@ -1,6 +1,6 @@
 package namespace
 
 // IsOpen returns true if Close() the handle has not been closed (-1)
-func (ns *Handle) IsOpen() bool {
-	return *ns != closedHandle
+func (h *Handle) IsOpen() bool {
+	return *h != closedHandle
 }
