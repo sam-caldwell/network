aee0024980c465fa98d32ee6a71e3b6f2e9745ea
Sam Caldwell <github@samcaldwell.net>
2024-09-11 21:01:33 -0500

    add GetFromPath() and GetFromName() with passing tests

--- namespace/GetFromPath.go
+++ namespace/GetFromPath.go
@@ -8,16 +8,11 @@ import (
 // existing namespace file handle from the namespaces map, or if none is found or the found file handle is closed,
 // open the path and return its file handle.
 func GetFromPath(path string) (Handle, error) {
-	lock.Lock()
-	defer lock.Unlock()
-	if existingHandle, found := namespaces[path]; found {
-		return existingHandle, nil
-	}
+
 	fd, err := unix.Open(path, unix.O_RDONLY|unix.O_CLOEXEC, 0)
-	newHandle := Handle(fd)
 	if err != nil {
-		return newHandle, err
+		return -1, err
 	}
-	namespaces[path] = newHandle
-	return newHandle, err
+	return Handle(fd), nil
+
 }
