2332d23d3708068c9055352008e5fcb707296520
Sam Caldwell <github@samcaldwell.net>
2024-09-12 10:10:48 -0500

    add tests for InterfaceFamily values and method

--- netlink/core/InterfaceFamily.Equal.go
+++
@@ -1,8 +0,0 @@
-package core
-
-// Equal - Compare two InterfaceFamily objects and return boolean indicating equivalence.
-func (lhs *InterfaceFamily) Equal(rhs InterfaceFamily) bool {
-
-	return *lhs == rhs
-
-}
--- netlink/core/InterfaceFamily.ToUint8.go
+++
@@ -1,6 +0,0 @@
-package core
-
-// ToUint8 - Convert InterfaceFamily value to uint8
-func (lhs *InterfaceFamily) ToUint8() uint8 {
-	return uint8(*lhs)
-}
