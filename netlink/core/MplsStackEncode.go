efa74b280d5e7234e7a704cbc2c5f03f5b7e3d68
Sam Caldwell <github@samcaldwell.net>
2024-09-12 14:19:26 -0500

    add fixes and test for MplsStackEncode()

--- netlink/core/MplsStackEncode.go
+++ netlink/core/MplsStackEncode.go
@@ -3,24 +3,36 @@ package core
 import "encoding/binary"
 
 // MplsStackEncode - given a list of MplsLabels, return a serialized byte slice.
-func MplsStackEncode(labels ...MplsLabels) []byte {
+//
+// Reference: RFC 5462, RFC 3032
+//
+//	  0                   1                   2                   3
+//	  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//	 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//	 |                Label                  | TC  |S|       TTL     |
+//	 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//		Label:  Label Value, 20 bits
+//		TC:     Traffic Class field, 3 bits
+//		S:      Bottom of Stack, 1 bit
+//		TTL:    Time to Live, 8 bits
+func MplsStackEncode(labels ...uint32) []byte {
 
-	b := make([]byte, 4*len(labels))
+	const sBit = 1 << MplsLsSShift
+
+	output := make([]byte, 4*len(labels)) // pre-allocate 4 bytes per uint32 element in the input.
 
 	for idx, label := range labels {
+		shiftedLabel := label << MplsLsLabelShift
 
-		l := label << MplsLsLabelShift
-
+		// Set the S bit (Bottom of Stack) for the last label in the third byte (8th bit of 3rd byte)
 		if idx == len(labels)-1 {
-
-			l |= 1 << MplsLsSShift
-
+			shiftedLabel |= sBit
 		}
 
-		binary.BigEndian.PutUint32(b[idx*4:], uint32(l))
-
+		// Set the value in the byte slice
+		binary.BigEndian.PutUint32(output[idx*4:], shiftedLabel)
 	}
 
-	return b
-
+	return output
 }
--- netlink/core/MplsStackEncode_test.go
+++ netlink/core/MplsStackEncode_test.go
@@ -1,42 +1,67 @@
 package core
 
-//
-//func TestMplsStackEncode(t *testing.T) {
-//	tests := []struct {
-//		name     string
-//		input    []MplsLabels
-//		expected []byte
-//	}{
-//		{
-//			name:     "Valid MPLS Stack Encoding",
-//			input:    []MplsLabels{0x123, 0x234, 0x345, 0x456},
-//			expected: []byte{0x00, 0x01, 0x23, 0x00, 0x00, 0x02, 0x34, 0x00, 0x00, 0x03, 0x45, 0x00, 0x00, 0x04, 0x56, 0x80},
-//		},
-//		{
-//			name:     "Empty MPLS Stack",
-//			input:    []MplsLabels{},
-//			expected: []byte{},
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			result := MplsStackEncode(tt.input...)
-//			if len(result) != len(tt.expected) || !equalByteSlices(result, tt.expected) {
-//				t.Errorf("Expected %v, got %v", tt.expected, result)
-//			}
-//		})
-//	}
-//}
-//
-//func equalByteSlices(a, b []byte) bool {
-//	if len(a) != len(b) {
-//		return false
-//	}
-//	for i := range a {
-//		if a[i] != b[i] {
-//			return false
-//		}
-//	}
-//	return true
-//}
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMplsStackEncode(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []uint32
+		expected []byte
+	}{
+		{
+			name:  "Valid MPLS Stack Encoding",
+			input: []uint32{0x000, 0x123, 0x234, 0x345, 0x355},
+			expected: []byte{
+				//|<-----label (20 bit)----->|TCS|<---TTL--->|
+				//|             20           |3+1|           |
+				0b00000000, 0b00000000, 0b00000000, 0b00000000, // Label: 0x000, TC: 000, S: 0, TTL: 0x00
+				0b00000000, 0b00010010, 0b00110000, 0b00000000, // Label: 0x123, TC: 000, S: 0, TTL: 0x00
+				0b00000000, 0b00100011, 0b01000000, 0b00000000, // Label: 0x234, TC: 000, S: 0, TTL: 0x00
+				0b00000000, 0b00110100, 0b01010000, 0b00000000, // Label: 0x345, TC: 000, S: 0, TTL: 0x00
+				0b00000000, 0b00110101, 0b01010001, 0b00000000, // Label: 0x355, TC: 000, S: 1, TTL: 0x00
+			},
+		},
+		{
+			name:     "Empty MPLS Stack",
+			input:    []uint32{},
+			expected: []byte{},
+		},
+	}
+
+	equalByteSlices := func(a, b []byte) bool {
+		if len(a) != len(b) {
+			return false
+		}
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+		return true
+	}
+
+	binaryPrint := func(data []byte) string {
+		s := ""
+		for i, b := range data {
+			s += fmt.Sprintf("%08b ", b)
+			if (i+1)%4 == 0 {
+				s += "\n"
+			}
+		}
+		return s
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := MplsStackEncode(tt.input...)
+			if !equalByteSlices(result, tt.expected) {
+				t.Fatalf("MplsStackEncode() failure\n"+
+					"expected: %s\n"+
+					"  result: %s", binaryPrint(tt.expected), binaryPrint(result))
+			}
+		})
+	}
+}
