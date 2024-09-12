00cdea359feadee52b7124d0f2c8ab6e65b52fd4
Sam Caldwell <github@samcaldwell.net>
2024-09-11 22:17:22 -0500

    add test for GenlGtpCmdEnum

---
+++ netlink/core/GenlGtpCmdEnum_test.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+// TestGenlGtpCmdEnum tests the size of the GenlGtpCmdEnum type
+// and the values of its enumerated constants.
+func TestGenlGtpCmdEnum(t *testing.T) {
+	// Verify the size of the GenlGtpCmdEnum type is 1 byte (uint8)
+	if size := unsafe.Sizeof(GenlGtpCmdNewpdp); size != 1 {
+		t.Errorf("Expected size of GenlGtpCmdEnum to be 1 byte, but got %d", size)
+	}
+
+	// Test the values of the constants
+	tests := []struct {
+		name  string
+		value GenlGtpCmdEnum
+		want  GenlGtpCmdEnum
+	}{
+		{"GenlGtpCmdNewpdp", GenlGtpCmdNewpdp, 0},
+		{"GenlGtpCmdDelpdp", GenlGtpCmdDelpdp, 1},
+		{"GenlGtpCmdGetpdp", GenlGtpCmdGetpdp, 2},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.value != tt.want {
+				t.Errorf("Expected %s to have value %d, but got %d", tt.name, tt.want, tt.value)
+			}
+		})
+	}
+}
