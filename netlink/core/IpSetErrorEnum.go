8074699f460c6d944a7993195a4d4a51913c628f
Sam Caldwell <github@samcaldwell.net>
2024-09-12 11:20:31 -0500

    code cleanup

--- netlink/core/IpSetErrorEnum.go
+++ netlink/core/IpSetErrorEnum.go
@@ -1,5 +1,10 @@
 package core
 
+import (
+	"errors"
+	"strconv"
+)
+
 // IpSetErrorEnum -  represents an error type for IP sets in the Netlink interface.
 //
 // In the context of Netlink communication, IP sets are used to manage sets of IP addresses, ports, and other elements in the kernel.
@@ -73,3 +78,54 @@ const (
 	//IpsetErrTypeSpecific - IPSET_ERR_TYPE_SPECIFIC -  Type specific error codes
 	IpsetErrTypeSpecific = 4352
 )
+
+// Error - Return the error for a given IpSetError
+func (e IpSetErrorEnum) Error() error {
+	return errors.New(e.ErrorString())
+}
+
+// ErrorString - Return the string error for a given IpSetError
+func (e IpSetErrorEnum) ErrorString() string {
+	switch int(e) {
+	case IpsetErrPrivate:
+		return "private"
+	case IpsetErrProtocol:
+		return "invalid protocol"
+	case IpsetErrFindType:
+		return "invalid type"
+	case IpsetErrMaxSets:
+		return "max sets reached"
+	case IpsetErrBusy:
+		return "busy"
+	case IpsetErrExistSetname2:
+		return "exist_setname2"
+	case IpsetErrTypeMismatch:
+		return "type mismatch"
+	case IpsetErrExist:
+		return "exist"
+	case IpsetErrInvalidCidr:
+		return "invalid cidr"
+	case IpsetErrInvalidNetmask:
+		return "invalid netmask"
+	case IpsetErrInvalidFamily:
+		return "invalid family"
+	case IpsetErrTimeout:
+		return "timeout"
+	case IpsetErrReferenced:
+		return "referenced"
+	case IpsetErrIpaddrIpv4:
+		return "invalid ipv4 address"
+	case IpsetErrIpaddrIpv6:
+		return "invalid ipv6 address"
+	case IpsetErrCounter:
+		return "invalid counter"
+	case IpsetErrComment:
+		return "invalid comment"
+	case IpsetErrInvalidMarkmask:
+		return "invalid markmask"
+	case IpsetErrSkbinfo:
+		return "skbinfo"
+	default:
+		return "errno " + strconv.Itoa(int(e))
+	}
+}
