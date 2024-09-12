36d1d47f72ed5b693717b6b60124734e4f498289
Sam Caldwell <github@samcaldwell.net>
2024-09-12 10:44:55 -0500

    rename IpSetProtocol

---
+++ netlink/core/IpSetProtocol.go
@@ -0,0 +1,15 @@
+package core
+
+const (
+	// IpSetProtocol - IpSet_PROTOCOL - The protocol version
+	IpSetProtocol = 7
+
+	// IpSetProtocolMin - IpSet_PROTOCOL_MIN -  The protocol version minimum
+	IpSetProtocolMin = 6
+
+	// IpSetMaxnamelen - IpSet_MAXNAMELEN - Max length of strings including NUL: set and type identifiers
+	IpSetMaxnamelen = 32
+
+	// IpSetMaxCommentSize - IpSet_MAX_COMMENT_SIZE - Maximum permissible comment length we will accept over netlink
+	IpSetMaxCommentSize = 255
+)
