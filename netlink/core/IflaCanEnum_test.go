ba638befd24918f56086691c2fa93324e92e32b2
Sam Caldwell <github@samcaldwell.net>
2024-09-11 23:59:13 -0500

    add test for IflaCanEnum

---
+++ netlink/core/IflaCanEnum_test.go
@@ -0,0 +1,47 @@
+package core
+
+import (
+	"testing"
+	"unsafe"
+)
+
+// TestIflaCanEnum tests the size of the IflaCanEnum type and the values of its enumerated constants.
+func TestIflaCanEnum(t *testing.T) {
+	// Verify the size of the IflaCanEnum type is 1 byte (uint8)
+	if size := unsafe.Sizeof(IflaCanUnspec); size != 1 {
+		t.Errorf("Expected size of IflaCanEnum to be 1 byte, but got %d", size)
+	}
+
+	// Test the values of the constants
+	tests := []struct {
+		name  string
+		value IflaCanEnum
+		want  IflaCanEnum
+	}{
+		{"IflaCanUnspec", IflaCanUnspec, 0},
+		{"IflaCanBittiming", IflaCanBittiming, 1},
+		{"IflaCanBittimingConst", IflaCanBittimingConst, 2},
+		{"IflaCanClock", IflaCanClock, 3},
+		{"IflaCanState", IflaCanState, 4},
+		{"IflaCanCtrlmode", IflaCanCtrlmode, 5},
+		{"IflaCanRestartMs", IflaCanRestartMs, 6},
+		{"IflaCanRestart", IflaCanRestart, 7},
+		{"IflaCanBerrCounter", IflaCanBerrCounter, 8},
+		{"IflaCanDataBittiming", IflaCanDataBittiming, 9},
+		{"IflaCanDataBittimingConst", IflaCanDataBittimingConst, 10},
+		{"IflaCanTermination", IflaCanTermination, 11},
+		{"IflaCanTerminationConst", IflaCanTerminationConst, 12},
+		{"IflaCanBitrateConst", IflaCanBitrateConst, 13},
+		{"IflaCanDataBitrateConst", IflaCanDataBitrateConst, 14},
+		{"IflaCanBitrateMax", IflaCanBitrateMax, 15},
+		{"IflaCanMax", IflaCanMax, 15}, // Max equals the last value
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.value != tt.want {
+				t.Errorf("Expected %s to have value %d, but got %d", tt.name, tt.want, tt.value)
+			}
+		})
+	}
+}
