130cc2d28a01caea8234ca57469733fcd2e34c66
Sam Caldwell <github@samcaldwell.net>
2024-09-13 13:59:05 -0500

    add constant to indicate when minSize or maxSize should not be evaluated

--- netlink/core/checkInputSize.go
+++ netlink/core/checkInputSize.go
@@ -4,15 +4,19 @@ import (
 	"errors"
 )
 
+const (
+	disableSizeCheck = -1
+)
+
 // checkInputSize - Check the input size against
 func checkInputSize[T []byte | string](input T, minSize, maxSize int) error {
 	if []byte(input) == nil {
 		return errors.New(ErrNilInput)
 	}
-	if minSize >= 0 && len(input) < minSize {
+	if minSize != disableSizeCheck && len(input) < minSize {
 		return errors.New(ErrInputTooShort)
 	}
-	if maxSize >= 0 && len(input) > maxSize {
+	if maxSize != disableSizeCheck && len(input) > maxSize {
 		return errors.New(ErrInputTooLarge)
 	}
 	return nil
