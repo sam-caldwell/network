d0304597278783c3b1f0e6c6b10049b7632f80f3
Sam Caldwell <github@samcaldwell.net>
2024-09-13 15:24:26 -0500

    code cleanup

--- netlink/core/IfInfoMsg.go
+++ netlink/core/IfInfoMsg.go
@@ -39,6 +39,15 @@ type IfInfoMsg struct {
 // SizeOfIfInfoMsg -  Size of the IfInfoMsg struct
 const SizeOfIfInfoMsg = int(unsafe.Sizeof(IfInfoMsg{}))
 
+// NewIfInfomsg - Create an IfInfoMsg with family specified.
+func NewIfInfomsg(family InterfaceFamily) *IfInfoMsg {
+	return &IfInfoMsg{
+		IfInfomsg: unix.IfInfomsg{
+			Family: uint8(family),
+		},
+	}
+}
+
 // Len - Return IfInfoMsg length
 func (msg *IfInfoMsg) Len() int {
 	return SizeOfIfInfoMsg
