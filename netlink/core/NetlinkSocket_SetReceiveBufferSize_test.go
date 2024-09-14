5b9c64fb473fc9cd5cc2592e562311dc7311c0cf
Sam Caldwell <github@samcaldwell.net>
2024-09-14 10:09:01 -0500

    add test for NetlinkSocket.SetReceiveBufferSize()

---
+++ netlink/core/NetlinkSocket_SetReceiveBufferSize_test.go
@@ -0,0 +1,48 @@
+package core
+
+import (
+	"golang.org/x/sys/unix"
+	"testing"
+)
+
+func TestNetlinkSocket_SetReceiveBufferSize(t *testing.T) {
+	tests := []struct {
+		name        string
+		size        int
+		force       bool
+		expectedErr error
+	}{
+		{
+			name:        "set receive buffer size",
+			size:        4096,
+			force:       false,
+			expectedErr: nil,
+		},
+		{
+			name:        "force set receive buffer size",
+			size:        8192,
+			force:       true,
+			expectedErr: nil,
+		},
+	}
+
+	// Create a new socket for testing
+	fd, err := unix.Socket(unix.AF_NETLINK, unix.SOCK_RAW, unix.NETLINK_GENERIC)
+	if err != nil {
+		t.Fatalf("failed to create socket: %v", err)
+	}
+	defer func() { _ = unix.Close(fd) }()
+
+	mockSocket := &NetlinkSocket{fd: fd}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := mockSocket.SetReceiveBufferSize(tt.size, tt.force)
+
+			// Check for errors
+			if (err != nil) != (tt.expectedErr != nil) {
+				t.Errorf("SetReceiveBufferSize() error = %v, expectedErr %v", err, tt.expectedErr)
+			}
+		})
+	}
+}
