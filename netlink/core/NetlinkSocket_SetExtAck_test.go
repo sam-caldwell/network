ad40cdf1cccb3ad2706aa433196039b3763a4193
Sam Caldwell <github@samcaldwell.net>
2024-09-13 22:48:55 -0500

    add test for NetlinkSocket.SetExtAck()

--- netlink/core/NetlinkSocket_SetExtAck_test.go
+++ netlink/core/NetlinkSocket_SetExtAck_test.go
@@ -5,20 +5,6 @@ import (
 	"testing"
 )
 
-// MockNetlinkSocket for testing purposes
-type MockNetlinkSocket struct {
-	fd int
-}
-
-// SetExtAck - mock implementation of the SetExtAck method for testing
-func (s *MockNetlinkSocket) SetExtAck(enable bool) error {
-	var n int
-	if enable {
-		n = 1
-	}
-	return unix.SetsockoptInt(s.fd, unix.SOL_NETLINK, unix.NETLINK_EXT_ACK, n)
-}
-
 func TestNetlinkSocket_SetExtAck(t *testing.T) {
 	tests := []struct {
 		name        string
