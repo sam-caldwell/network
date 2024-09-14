4564ad5a06835ccd3ce8425d4215286931f9f171
Sam Caldwell <github@samcaldwell.net>
2024-09-13 21:55:23 -0500

    WIP: disabled tests for Execute and ExecuteIter()

--- netlink/core/NetlinkRequest.ExecuteIter_test.go
+++ netlink/core/NetlinkRequest.ExecuteIter_test.go
@@ -5,5 +5,43 @@ import (
 )
 
 func TestNetlinkRequestExecuteIter(t *testing.T) {
-	t.Skip("not implemented")
+
+	t.Skip("disabled")
+
+	const testDockerImage = "network-test:latest"
+
+	t.Run("Test NetlinkRequest.ExecuteIter() method happy path", func(t *testing.T) {
+		t.Skip("disabled")
+
+		//t.Run("build container", func(t *testing.T) {
+		//	if err := test.BuildTestContainer(testDockerImage); err != nil {
+		//		t.Fatalf("container build error: %v", err)
+		//	}
+		//})
+		//
+		//t.Run("run test container", func(t *testing.T) {
+		//	if err := test.RunContainer(testDockerImage, "TestNetlinkRequestExecuteIterFuncHappy"); err != nil {
+		//		t.Fatalf("test container failed: %v", err)
+		//	}
+		//})
+
+	})
+
+	t.Run("Test NetlinkRequest.ExecuteIter() method error scenario", func(t *testing.T) {
+		t.Skip("disabled")
+
+		//t.Run("build container", func(t *testing.T) {
+		//	if err := test.BuildTestContainer(testDockerImage); err != nil {
+		//		t.Fatalf("container build error: %v", err)
+		//	}
+		//})
+		//
+		//t.Run("run test container", func(t *testing.T) {
+		//	err := test.RunContainer(testDockerImage, "TestNetlinkRequestExecuteIterFuncErrorScenario")
+		//	if err != nil {
+		//		t.Fatalf("test container failed: %v", err)
+		//	}
+		//})
+
+	})
 }
--- netlink/core/NetlinkRequest.Execute_test.go
+++ netlink/core/NetlinkRequest.Execute_test.go
@@ -4,6 +4,40 @@ import (
 	"testing"
 )
 
-func TestNetlinkRequestExecute(t *testing.T) {
-	t.Skip("not implemented")
+func TestNetlinkRequestExecuteWithMock(t *testing.T) {
+
+	t.Run("Test NetlinkRequest.Execute() method happy path", func(t *testing.T) {
+		t.Skip("disabled")
+
+		//t.Run("build container", func(t *testing.T) {
+		//	if err := test.BuildTestContainer(testDockerImage); err != nil {
+		//		t.Fatalf("container build error: %v", err)
+		//	}
+		//})
+		//
+		//t.Run("run test container", func(t *testing.T) {
+		//	if err := test.RunContainer(testDockerImage, "TestNetlinkRequestExecuteFuncHappy"); err != nil {
+		//		t.Fatalf("test container failed: %v", err)
+		//	}
+		//})
+
+	})
+
+	t.Run("Test NetlinkRequest.Execute() method error scenario", func(t *testing.T) {
+		t.Skip("disabled")
+
+		//t.Run("build container", func(t *testing.T) {
+		//	if err := test.BuildTestContainer(testDockerImage); err != nil {
+		//		t.Fatalf("container build error: %v", err)
+		//	}
+		//})
+		//
+		//t.Run("run test container", func(t *testing.T) {
+		//	err := test.RunContainer(testDockerImage, "TestNetlinkRequestExecuteFuncErrorScenario")
+		//	if err != nil {
+		//		t.Fatalf("test container failed: %v", err)
+		//	}
+		//})
+
+	})
 }
