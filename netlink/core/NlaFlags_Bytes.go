8f0c84153345b36695a9eedea49367380258fc22
Sam Caldwell <github@samcaldwell.net>
2024-09-14 13:00:08 -0500

    add NlaFlags.Bytes() and test

--- netlink/core/NlaFlags_Bytes.go
+++ netlink/core/NlaFlags_Bytes.go
@@ -4,7 +4,7 @@ import (
 	"github.com/sam-caldwell/convert"
 )
 
-// Bytes - Convert NlaFlags (uint16) to bytes
+// Bytes - Convert NlaFlags (uint16) to bytes.  Uses native endianness.
 func (m *NlaFlags) Bytes() []byte {
 
 	return convert.Uint16ToBytes(uint16(*m))
