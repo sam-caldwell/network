fa8ac36c56686fee616a8c0b2033fdbb70cb5f63
Sam Caldwell <github@samcaldwell.net>
2024-09-14 21:35:44 -0500

    add documentation for NlaFlags

--- netlink/core/NlaFlags.go
+++ netlink/core/NlaFlags.go
@@ -6,15 +6,26 @@ type NlaFlags uint16
 
 const (
 
-	// NlaFNested - NLA_F_NESTED (1 << 15)
-	NlaFNested NlaFlags = 32768
+	// NlaFNested - NLA_F_NESTED (1 << 15) 0x8000
+	NlaFNested NlaFlags = 0b1000000000000000
 
-	// NlaFNetByteorder - NLA_F_NET_BYTEORDER (1 << 14)
-	NlaFNetByteorder NlaFlags = 16384
+	// NlaFNetByteorder - NLA_F_NET_BYTEORDER (1 << 14) 0x4000
+	NlaFNetByteorder NlaFlags = 0b0100000000000000
 
-	// NlaTypeMask - NLA_TYPE_MASK - Mask bits for the NLA type
+	// NlaTypeMask - NLA_TYPE_MASK - Mask bits for the NLA type (0x3FFF)
+	//
+	//    a   0b1000000000000000 = 0x8000
+	//    b   0b0100000000000000 = 0x4000
+	//  -------------------------
+	//   a|b  0b1100000000000000 = 0xC000
+	// ^(a|b) 0b0011111111111111 = 0x3FFF
 	NlaTypeMask = ^(NlaFNested | NlaFNetByteorder)
 
 	// NlaAlignto - NLA_ALIGNTO 4
-	NlaAlignto NlaFlags = NlaFlags(unsafe.Sizeof(NlaTypeMask) * 2)
+	//
+	//   sizeOf(NlaTypeMask) = 2
+	// x unused 32bit digits = 2
+	// -------------------------
+	//            NlaAlignto = 4
+	NlaAlignto = NlaFlags(unsafe.Sizeof(NlaTypeMask) * 2)
 )
