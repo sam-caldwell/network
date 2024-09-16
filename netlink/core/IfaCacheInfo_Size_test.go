d1ba297175b7a63052ff1b97f28d3dea583b6089
Sam Caldwell <github@samcaldwell.net>
2024-09-15 22:58:46 -0500

    cleanup

--- netlink/core/IfaCacheInfo_Size_test.go
+++ netlink/core/IfaCacheInfo_Size_test.go
@@ -8,7 +8,7 @@ import (
 func TestIfaCacheInfo_Size(t *testing.T) {
 
 	const expectedSizeInBytes = int(unsafe.Sizeof(IfaCacheInfo{}))
-	if IfaCacheInfoSize != int(unsafe.Sizeof(IfaCacheInfo{})) {
+	if IfaCacheInfoSize != expectedSizeInBytes {
 		t.Fatalf("expected IfaCacheInfoSize mismatch")
 	}
 }
