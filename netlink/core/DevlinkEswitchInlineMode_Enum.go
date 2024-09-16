93056bf9104b46f04c606bc5778278a6fdbc2a97
Sam Caldwell <github@samcaldwell.net>
2024-09-15 22:31:17 -0500

    cleanup

--- netlink/core/DevlinkEswitchInlineMode_Enum.go
+++ netlink/core/DevlinkEswitchInlineMode_Enum.go
@@ -1,28 +1,28 @@
 package core
 
-// DevlinkEswitchInlineModeEnum - devlink_eswitch_inline_mode
+// DevlinkEswitchInlineMode - devlink_eswitch_inline_mode
 //
 // See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-type DevlinkEswitchInlineModeEnum uint8
+type DevlinkEswitchInlineMode uint8
 
 const (
 	// DevlinkEswitchInlineModeNone - DEVLINK_ESWITCH_INLINE_MODE_NONE
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkEswitchInlineModeNone DevlinkEswitchInlineModeEnum = 0
+	DevlinkEswitchInlineModeNone DevlinkEswitchInlineMode = 0
 
 	// DevlinkEswitchInlineModeLink - DEVLINK_ESWITCH_INLINE_MODE_LINK
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkEswitchInlineModeLink DevlinkEswitchInlineModeEnum = 1
+	DevlinkEswitchInlineModeLink DevlinkEswitchInlineMode = 1
 
 	// DevlinkEswitchInlineModeNetwork - DEVLINK_ESWITCH_INLINE_MODE_NETWORK
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkEswitchInlineModeNetwork DevlinkEswitchInlineModeEnum = 2
+	DevlinkEswitchInlineModeNetwork DevlinkEswitchInlineMode = 2
 
 	// DevlinkEswitchInlineModeTransport - DEVLINK_ESWITCH_INLINE_MODE_TRANSPORT
 	//
 	// See https://github.com/torvalds/linux/blob/master/include/uapi/linux/devlink.h
-	DevlinkEswitchInlineModeTransport DevlinkEswitchInlineModeEnum = 3
+	DevlinkEswitchInlineModeTransport DevlinkEswitchInlineMode = 3
 )
--- netlink/core/DevlinkEswitchInlineMode_Enum_test.go
+++ netlink/core/DevlinkEswitchInlineMode_Enum_test.go
@@ -8,14 +8,14 @@ import (
 func TestDevlinkEswitchInlineModeEnum(t *testing.T) {
 	t.Run("size check", func(t *testing.T) {
 		const expectedSizeInBytes = 1
-		if sz := int(unsafe.Sizeof(DevlinkEswitchInlineModeEnum(0))); sz != expectedSizeInBytes {
+		if sz := int(unsafe.Sizeof(DevlinkEswitchInlineMode(0))); sz != expectedSizeInBytes {
 			t.Fatal("size check failed")
 		}
 	})
 	t.Run("value check", func(t *testing.T) {
 		type TestData struct {
-			actual DevlinkEswitchInlineModeEnum
-			expect DevlinkEswitchInlineModeEnum
+			actual DevlinkEswitchInlineMode
+			expect DevlinkEswitchInlineMode
 		}
 		testData := []TestData{
 			{actual: DevlinkEswitchInlineModeNone, expect: 0},
