20016e2701508d59f27ae81795aef67057315292
Sam Caldwell <github@samcaldwell.net>
2024-09-15 22:53:35 -0500

    code cleanup

--- netlink/core/GtpRole_Enum.go
+++ netlink/core/GtpRole_Enum.go
@@ -1,20 +1,20 @@
 package core
 
-// GtpRoleEnum - Enumeration for GTP (GPRS Tunneling Protocol) roles.
+// GtpRole - Enumeration for GTP (GPRS Tunneling Protocol) roles.
 //
 // This enumeration defines the roles within a GTP setup, such as GGSN (Gateway GPRS Support Node) and SGSN (Serving GPRS Support Node),
 // which are key components in mobile networks.
 //
 // See https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_link.h
-type GtpRoleEnum uint8
+type GtpRole uint8
 
 const (
 
 	// GtpRoleGgsn - GTP_ROLE_GGSN -
 	// Represents the GGSN (Gateway GPRS Support Node) role in a GTP context.
-	GtpRoleGgsn GtpRoleEnum = 0
+	GtpRoleGgsn GtpRole = 0
 
 	// GtpRoleSgsn - GTP_ROLE_SGSN -
 	// Represents the SGSN (Serving GPRS Support Node) role in a GTP context.
-	GtpRoleSgsn GtpRoleEnum = 1
+	GtpRoleSgsn GtpRole = 1
 )
--- netlink/core/GtpRole_Enum_test.go
+++ netlink/core/GtpRole_Enum_test.go
@@ -5,18 +5,18 @@ import (
 	"unsafe"
 )
 
-// TestGtpRoleEnum tests the size of the GtpRoleEnum type and the values of its enumerated constants.
+// TestGtpRoleEnum tests the size of the GtpRole type and the values of its enumerated constants.
 func TestGtpRoleEnum(t *testing.T) {
-	// Verify the size of the GtpRoleEnum type is 1 byte (uint8)
+	// Verify the size of the GtpRole type is 1 byte (uint8)
 	if size := unsafe.Sizeof(GtpRoleGgsn); size != 1 {
-		t.Errorf("Expected size of GtpRoleEnum to be 1 byte, but got %d", size)
+		t.Errorf("Expected size of GtpRole to be 1 byte, but got %d", size)
 	}
 
 	// Test the values of the constants
 	tests := []struct {
 		name  string
-		value GtpRoleEnum
-		want  GtpRoleEnum
+		value GtpRole
+		want  GtpRole
 	}{
 		{"GtpRoleGgsn", GtpRoleGgsn, 0},
 		{"GtpRoleSgsn", GtpRoleSgsn, 1},
