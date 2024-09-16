8de4d181783d6e16682158d6a369de9be02ed254
Sam Caldwell <github@samcaldwell.net>
2024-09-15 21:20:05 -0500

    add containerized test for NetlinkSocket.Send()

--- netlink/core/NetlinkSocket_Send_test.go
+++ netlink/core/NetlinkSocket_Send_test.go
@@ -1,1 +1,27 @@
 package core
+
+import (
+	"github.com/sam-caldwell/network/test"
+	"testing"
+)
+
+func TestNetlinkSocket_Send(t *testing.T) {
+
+	const testDockerImage = "network-test:latest"
+
+	t.Run("Test NetlinkSocket.Send() method happy path", func(t *testing.T) {
+
+		t.Run("build container", func(t *testing.T) {
+			if err := test.BuildTestContainer(testDockerImage); err != nil {
+				t.Fatalf("container build error: %v", err)
+			}
+		})
+
+		t.Run("run test container", func(t *testing.T) {
+			if err := test.RunContainer(testDockerImage, "TestNetlinkSocketSend"); err != nil {
+				t.Fatalf("test container failed: %v", err)
+			}
+		})
+
+	})
+}
